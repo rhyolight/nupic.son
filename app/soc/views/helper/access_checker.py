becbfc10fd748b58aa9c447176307a8644c14d30
Madhusudan.C.S <get_email Madhusudan.C.S>
2011-04-08 22:52:09 +0530

    Language fixes in access check messages as suggested by Sverre.

--- app/soc/views/helper/access_checker.py
+++ app/soc/views/helper/access_checker.py
@@ -76,9 +76,9 @@ DEF_PROPOSAL_MODIFICATION_REQUEST_MSG = ugettext(
     'proposal.')
 
 DEF_PROPOSAL_IGNORED_MESSAGE = ugettext(
-    'Your organization administrators have flagged this proposal to be '
-    'ignored. If you think this is incorrect, contact your organization '
-    'administrators to resolve the situation.')
+    'An organization administrator has flagged this proposal to be '
+    'ignored. If you think this is incorrect, contact an organization '
+    'administrator to resolve the situation.')
 
 DEF_PAGE_INACTIVE_BEFORE_MSG_FMT = ugettext(
     'This page is inactive before %s')
