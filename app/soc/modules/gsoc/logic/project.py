ae3f4cec775d95fe923f1c7c19a18a196f182acb
Madhusudan.C.S <get_email Madhusudan.C.S>
2011-04-25 01:14:05 +0530

    Move FeaturedProject logic from old models based logic to new project logic module.
    The function is also refactored to obtain the query within the function.

---
+++ app/soc/modules/gsoc/logic/project.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python2.5
+#
+# Copyright 2011 the Melange authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Logic for GSoC Project Model.
+"""
+
+__authors__ = [
+    '"Madhusudan.C.S" <madhusudancs@gmail.com>',
+    ]
+
+
+import datetime
+
+from google.appengine.api import memcache
+from google.appengine.ext import db
+
+from soc.modules.gsoc.models.project import GSoCProject
+
+
+def getFeaturedProject(self, current_timeline, program):
+  """Return a featured project for a given program.
+
+  Args:
+    current_timeline: where we are currently on the program timeline
+    program: entity representing the program from which the featured
+        projects should be fetched
+  """
+  # expiry time to fetch the new featured project entity
+  # the current expiry time is 2 hours.
+  expiry_time = datetime.timedelta(seconds=7200)
+
+  properties = {
+      # TODO(Madhu): Enable it once the featured property is added to student_project model
+      #'is_featured': True
+      }
+
+  def queryForProject():
+    query = GSoCProject.all()
+    query.filter('program', program)
+    if current_timeline == 'coding_period':
+      project_status = 'accepted'
+    else:
+      project_status = 'completed'
+    query.filter('status', project_status)
+    return query
+
+  q = queryForProject()
+
+  # the cache stores a 3-tuple in the order student_project entity,
+  # cursor and the last time the cache was updated
+  fsp_cache = memcache.get('featured_gsoc_project')
+
+  if fsp_cache:
+    cached_project, cached_cursor, cache_expiry_time = fsp_cache
+    if not datetime.datetime.now() > cache_expiry_time + expiry_time:
+      return cached_project
+    else:
+      q.with_cursor(cached_cursor)
+      if q.count() == 0:
+        q = queryForProject()
+
+  new_project = q.get()
+  new_cursor = q.cursor()
+  memcache.set(
+    key='featured_gsoc_project',
+    value=(new_project, new_cursor, datetime.datetime.now()))
+
+  return new_project
