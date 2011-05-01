4e7645aa2454171d6f50f0cc1d3fce9a0c7f9cfb
Madhusudan.C.S <get_email Madhusudan.C.S>
2011-05-02 01:51:52 +0530

    Rewrite the parts of the map code that use obsolete API v2 and retain the rest.
    --HG--
    extra : rebase_source : fa56152c84d21781b9973a615deeabd5c75bbda6

--- app/soc/content/js/melange.map.js
+++ app/soc/content/js/melange.map.js
@@ -54,6 +54,117 @@
     };
     var map = new google.maps.Map(document.getElementById(map_div),
         init_options);
-  }
 
+    var mentors = {};
+    var students = {};
+    var projects = {};
+    var polylines = [];
+
+    // Iterate over projects to draw polylines
+    jQuery.each(map_data.projects, function (key, project) {
+      var current_student = map_data.students[project.student_key];
+      var current_mentor = map_data.mentors[project.mentor_key];
+      if (current_student !== undefined && 
+          current_mentor !== undefined &&
+          current_student.lat !== null &&
+          current_student.lng !== null &&
+          current_mentor.lat !== null &&
+          current_mentor.lng !== null) {
+            polylines.push([
+              new google.maps.LatLng(current_student.lat, current_student.lng),
+              new google.maps.LatLng(current_mentor.lat, current_mentor.lng)
+            ]);
+      }
+    });
+
+    // Iterate over students
+    jQuery.each(map_data.students, function (key, person) {
+      var html = "";
+      var marker = null;
+
+      if (person.lat !== null && person.lng !== null) {
+        var point = new google.maps.LatLng(person.lat, person.lng);
+
+        var marker = new google.maps.Marker({
+            position: point,
+            title: person.name,
+            map: map
+        });
+        html = [
+          "<strong>", person.name, "</strong><br />",
+          "<span style='font-style:italic;'>Student</span><br />",
+          "<div style='height:100px;width:300px;",
+          "overflow:auto;'>"
+        ].join("");
+        // iterate through projects
+        jQuery.each(person.projects, function () {
+          var current_project = map_data.projects[this];
+          html += [
+            "<a href='", current_project.link, "'>",
+            current_project.title, "</a><br />",
+            "Mentor: ", current_project.mentor_name, "<br />"
+          ].join("");
+        });
+        html += "</div>";
+
+        var infowindow = new google.maps.InfoWindow({
+          content: html
+        });
+        google.maps.event.addListener(marker, "click", function () {
+          infowindow.open(map, marker);
+        });
+      }
+    });
+
+    // Iterate over mentors
+    jQuery.each(map_data.mentors, function (key, person) {
+      var html = "";
+      var marker = null;
+
+      if (person.lat !== null && person.lng !== null) {
+        var point = new google.maps.LatLng(person.lat, person.lng);
+
+        marker = new google.maps.Marker({
+            position: point,
+            title: person.name,
+            icon: "/soc/content/" + melange.config.app_version + "/images/mentor-marker.png",
+            map: map
+        });
+        html = [
+          "<strong>", person.name, "</strong><br />",
+          "<span style='font-style:italic;'>Mentor</span><br />",
+          "<div style='height:100px;width:300px;",
+          "overflow:auto;font-size:70%'>"
+        ].join("");
+        // iterate through projects
+        jQuery.each(person.projects, function () {
+          var current_project = map_data.projects[this];
+          html += [
+            "<a href='", current_project.link, "'>",
+            current_project.title, "</a><br />",
+            "Student: ", current_project.student_name, "<br />"
+          ].join("");
+        });
+        html += "</div>";
+
+        var infowindow = new google.maps.InfoWindow({
+          content: html
+        });
+        google.maps.event.addListener(marker, "click", function () {
+          infowindow.open(map, marker);
+        });
+      }
+    });
+
+    // Draw all polylines
+    jQuery.each(polylines, function () {
+      new google.maps.Polyline({
+        path: this,
+        strokeColor: "#FF0000",
+        strokeOpacity: 1.0,
+        strokeWeight: 3,
+        map: map
+      });
+    });
+  }
 }());
