429fdc0aab10f25b115ed86cf87561ac620e922c
Sverre Rabbelier <get_email Sverre Rabbelier>
2011-05-04 23:56:28 +0200

    Add note about autocomplete and fix UCB name

--- app/soc/models/role.py
+++ app/soc/models/role.py
@@ -46,11 +46,13 @@ class StudentInfo(soc.models.base.ModelWithFieldAttributes):
   school_name.help_text = ugettext(
       'Please enter the full name of your school, college or university in'
       ' this field. Please use the complete formal name of your school, e.g.'
-      ' UC Berkeley instead of Cal or UCB. It would be most wonderful if you'
-      ' could provide your school\'s name in English, as all the program '
-      'administrators speak English as their first language and it will make'
-      ' it much easier for us to assemble program statistics, etc., later if'
-      ' we can easily read the name of your school.')
+      ' University of California at Berkley, instead of Cal or UCB. It would'
+      ' be most wonderful if you could provide your school\'s name in English,'
+      ' as all the program administrators speak English as their first'
+      ' language and it will make it much easier for us to assemble program'
+      ' statistics, etc., later if we can easily read the name of your school.'
+      ' Also, please try to select your school\'s name from the dropdown. If'
+      ' your school is not listed, just enter it manually.')
 
   school_country = db.StringProperty(required=True,
       verbose_name=ugettext('School Country/Territory'),
