fb1fffd93a60670802d5cf4fd04a07d445d36b7e
Madhusudan.C.S <get_email Madhusudan.C.S>
2011-06-20 03:24:49 +0530

    Refactor the forms code in project evaluation module to use the Forms class from forms module.

--- app/soc/modules/gsoc/views/project_evaluation.py
+++ app/soc/modules/gsoc/views/project_evaluation.py
@@ -37,8 +37,8 @@ from soc.modules.gsoc.views.base_templates import LoggedInMsg
 from soc.modules.gsoc.views.helper import url_patterns
 
 
-class SurveyEditForm(forms.ModelForm):
-  """Django form for creating and/or editing survey.
+class GSoCProjectSurveyEditForm(forms.SurveyEditForm):
+  """Form to create/edit GSoC project survey for students.
   """
 
   class Meta:
@@ -49,103 +49,9 @@ class SurveyEditForm(forms.ModelForm):
                'prefix', 'survey_order']
 
 
-class SurveyTakeForm(forms.ModelForm):
-  """Django form for taking a survey.
+class GSoCProjectSurveyTakeForm(forms.SurveyTakeForm):
+  """Form for students to respond to the survey during evaluations.
   """
-
-  def __init__(self, survey_content, *args, **kwargs):
-    super(SurveyTakeForm, self).__init__(*args, **kwargs)
-    self.survey_content = survey_content
-    self.constructForm()
-
-  def create(self, commit=True, key_name=None, parent=None):
-    """Save this form's cleaned data as dynamic properties of a new
-    model instance.
-
-    Args:
-      commit: optional bool, default True; if true, the model instance
-        is also saved to the datastore.
-      key_name: the key_name of the new model instance, default None
-      parent: the parent of the new model instance, default None
-
-    Returns:
-      The model instance created by this call.
-    Raises:
-      ValueError if the data couldn't be validated.
-    """
-    instance = super(SurveyTakeForm, self).create(
-        commit=False, key_name=key_name, parent=parent)
-
-    for name, value in self.cleaned_data.iteritems():
-      # if the property is not to be updated, skip it
-      if self._meta.exclude:
-        if name in self._meta.exclude:
-          continue
-      if self._meta.fields:
-        if name not in self._meta.fields:
-          continue
-
-      setattr(instance, name, value)
-
-    if commit:
-      instance.put()
-    return instance
-
-  def constructForm(self):
-    """Constructs the form based on the schema stored in the survey content
-    """
-    # insert dynamic survey fields
-    if self.survey_content:
-      # TODO(madhu): Convert this to JSON
-      schema = eval(self.survey_content.schema)
-      for field_name in self.survey_content.survey_order:
-        field_info = schema.get(field_name)
-        self.constructField(field_name, field_info)
-
-  def constructField(self, field_name, field_info):
-    """Constructs the field for the given field metadata
-
-    Args:
-      field_name: Name of the field that must be populated
-      field_info: Meta data containing how the field must be constructed
-    """
-    type = field_info.get('type', '')
-    render = field_info.get('render', '')
-    label = field_info.get('question', '')
-    required = field_info.get('required', True)
-    comment = field_info.get('has_comment', False)
-    help_text = field_info.get('tip', '')
-
-    choices = [(choice, choice) for choice in getattr(
-        self.survey_content, field_name, [])]
-
-    widget = None
-
-    if type == 'selection':
-      field = django_forms.ChoiceField
-    elif type == 'pick_multi':
-      field = django_forms.MultipleChoiceField
-      widget = forms.CheckboxSelectMultiple()
-    elif type == 'choice':
-      field = django_forms.ChoiceField
-      widget = forms.RadioSelect()
-    elif type == 'pick_quant':
-      field = django_forms.ChoiceField
-      widget = forms.RadioSelect()
-    elif type == 'long_answer':
-      field = django_forms.CharField
-      widget = django_forms.Textarea()
-    elif type == 'short_answer':
-      field = django_forms.CharField
-
-    self.fields[field_name] = field(label=label, required=required,
-                                    help_text=help_text)
-    if widget:
-      self.fields[field_name].widget = widget
-    if choices:
-      self.fields[field_name].choices = choices
-    if self.instance:
-      self.fields[field_name].initial = getattr(self.instance, field_name)
 
   class Meta:
     model = GSoCProjectSurveyRecord
@@ -177,8 +83,8 @@ class SurveyEditPage(RequestHandler):
     org_app = db.get(org_app_key)
     # Test code end
 
-    form = SurveyEditForm(self.data.POST or None,
-                          instance=org_app.survey_content)
+    form = GSoCProjectSurveyEditForm(self.data.POST or None,
+                                     instance=org_app.survey_content)
 
     context = {
         'page_name': "Midterm survey page",
@@ -213,12 +119,12 @@ class SurveyTakePage(RequestHandler):
 
   def context(self):
     if self.data.project_survey_record:
-      form = SurveyTakeForm(
+      form = GSoCProjectSurveyTakeForm(
           self.data.project_survey.survey_content,
           self.data.POST or None, instance=self.data.project_survey_record)
     else:
-      form = SurveyTakeForm(self.data.project_survey.survey_content,
-                            self.data.POST or None)
+      form = GSoCProjectSurveyTakeForm(
+          self.data.project_survey.survey_content, self.data.POST or None)
 
     context = {
         'page_name': "Midterm survey page",
@@ -236,12 +142,12 @@ class SurveyTakePage(RequestHandler):
       a newly created or updated survey record entity or None
     """
     if self.data.project_survey_record:
-      form = SurveyTakeForm(
+      form = GSoCProjectSurveyTakeForm(
           self.data.project_survey.survey_content,
           self.data.POST, instance=self.data.project_survey_record)
     else:
-      form = SurveyTakeForm(self.data.project_survey.survey_content,
-                            self.data.POST)
+      form = GSoCProjectSurveyTakeForm(
+          self.data.project_survey.survey_content, self.data.POST)
 
     if not form.is_valid():
       return None
