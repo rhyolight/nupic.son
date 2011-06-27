f1729bba7e52a23757cf5e5c6d077af1e92ba089
Madhusudan.C.S <get_email Madhusudan.C.S>
2011-06-27 21:08:48 +0530

    Allow dev_appserver to use crypto C extension. Details in the issue report below.
    http://code.google.com/p/googleappengine/issues/detail?id=4168
    This patch was also submitted in the bug report itself. The bug
    probably only hits Ubuntu/Debian users.

--- thirdparty/google_appengine/google/appengine/tools/dev_appserver.py
+++ thirdparty/google_appengine/google/appengine/tools/dev_appserver.py
@@ -1564,6 +1564,9 @@ class HardenedModulesHook(object):
       'RIPEMD',
       'SHA256',
       'XOR',
+      '_counter',
+      '_fastmath',
+      'strxor',
 
       '_Crypto_Cipher__AES',
       '_Crypto_Cipher__ARC2',
