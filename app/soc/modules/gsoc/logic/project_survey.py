c4d804b7065ca40717ee49c5ca07525e445a7e1d
Madhusudan.C.S <get_email Madhusudan.C.S>
2011-07-04 02:05:18 +0530

    Implement logic functions to fetch the survey entities with the given name, for the given program.
    --HG--
    extra : rebase_source : 1ef98f37d1c2f6d3e11d332402e933efde486058

---
+++ app/soc/modules/gsoc/logic/project_survey.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python2.5
+#
+# Copyright 2011 the Melange authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Logic for ProjectSurvey which stores student evaluation survey Model.
+"""
+
+__authors__ = [
+    '"Madhusudan.C.S" <madhusudancs@gmail.com>',
+    ]
+
+
+from soc.modules.gsoc.models.project_survey import ProjectSurvey
+
+
+def getProjectSurveyForProgram(program, survey):
+  """Return the survey entity for a given program and the survey link id.
+
+  Args:
+    program: entity representing the program from which the featured
+        projects should be fetched
+    survey: link id of the survey which should be fetched
+  """
+  q = ProjectSurvey.all()
+  q.filter('scope', program)
+  q.filter('link_id', survey)
+
+  return q.get()
