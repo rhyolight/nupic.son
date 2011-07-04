783bdb7e03f091a53cff3ba71049a140f08dd4d5
Madhusudan.C.S <get_email Madhusudan.C.S>
2011-07-05 03:01:20 +0530

    Rewrite AssignMentor template to build a context containing list of mentors.
    Due to the recent changes of deciding to use a list of mentors for a project
    instead of separate single mentor property and a list of additional_mentors
    property, we make changes to this template class to build a list of mentors,
    their keys and names as the context and pass on to the HTML template that
    renders the final select widgets.

--- app/soc/modules/gsoc/views/assign_mentor.py
+++ app/soc/modules/gsoc/views/assign_mentor.py
@@ -34,7 +34,7 @@ def getMentorsChoicesToAssign(mentors, current_mentor=None):
   Args:
     mentors: List of potential mentor entities whose Django style choices
         must be returned
-    current_mentor: Currently assigned mentor entity in this list
+    current_mentor: Key of currently assigned mentor key
     
   """
 
@@ -46,7 +46,7 @@ def getMentorsChoicesToAssign(mentors, current_mentor=None):
         'key': m_key,
         'name': m.name(),
         }
-    if current_mentor and m_key == current_mentor.key():
+    if current_mentor and m_key == current_mentor:
       choice['selected'] = True
 
     mentors_choices.append(choice)
@@ -57,13 +57,13 @@ class AssignMentorFields(Template):
   """Template to render the fields necessary to assign a mentor to a proposal.
   """
 
-  def __init__(self, data, current_mentor, action,
+  def __init__(self, data, current_mentors, action,
                all_mentors=None, possible_mentors=None,
                mentor_required=False):
     """Instantiates the template for Assign mentor buttons for org admin.
 
     data: The request data object
-    current_mentor: Currently assigned mentor entity
+    current_mentors: List of Keys of currently assigned mentors to the project
     action: The form action URL to which the form should be posted
     all_mentors: Set of all the mentors that can be assigned to this entity
     possible_mentors: List of possible mentors that can be assigned to
@@ -71,33 +71,55 @@ class AssignMentorFields(Template):
     mentor_required: True if org admin is not allowed to unassign a mentor
     """
     super(AssignMentorFields, self).__init__(data)
-    self.current_mentor = current_mentor
+    self.current_mentors = current_mentors
     self.action = action
     self.all_mentors = all_mentors
     self.possible_mentors = possible_mentors
     self.mentor_required = mentor_required
 
-  def context(self):
-    context = {
-        'action': self.action,
-        'mentor_required': self.mentor_required,
-        }
+  def _getMentorContext(self, current_mentor=None):
+    """Returns the context for assigning mentors along with the current state.
 
+    Args:
+      current_mentor: Currently assigned mentor key to be set as initial value
+    """
+    mentor_context = {}
     if self.possible_mentors:
       possible_mentors = db.get(self.possible_mentors)
       possible_mentor_choices = getMentorsChoicesToAssign(
-          possible_mentors, self.current_mentor)
-      context['possible_mentors'] = sorted(possible_mentor_choices)
+          possible_mentors, current_mentor)
+      mentor_context['possible_mentors'] = sorted(
+          possible_mentor_choices, key=lambda c: c.get('name', ''))
 
     if self.all_mentors:
       if self.possible_mentors:
         self.all_mentors = set(self.all_mentors) - set(self.possible_mentors)
       all_mentors = db.get(self.all_mentors)
       all_mentor_choices = getMentorsChoicesToAssign(
-          all_mentors, self.current_mentor)
-      context['all_mentors'] = sorted(all_mentor_choices)
+          all_mentors, current_mentor)
+      mentor_context['all_mentors'] = sorted(
+          all_mentor_choices, key=lambda c: c.get('name', ''))
 
-    return context
+    return mentor_context
+
+  def context(self):
+    mentors = []
+
+    # add a select drop down context for each assigned mentor with that
+    # mentor set as initial value
+    for m in self.current_mentors:
+      mentors.append(self._getMentorContext(current_mentor=m))
+
+    # if there are no mentors assigned at all render a single drop down
+    # without any initial mentor set
+    if not self.current_mentors:
+      mentors.append(self._getMentorContext())
+
+    return {
+        'action': self.action,
+        'mentor_required': self.mentor_required,
+        'mentors': mentors,
+        }
 
   def templatePath(self):
     return 'v2/modules/gsoc/_assign_mentor/base.html'
