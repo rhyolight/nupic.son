72b2128287f9a431a43dabc4fcfa648e87ffb972
Madhusudan.C.S <get_email Madhusudan.C.S>
2011-07-05 04:31:10 +0530

    Add mapreduce processor to merge mentor and additional_mentors properties into mentors property.

---
+++ app/soc/mapreduce/convert_project_mentors.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python2.5
+#
+# Copyright 2011 the Melange authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Map reduce to merge mentor and co-mentors properties in GSoCProject.
+"""
+
+__authors__ = [
+  '"Madhusudan.C.S" <madhusudancs@gmail.com>',
+  ]
+
+
+import logging
+
+from google.appengine.ext import db
+from google.appengine.ext.mapreduce import operation
+
+from soc.modules.gsoc.models.profile import GSoCProfile
+from soc.modules.gsoc.models.project import GSoCProject
+
+
+def process(project_key):
+  project = db.get(project_key)
+  if not project:
+    yield operation.counters.Increment("missing_project")
+
+  mentors = [project.mentor.key()]
+  for am in project.additional_mentors:
+    if am not in mentors:
+      mentors.append(am)
+
+  def update_project_txn():
+    project = db.get(project_key)
+    if not project:
+      logging.error("Missing project for key '%s'." % project_key)
+      return False
+
+    project.mentors = mentors
+    logging.warn(mentors)
+    project.put()
+    return True
+
+  if db.run_in_transaction(update_project_txn):
+    yield operation.counters.Increment("projects_updated")
+  else:
+    yield operation.counters.Increment("missing_project")
