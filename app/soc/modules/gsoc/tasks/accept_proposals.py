9f39bdaef0aa1a595ec92154253143d2a4836b3e
Sverre Rabbelier <get_email Sverre Rabbelier>
2011-07-13 10:56:12 +0200

    Fix accept proposals task

--- app/soc/modules/gsoc/tasks/accept_proposals.py
+++ app/soc/modules/gsoc/tasks/accept_proposals.py
@@ -272,7 +272,7 @@ class ProposalAcceptanceTask(object):
       'program': proposal.program,
       'title': proposal.title,
       'abstract': proposal.abstract,
-      'mentor': proposal.mentor,
+      'mentors': [proposal.mentor],
       }
     student_profile = proposal.parent()
     project = GSoCProject(parent=student_profile, **fields)
