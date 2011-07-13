cfb4eb775e7db8559039308192d1cded2fa6ab8e
Madhusudan.C.S <get_email Madhusudan.C.S>
2011-07-14 05:06:21 +0530

    Add a student evaluation componennt and make mentor evaluation component inherit from it.
    We also add additional methods to return the status and the grade
    for the mentor evaluation list component.

--- app/soc/modules/gsoc/views/org_admin_dashboard.py
+++ app/soc/modules/gsoc/views/org_admin_dashboard.py
@@ -32,6 +32,8 @@ from soc.views.template import Template
 from soc.modules.gsoc.logic import project as project_logic
 from soc.modules.gsoc.logic import grading_project_survey as gps_logic
 from soc.modules.gsoc.logic import grading_project_survey_record as gpsr_logic
+from soc.modules.gsoc.logic import project_survey as ps_logic
+from soc.modules.gsoc.logic import project_survey_record as psr_logic
 from soc.modules.gsoc.models.project import GSoCProject
 from soc.modules.gsoc.views import dashboard
 from soc.modules.gsoc.views.helper import url_patterns
@@ -43,19 +45,17 @@ DEF_NOT_ADMIN_MSG = ugettext(
     'organization in the program to access this page.')
 
 
-class MentorEvaluationComponent(dashboard.Component):
-  """Component for listing mentor evaluations for organizations.
+class StudentEvaluationComponent(dashboard.Component):
+  """Component for listing student evaluations for organizations.
   """
+    self.idx = idx
 
-  def __init__(self, request, data):
+  def __init__(self, request, data, idx):
     self.request = request
     self.data = data
 
-    evaluation = 'midterm'
-    mm_eval = gps_logic.getGradingProjectSurveyForProgram(
-        self.data.program, evaluation)
+    self.evaluation = 'midterm'
 
-    r = data.redirect
     list_config = lists.ListConfiguration(add_key_column=False)
     list_config.addColumn('key', 'Key', (lambda ent, *args: "%s/%s" % (
         ent.parent().key().name(), ent.key().id())), hidden=True)
@@ -68,24 +68,24 @@ class MentorEvaluationComponent(dashboard.Component):
         'mentors', 'Mentors', lambda entity, mentors, *args: ', '.join(
             [mentors.get(m).name() for m in entity.mentors]))
     list_config.addColumn(
-        'status', 'Status', lambda entity, *args: dashboard.colorize(
-            gpsr_logic.evalRecordExistsForStudent(
-            mm_eval, entity), "Submitted", "Not submitted"))
+        'status', 'Status', self._getStatus)
     list_config.setDefaultSort('student')
-    list_config.setRowAction(lambda entity, *args, **kwargs:
-        r.survey_record(
-            'midterm', entity.key().id_or_name(),
-            entity.parent().link_id).urlOf(
-                'gsoc_take_mentor_evaluation'))
     self._list_config = list_config
+
+  def _getStatus(self, entity, *args):
+    eval = ps_logic.getProjectSurveyForProgram(
+        self.data.program, self.evaluation)
+    return dashboard.colorize(
+        psr_logic.evalRecordExistsForStudent(
+        eval, entity), "Submitted", "Not submitted")
 
   def context(self):
     list = lists.ListConfigurationResponse(
-        self.data, self._list_config, idx=0)
+        self.data, self._list_config, idx=self.idx)
 
     return {
         'lists': [list],
-        'title': 'Mentor Evaluations - Midterm',
+        'title': 'Student Evaluations - Midterm',
         }
 
   def getListData(self):
@@ -95,7 +95,7 @@ class MentorEvaluationComponent(dashboard.Component):
     returned.
     """
     idx = lists.getListIndex(self.request)
-    if idx == 0:
+    if idx == self.idx:
       list_query = project_logic.getProjectsQueryForOrgs(
           orgs=self.data.org_admin_for)
 
@@ -112,6 +112,43 @@ class MentorEvaluationComponent(dashboard.Component):
 
   def templatePath(self):
     return'v2/modules/gsoc/dashboard/list_component.html'
+
+
+class MentorEvaluationComponent(StudentEvaluationComponent):
+  """Component for listing mentor evaluations for organizations.
+  """
+
+  def __init__(self, request, data, idx):
+    super(MentorEvaluationComponent, self).__init__(request, data, idx)
+
+    self.record = None
+
+    self._list_config.addColumn(
+        'grade', 'Grade', self._getGrade)
+    self._list_config.setRowAction(lambda entity, *args, **kwargs:
+        data.redirect.survey_record(
+            self.evaluation, entity.key().id_or_name(),
+            entity.parent().link_id).urlOf(
+                'gsoc_take_mentor_evaluation'))
+
+  def _getStatus(self, entity, *args):
+    eval = gps_logic.getGradingProjectSurveyForProgram(
+        self.data.program, self.evaluation)
+    self.record = gpsr_logic.getEvalRecord(eval, entity)
+    return dashboard.colorize(
+        bool(self.record), "Submitted", "Not submitted")
+
+  def _getGrade(self, entity, *args):
+    if self.record:
+      return dashboard.colorize(
+        self.record.grade, "Pass", "Fail")
+    else:
+      return "N/A"
+
+  def context(self):
+    context = super(MentorEvaluationComponent, self).context()
+    context['title'] = 'Mentor Evaluations - Midterm'
+    return context
 
 
 class Dashboard(dashboard.Dashboard):
@@ -138,5 +175,6 @@ class Dashboard(dashboard.Dashboard):
   def _getActiveComponents(self):
     """Returns the components that are active on the page.
     """
-    components = [MentorEvaluationComponent(self.request, self.data)]
+    components = [MentorEvaluationComponent(self.request, self.data, 0),
+                  StudentEvaluationComponent(self.request, self.data, 1)]
     return components
