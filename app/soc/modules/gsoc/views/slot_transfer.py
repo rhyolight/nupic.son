0b0f700e5e15f3c557d71e30fe52358c58484d15
Praveen Kumar <get_email Praveen Kumar>
2011-08-10 21:52:29 +0530

    Fix gsoc.views.slot_transfer.

--- app/soc/modules/gsoc/views/slot_transfer.py
+++ app/soc/modules/gsoc/views/slot_transfer.py
@@ -144,7 +144,7 @@ class UpdateSlotTransferPage(RequestHandler):
 
   def checkAccess(self):
     self.check.isLoggedIn()
-    self.check.isProgramActive()
+    self.check.isProgramVisible()
     self.mutator.organizationFromKwargs()
     self.check.isOrganizationInURLActive()
     self.check.isOrgAdminForOrganization(self.data.organization)
