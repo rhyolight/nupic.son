88022df4b41440228c3b109e80284163c60411a6
Sverre Rabbelier <get_email Sverre Rabbelier>
2011-08-24 15:08:32 -0700

    Spring cleaning

--- app/soc/views/helper/decorators.py
+++ app/soc/views/helper/decorators.py
@@ -19,102 +19,12 @@
 
 __authors__ = [
   '"Doug Coker" <dcoker@google.com>',
-  '"Sverre Rabbelier" <sverre@rabbelier.nl>',
-  '"Pawel Solyga" <pawel.solyga@gmail.com>',
   ]
 
 
 from functools import wraps
 
 from django import http
-from django.utils.translation import ugettext
-
-from soc.logic import dicts
-from soc.views.helper import responses
-
-
-class Error(Exception):
-  """Base class for all exceptions raised by this module.
-  """
-
-  pass
-
-
-def view(func):
-  """Decorator that insists that exceptions are handled by view.
-  """
-
-  @wraps(func)
-  def view_wrapper(request, *args, **kwds):
-    """View decorator wrapper method.
-    """
-
-    return func(request, *args, **kwds)
-
-  return view_wrapper
-
-
-def merge_params(func):
-  """Decorator that merges 'params' with self._params.
-  """
-
-  @wraps(func)
-  def wrapper(self, *args, **kwargs):
-    """Decorator wrapper method.
-    """
-    params = kwargs.get('params', {})
-    kwargs['params'] = dicts.merge(params, self._params)
-    return func(self, *args, **kwargs)
-
-  return wrapper
-
-
-def check_access(func):
-  """This decorator does access checks for the specified view method.
-
-  The rights dictionary is extracted from 'params', or, if either 'params' or
-  'rights' do not exist, from self._params['rights'].
-  """
-
-  # Do not pollute helper.decorators with access specific imports
-  from soc.views import out_of_band
-  from soc.views import helper
-  from soc.views.helper import responses
-
-  @wraps(func)
-  def wrapper(self, request, access_type, *args, **kwargs):
-    """Decorator wrapper method.
-    """
-    params = kwargs.get('params', {})
-
-    # Try to extract rights
-    if 'rights' in params:
-      rights = params['rights']
-    else:
-      rights = self._params['rights']
-
-    check_kwargs = kwargs.copy()
-    context = responses.getUniversalContext(request)
-    responses.useJavaScript(context, self._params['js_uses_all'])
-
-    id = context['account']
-    user = context['user']
-
-    check_kwargs['GET'] = request.GET
-    check_kwargs['POST'] = request.POST
-    check_kwargs['context'] = context
-
-    # reset and pre-fill the Checker's cache
-    rights.setCurrentUser(id, user)
-
-    # Do the access check dance
-    try:
-      rights.checkAccess(access_type, check_kwargs)
-    except out_of_band.Error, error:
-      return helper.responses.errorResponse(error, request)
-    return func(self, request, access_type, *args, **kwargs)
-
-  return wrapper
 
 
 def mutation(func):
