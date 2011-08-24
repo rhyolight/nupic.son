fc2e96564a6e94ff8c04428d8965e419f9eb9fa0
Madhusudan.C.S <get_email Madhusudan.C.S>
2011-08-24 06:42:26 +0530

    Implement the backup admin field in the org app record form.

--- app/soc/views/org_app.py
+++ app/soc/views/org_app.py
@@ -22,12 +22,14 @@ __authors__ = [
   ]
 
 
+from django import forms as django_forms
 from django.utils.translation import ugettext
 
 from soc.views import forms
 from soc.views import survey
 from soc.views.helper import lists
 
+from soc.logic import cleaning
 from soc.models.org_app_record import OrgAppRecord
 from soc.models.org_app_survey import OrgAppSurvey
 from soc.views.helper.access_checker import isSet
@@ -51,10 +53,30 @@ class OrgAppTakeForm(forms.SurveyTakeForm):
   """Form for would-be organization admins to apply for the program.
   """
 
+  backup_admin_id = django_forms.CharField(
+      label=ugettext('Backup Admin'), required=True,
+      help_text=ugettext('The Link ID of the user who will serve as the '
+                         'backup admin for this organization.'))
+
   class Meta:
     model = OrgAppRecord
     css_prefix = 'org-app-record'
-    exclude = ['status', 'user', 'survey', 'created', 'modified']
+    exclude = ['main_admin', 'backup_admin', 'status', 'user', 'survey',
+               'created', 'modified']
+
+  clean_org_id = cleaning.clean_link_id('org_id')
+
+  def clean_backup_admin_id(self):
+    backup_admin = cleaning.clean_existing_user('backup_admin_id')(self)
+    self.cleaned_data['backup_admin'] = backup_admin
+    return backup_admin
+
+  def clean(self):
+    cleaned_data = self.cleaned_data
+
+    cleaned_data.pop('backup_admin_id')
+
+    return cleaned_data
 
 
 class OrgAppRecordsList(SiteRequestHandler):
