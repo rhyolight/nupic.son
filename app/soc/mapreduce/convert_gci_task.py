aed3e843d7d6cdb9d5022d42664df0fed7a11b22
Madhusudan.C.S <get_email Madhusudan.C.S>
2011-08-26 23:45:22 +0530

    Mapreduce to convert the GCITasks from keyname based to ID based.
    In the process we recreate all the GCIComment and GCIWorkSubmission entities
    because they have GCITask as parents. So we recreate them with the new
    parent.
    
    We move the subscribers property from GCITaskSubscription model to the
    task model since there is no real need to have a separate model for it.
    
    We also convert the GCIStudentRanking entities which contains the references
    to list of tasks to update to the new entities.
    
    NOTE: We copy the link_id from the old GCITask entities to the new one
    although we will not use them because for replacing the old GCITask entities
    with the new one in the GCIStudentRanking entities we need a way to identify
    the new GCITask entities in the datastore that corresponds to the old entities.
    Once the conversion is run, we can eliminate Link ID completely.

---
+++ app/soc/mapreduce/convert_gci_task.py
@@ -0,0 +1,105 @@
+#!/usr/bin/python2.5
+#
+# Copyright 2011 the Melange authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""GCI Task updating MapReduce.
+"""
+
+__authors__ = [
+  '"Madhusudan.C.S" <madhusudancs@gmail.com>',
+  ]
+
+
+from google.appengine.ext import db
+from google.appengine.ext.mapreduce import operation
+
+from soc.modules.gci.models.comment import GCIComment
+from soc.modules.gci.models.task import GCITask
+from soc.modules.gci.models.task_subscription import GCITaskSubscription
+from soc.modules.gci.models.work_submission import GCIWorkSubmission
+
+
+TASK_PROPERTIES = GCITask.properties()
+# We do not want history property in the new entities in any more because
+# we are not using it
+TASK_PROPERTIES.pop('scope', 'history')
+
+COMMENT_PROPERTIES = GCIComment.properties()
+WORK_SUBMISSION_PROPERTIES = GCIWorkSubmission.properties()
+
+
+def process_task(task):
+  """Conversion to make GCI Tasks ID based and getting rid of unnecessary
+  properties.
+  """
+  # Get the values for all the properties in the GCITask model from the
+  # old entity to create the new entity.
+  new_task_properties = {}
+  for prop in TASK_PROPERTIES:
+    new_task_properties[prop] = getattr(task, prop)
+    new_task_properties['org'] = task.scope
+
+  # We do not want a separate model for subscriptions because there
+  # is only one property that stores the list of all subscribers to
+  # the task which can be stored in the task itself. So moving it to
+  # the task.
+  q = GCITaskSubscription.all()
+  q.filter('task', task)
+  task_subscription = q.get()
+
+  new_task_properties['subscribers'] = task_subscription.subscribers
+
+  new_task = GCITask(**new_task_properties)
+  new_task_key = new_task.put()
+
+  if new_task_key:
+    # Update all the comments with the new task as the parent
+    comments = GCIComment.all().ancestor(new_task_key).fetch(1000)
+    for c in comments:
+      new_comm_properties = {}
+      for c_prop in COMMENT_PROPERTIES:
+        new_comm_properties[c_prop] = getattr(c, c_prop)
+      new_comment = GCIComment(parent=new_task_key, **new_comm_properties)
+      yield operation.db.Put(new_comment)
+      yield operation.counters.Increment("comment_updated")
+
+    # Update all the work submission entities with the new task as the parent
+    work_submissions = GCIWorkSubmission.all().ancestor(new_task_key).fetch(1000)
+    for ws in work_submissions:
+      new_ws_properties = {}
+      for ws_prop in WORK_SUBMISSION_PROPERTIES:
+        new_ws_properties[ws_prop] = getattr(ws, ws_prop)
+      new_ws = GCIWorkSubmission(parent=new_task_key, **new_comm_properties)
+      yield operation.db.Put(new_ws)
+      yield operation.counters.Increment("work_submission_updated")
+
+    yield operation.counters.Increment("task_updated")
+
+
+def process_student_ranking(student_ranking):
+  """Replace all the references to the list of old tasks to the new tasks.
+  """
+  tasks = db.get(student_ranking.tasks)
+  new_tasks = []
+  for t in tasks:
+    q = GCITask.all()
+    q.filter('org', t.scope)
+    q.filter('link_id', t.link_id)
+    new_t = q.get()
+    if new_t:
+      new_tasks.append(new_t)
+
+  yield operation.db.Put(student_ranking)
+  yield operation.counters.Increment("student_ranking_updated")
