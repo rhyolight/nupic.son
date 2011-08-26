74d185e71ecb8480a97d6ab41edd89fdfa11954d
Akeda Bagus <get_email Akeda Bagus>
2011-08-27 02:23:55 +0700

    Generic iconic dashboard and component list.
    It's coincidental that Lennie already created this new file.
    So I just added the base class for iconic dashboard.

--- app/soc/views/dashboard.py
+++ app/soc/views/dashboard.py
@@ -14,22 +14,31 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Base module for participant dashboards.
+"""Module for rendering dashboard and component list.
+
+The classes in this module are intended to serve as base classes for
+iconic dashboard (Dashboard) and component list (Component).
 """
 
 __authors__ = [
+  '"Akeda Bagus" <admin@gedex.web.id>',
   '"Lennard de Rijk" <ljvderijk@gmail.com>',
   ]
 
+import math
+
+from django.utils.translation import ugettext
+
 from soc.views.template import Template
+from soc.views.toggle_button import ToggleButtonTemplate
 
 
 class Component(Template):
-  """Base component for the dashboard.
+  """Base component for the list component.
   """
 
   def __init__(self, request, data):
-    """Initializes the component.
+    """Initializes the list component.
 
     Args:
       request: The HTTPRequest object
@@ -56,3 +65,91 @@ class Component(Template):
     # by default post is not supported
     return False
 
+
+class Dashboard(Template):
+  """Base template to render iconic dashboard.
+
+  This class cannot be instantiated directly. Iconic dashboard must be
+  implemented by derived classes with at least title, name, and subpages set in
+  the context. Dashboard can be nested by supplying subpages_link's context.
+  Dashboard also can be rendered as a list component container by
+  supplying component's context (with list component as its value).
+
+  See soc.modules.gsoc.views.admin.MainDashboard and
+  soc.modules.gsoc.views.dashboard.ComponentsDashboard as an example of iconic
+  dashboard implementation.
+  """
+
+  def __init__(self, request, data, subpages=None):
+    """Initializes the dashboard.
+
+    Args:
+      request: The HTTPRequest object
+      data: The RequestData object
+      subpages: Subpages of current dashboard
+    """
+    self.request = request
+    self.data = data
+    self.subpages = subpages
+
+  def getSubpagesLink(self):
+    """Returns the link to other dashboard that appears
+    on top of the dashboard.
+    """
+    return self.subpages
+
+  def templatePath(self):
+    """Returns the path to the template that should be used in render()
+    """
+    return 'v2/soc/dashboard/base.html'
+
+  def _divideSubPages(self, subpages):
+    """Returns the subpages divided into two columns.
+    """
+    middle_ceil = (len(subpages) + 1) / 2
+
+    return [
+        subpages[:middle_ceil],
+        subpages[middle_ceil:],
+    ]
+
+
+class DashboardUserActions(Template):
+  """Template to render the left side user actions of dashboard.
+  """
+
+  DEF_USE_COLORBOX_HELP_MSG = ugettext(
+      'Choosing Yes will allow you to open link in the dashboard within '
+      'a box without leaving the page.')
+
+  def __init__(self, data):
+    super(DashboardUserActions, self).__init__(data)
+
+    self.toggle_buttons = []
+
+    state = self.data.GET.get('colorbox')
+    use_colorbox = ToggleButtonTemplate(
+        self.data, 'on_off', 'Use colorbox', 'use-colorbox',
+        self.actionURL(),
+        checked=state,
+        help_text=self.DEF_USE_COLORBOX_HELP_MSG,
+        labels = {
+            'checked': 'Yes',
+            'unchecked': 'No'})
+
+    self.toggle_buttons.append(use_colorbox)
+
+  def actionURL(self):
+    """Derived class must implement this method by returning a URL.
+
+    After the button is toggled, the page will be redirected to a given URL.
+    """
+    raise NotImplementedError()
+
+  def context(self):
+    return {
+        'toggle_buttons': self.toggle_buttons
+    }
+
+  def templatePath(self):
+    return 'v2/soc/dashboard/_user_action.html'
