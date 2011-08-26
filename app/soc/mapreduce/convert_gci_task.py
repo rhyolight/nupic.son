8c6257f72034973a94444d019bfe35b074d5cd3e
Madhusudan.C.S <get_email Madhusudan.C.S>
2011-08-27 00:26:46 +0530

    Ensure the entities fetch in conversion belong the GCITask model.

--- app/soc/mapreduce/convert_gci_task.py
+++ app/soc/mapreduce/convert_gci_task.py
@@ -99,7 +99,7 @@ def new_task_for_old(task):
 def process_student_ranking(student_ranking):
   """Replace all the references to the list of old tasks to the new tasks.
   """
-  tasks = db.get(student_ranking.tasks)
+  tasks = GCITask.get(student_ranking.tasks)
   new_tasks = []
   for t in tasks:
     new_t = new_task_for_old(t)
