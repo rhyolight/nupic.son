d01aa683187cf02a036bcdea1c6c6e0414f092db
Leo (Chong Liu) <get_email Leo (Chong Liu)>
2011-09-09 08:02:48 +0100

    Corrected a mistake :-).
    Should have commented GCI TimelineHelper rather than the GSoC one.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -410,8 +410,7 @@ class GSoCDjangoTestCase(DjangoTestCase):
     from soc.modules.gsoc.models.timeline import GSoCTimeline
     from soc.modules.seeder.logic.providers.string import DocumentKeyNameProvider
     from soc.models.org_app_survey import OrgAppSurvey
-    #TODO(Leo): add TimelineHelper for GCI
-    #from tests.timeline_utils import TimelineHelper
+    from tests.timeline_utils import TimelineHelper
     from tests.profile_utils import GSoCProfileHelper
 
     # Initialize instances in the parent first
@@ -458,7 +457,7 @@ class GSoCDjangoTestCase(DjangoTestCase):
 
     self.org = self.createOrg()
 
-    #self.timeline = TimelineHelper(self.gsoc.timeline, self.org_app)
+    self.timeline = TimelineHelper(self.gsoc.timeline, self.org_app)
     self.data = GSoCProfileHelper(self.gsoc, self.dev_test)
 
   def createOrg(self, override={}):
@@ -524,7 +523,8 @@ class GCIDjangoTestCase(DjangoTestCase):
     from soc.modules.gci.models.timeline import GCITimeline
     from soc.modules.seeder.logic.providers.string import DocumentKeyNameProvider
     from soc.models.org_app_survey import OrgAppSurvey
-    from tests.timeline_utils import TimelineHelper
+    #TODO(Leo): add TimelineHelper for GCI
+    #from tests.timeline_utils import TimelineHelper
     from tests.profile_utils import GCIProfileHelper
 
     # Initialize instances in the parent first
@@ -574,7 +574,7 @@ class GCIDjangoTestCase(DjangoTestCase):
 
     self.org = self.createOrg()
 
-    self.timeline = TimelineHelper(self.gci.timeline, self.org_app)
+    #self.timeline = TimelineHelper(self.gci.timeline, self.org_app)
     self.data = GCIProfileHelper(self.gci, self.dev_test)
 
   def createOrg(self, override={}):
