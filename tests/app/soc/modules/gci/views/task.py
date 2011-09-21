672b2d1e8e58d6a84e7143daf7f849cfb4b58029
Lennard de Rijk <get_email Lennard de Rijk>
2011-09-21 14:44:43 +0200

    Implement test for GCITask public view as far as possible.
    Missing timeline helper will probably make this test fail at the moment.

--- tests/app/soc/modules/gci/views/task.py
+++ tests/app/soc/modules/gci/views/task.py
@@ -23,10 +23,11 @@ __authors__ = [
   ]
 
 
-from tests.test_utils import GSoCDjangoTestCase
+from tests.profile_utils import GCIProfileHelper
+from tests.test_utils import GCIDjangoTestCase
 
 
-class TaskViewTest(GSoCDjangoTestCase):
+class TaskViewTest(GCIDjangoTestCase):
   """Tests GCITask public view.
   """
 
@@ -36,9 +37,22 @@ class TaskViewTest(GSoCDjangoTestCase):
   def testBasicTaskView(self):
     """Tests the rendering of the task view.
     """
-    # TODO(ljvderijk): Write this task after discussion on test framework
     # Create a task, status published
+    profile = GCIProfileHelper(self.gci, self.dev_test)
+    task = profile.createOtherUser('mentor@example.com').\
+        createMentorWithTask(self.org)
+
     # Set timeline to taskPubliclyVisible
+    # TODO(ljvderijk): Use timeline helper from GCI.
+    #self.timeline.taskPubliclyVisible()
+
     # Use a non-logged-in request to the page for that task
+    self.profile.clear()
+
+    url = '/gci/task/view/%s/%s' %(task.program.key().name(), task.key().id())
+    response = self.client.get(url)
+
     # Expect a proper response (200)
-    pass
+    self.assertResponseOK(response)
+    self.assertGCITemplatesUsed(response)
+    self.assertTemplateUsed(response, 'v2/modules/gci/task/public.html')
