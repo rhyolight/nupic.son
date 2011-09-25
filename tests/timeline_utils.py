a7da3b39c2d32fe4cd2502c555f01702d6a02a2d
Leo (Chong Liu) <get_email Leo (Chong Liu)>
2011-09-25 21:46:01 +0100

    Added GCITimelineHelper.

--- tests/timeline_utils.py
+++ tests/timeline_utils.py
@@ -206,3 +206,122 @@ class GSoCTimelineHelper(TimelineHelper):
     self.timeline.accepted_students_announced_deadline = past()
     self.timeline.put()
     self.org_appl.put()
+
+
+class GCITimelineHelper(TimelineHelper):
+  """Helper class to aid in setting the GCI timeline.
+  """
+
+  def __init__(self, timeline, org_appl):
+    super(GCITimelineHelper, self).__init__(timeline, org_appl)
+
+  def _empty(self):
+    """Removes all timeline settings.
+
+    Note: does not save changes.
+    """
+    super(GCITimelineHelper, self)._empty()
+    self.timeline.tasks_publicly_visible = None
+    self.timeline.task_claim_deadline = None
+    self.timeline.stop_all_work_deadline = None
+
+  def offSeason(self):
+    """Sets the current period to off season.
+    """
+    super(GCITimelineHelper, self).offSeason()
+    self.timeline.tasks_publicly_visible = past()
+    self.timeline.task_claim_deadline = past()
+    self.timeline.stop_all_work_deadline = past()
+    self.timeline.put()
+    self.org_appl.put()
+
+  def kickoff(self):
+    """Sets the current period to the program kickoff.
+    """
+    super(GCITimelineHelper, self).kickoff()
+    self.timeline.tasks_publicly_visible = future()
+    self.timeline.task_claim_deadline = future()
+    self.timeline.stop_all_work_deadline = future()
+    self.timeline.put()
+    self.org_appl.put()
+
+  def orgSignup(self):
+    """Sets the current period to the organization signup phase.
+    """
+    super(GCITimelineHelper, self).orgSignup()
+    self.timeline.tasks_publicly_visible = future()
+    self.timeline.task_claim_deadline = future()
+    self.timeline.stop_all_work_deadline = future()
+    self.timeline.put()
+    self.org_appl.put()
+
+  def orgsAnnounced(self):
+    """Sets the current period to the organization signup phase.
+    """
+    super(GCITimelineHelper, self).orgsAnnounced()
+    self.timeline.tasks_publicly_visible = future()
+    self.timeline.task_claim_deadline = future()
+    self.timeline.stop_all_work_deadline = future()
+    self.timeline.put()
+    self.org_appl.put()
+
+  def tasksPubliclyVisible(self):
+    """Sets the current period to the tasks publicly visible phase.
+    """
+    self._empty()
+    self.timeline.program_start = past()
+    self.timeline.program_end = future()
+    self.org_appl.survey_start = past()
+    self.org_appl.survey_end = past()
+    self.timeline.accepted_organization_announced_deadline = past()
+    self.timeline.student_signup_start = future()
+    self.timeline.student_signup_end = future()
+    self.timeline.tasks_publicly_visible = past()
+    self.timeline.task_claim_deadline = future()
+    self.timeline.stop_all_work_deadline = future()
+    self.timeline.put()
+    self.org_appl.put()
+
+  def studentSignup(self):
+    """Sets the current period to the student signup phase.
+    """
+    super(GCITimelineHelper, self).studentSignup()
+    self.timeline.tasks_publicly_visible = past()
+    self.timeline.task_claim_deadline = future()
+    self.timeline.stop_all_work_deadline = future()
+    self.timeline.put()
+    self.org_appl.put()
+
+  def taskClaimEnded(self):
+    """Sets the current period to the task claim ended phase.
+    """
+    self._empty()
+    self.timeline.program_start = past()
+    self.timeline.program_end = future()
+    self.org_appl.survey_start = past()
+    self.org_appl.survey_end = past()
+    self.timeline.accepted_organization_announced_deadline = past()
+    self.timeline.student_signup_start = past()
+    self.timeline.student_signup_end = future()
+    self.timeline.tasks_publicly_visible = past()
+    self.timeline.task_claim_deadline = past()
+    self.timeline.stop_all_work_deadline = future()
+    self.timeline.put()
+    self.org_appl.put()
+
+  def pencilDown(self):
+    """Sets the current period to the phase when all work should stop.
+    """
+    self._empty()
+    self.timeline.program_start = past()
+    self.timeline.program_end = future()
+    self.org_appl.survey_start = past()
+    self.org_appl.survey_end = past()
+    self.timeline.accepted_organization_announced_deadline = past()
+    self.timeline.student_signup_start = past()
+    self.timeline.student_signup_end = future()
+    self.timeline.tasks_publicly_visible = past()
+    self.timeline.task_claim_deadline = past()
+    self.timeline.stop_all_work_deadline = past()
+    self.timeline.put()
+    self.org_appl.put()
