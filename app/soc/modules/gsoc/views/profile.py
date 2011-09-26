925c462e00ac96fc6220ba0278edec09dfa4f0ab
Daniel Hans <get_email Daniel Hans>
2011-09-26 18:24:10 +0200

    GSoC Profile view updated to use methods from generic profile view.
    Should be committed in one of my last revisions, but for some reason I skipped this file.

--- app/soc/modules/gsoc/views/profile.py
+++ app/soc/modules/gsoc/views/profile.py
@@ -23,15 +23,13 @@ __authors__ = [
   ]
 
 
-from google.appengine.api import users
-from google.appengine.ext import db
-
 from django.forms import fields
 from django.core.urlresolvers import reverse
 
 from soc.logic import cleaning
 from soc.logic import dicts
 from soc.views import forms
+from soc.views import profile
 from soc.views.helper import url_patterns
 
 from soc.models.user import User
@@ -43,30 +41,6 @@ from soc.modules.gsoc.models.profile import GSoCStudentInfo
 from soc.modules.gsoc.views.base import RequestHandler
 from soc.modules.gsoc.views.base_templates import LoggedInMsg
 from soc.modules.gsoc.views.helper.url_patterns import url
-
-
-class EmptyForm(forms.ModelForm):
-  """Empty form that is always valid.
-  """
-
-  def is_valid(self):
-    return True
-
-
-class UserForm(forms.ModelForm):
-  """Django form for the user profile.
-  """
-
-  def __init__(self, *args, **kwargs):
-    super(UserForm, self).__init__(*args, **kwargs)
-    self.fields['link_id'].label = "Username"
-
-  class Meta:
-    model = User
-    css_prefix = 'user'
-    fields = ['link_id']
-
-  clean_link_id = cleaning.clean_user_not_exist('link_id')
 
 
 class StudentNotificationForm(forms.ModelForm):
@@ -105,21 +79,14 @@ class MentorNotificationForm(forms.ModelForm):
     fields = MENTOR_FIELDS
 
 
-PROFILE_EXCLUDE = [
-    'link_id', 'user', 'scope', 'scope_path', 'status',
-    'agreed_to_tos_on', 'name_on_documents',
+PROFILE_EXCLUDE = profile.PROFILE_EXCLUDE + [
     # notifications
-    'notify_new_requests', 'notify_new_invites',
-    'notify_invite_handled', 'notify_request_handled',
     'notify_new_proposals', 'notify_proposal_updates',
     'notify_public_comments', 'notify_private_comments',
-    # role data
-    'student_info', 'mentor_for', 'org_admin_for',
-    'is_student', 'is_mentor', 'is_org_admin',
 ]
 
 
-class ProfileForm(forms.ModelForm):
+class GSoCProfileForm(profile.ProfileForm):
   """Django form for profile page.
   """
 
@@ -136,61 +103,19 @@ class ProfileForm(forms.ModelForm):
 
     widgets = forms.mergeWidgets(_choiceWidgets, _hiddenWidgets)
 
-  def __init__(self, *args, **kwargs):
-    super(ProfileForm, self).__init__(*args, **kwargs)
-    self.fields['given_name'].group = "2. Contact Info (Private)"
-    self.fields['surname'].group = "2. Contact Info (Private)"
 
-  public_name = fields.CharField(required=True)
-
-  clean_given_name = cleaning.clean_valid_shipping_chars('given_name')
-  clean_surname = cleaning.clean_valid_shipping_chars('surname')
-  clean_email = cleaning.clean_email('email')
-  clean_phone = cleaning.clean_phone_number('phone')
-  clean_res_street = cleaning.clean_valid_shipping_chars('res_street')
-  clean_res_street_extra = cleaning.clean_valid_shipping_chars(
-      'res_street_extra')
-  clean_res_city = cleaning.clean_valid_shipping_chars('res_city')
-  clean_res_state = cleaning.clean_valid_shipping_chars('res_state')
-  clean_res_postalcode = cleaning.clean_valid_shipping_chars(
-      'res_postalcode')
-  clean_ship_name = cleaning.clean_valid_shipping_chars('ship_name')
-  clean_ship_street = cleaning.clean_valid_shipping_chars('ship_street')
-  clean_ship_street_extra = cleaning.clean_valid_shipping_chars(
-      'ship_street_extra')
-  clean_ship_city = cleaning.clean_valid_shipping_chars('ship_city')
-  clean_ship_state = cleaning.clean_valid_shipping_chars('ship_state')
-  clean_ship_postalcode = cleaning.clean_valid_shipping_chars(
-      'ship_postalcode')
-  clean_home_page = cleaning.clean_url('home_page')
-  clean_blog = cleaning.clean_url('blog')
-  clean_photo_url = cleaning.clean_url('photo_url')
-
-  def clean(self):
-    country = self.cleaned_data.get('res_country')
-    state = self.cleaned_data.get('res_state')
-    if country == 'United States' and (not state or len(state) != 2):
-      self._errors['res_state'] = ["Please use a 2-letter state name"]
-
-    country = self.cleaned_data.get('ship_country')
-    state = self.cleaned_data.get('ship_state')
-    if country == 'United States' and (not state or len(state) != 2):
-      self._errors['ship_state'] = ["Please use a 2-letter state name"]
-    return self.cleaned_data
-
-
-class CreateProfileForm(ProfileForm):
+class CreateGSoCProfileForm(GSoCProfileForm):
   """Django edit form for profiles.
   """
 
   class Meta:
-    model = ProfileForm.Meta.model
-    css_prefix = ProfileForm.Meta.css_prefix
+    model = GSoCProfileForm.Meta.model
+    css_prefix = GSoCProfileForm.Meta.css_prefix
     exclude = PROFILE_EXCLUDE
-    widgets = ProfileForm.Meta.widgets
+    widgets = GSoCProfileForm.Meta.widgets
 
   def __init__(self, tos_content, *args, **kwargs):
-    super(CreateProfileForm, self).__init__(*args, **kwargs)
+    super(CreateGSoCProfileForm, self).__init__(*args, **kwargs)
     self.tos_content = tos_content
     self.fields['agreed_to_tos'].widget = forms.TOSWidget(tos_content)
 
@@ -207,7 +132,7 @@ class CreateProfileForm(ProfileForm):
     return value
 
 
-class StudentInfoForm(forms.ModelForm):
+class GSoCStudentInfoForm(forms.ModelForm):
   """Django form for the student profile page.
   """
 
@@ -216,7 +141,8 @@ class StudentInfoForm(forms.ModelForm):
     css_prefix = 'student_info'
     exclude = [
         'school', 'school_type', 'number_of_proposals', 'number_of_projects',
-        'tax_form', 'enrollment_form', 'project_for_orgs'
+        'tax_form', 'enrollment_form', 'project_for_orgs', 'passed_evaluations',
+        'failed_evaluations', 'grade'
     ]
     widgets = forms.choiceWidgets(GSoCStudentInfo,
         ['school_country', 'school_type', 'degree'])
@@ -225,17 +151,9 @@ class StudentInfoForm(forms.ModelForm):
   clean_school_home_page =  cleaning.clean_url('school_home_page')
 
 
-class ProfilePage(RequestHandler):
-  """View for the participant profile.
+class GSoCProfilePage(profile.ProfilePage, RequestHandler):
+  """View for the GSoC participant profile.
   """
-
-  def djangoURLPatterns(self):
-    return [
-        url(r'profile/%s$' % url_patterns.PROGRAM,
-         self, name='edit_gsoc_profile'),
-        url(r'profile/%s$' % url_patterns.CREATE_PROFILE,
-         self, name='create_gsoc_profile'),
-    ]
 
   def checkAccess(self):
     self.check.isLoggedIn()
@@ -254,183 +172,6 @@ class ProfilePage(RequestHandler):
 
   def templatePath(self):
     return 'v2/modules/gsoc/profile/base.html'
-
-  def _getTOSContent(self):
-    """Convenience method to obtain the relevant Terms of Service content
-    for the role in the program.
-    """
-    tos_content = None
-    role = self.data.kwargs.get('role')
-
-    program = self.data.program
-    if role == 'student' and program.student_agreement:
-      tos_content = program.student_agreement.content
-    elif role == 'mentor' and program.mentor_agreement:
-      tos_content = program.mentor_agreement.content
-    elif role == 'org_admin' and program.org_admin_agreement:
-      tos_content = program.org_admin_agreement.content
-
-    return tos_content
-
-  def context(self):
-    role = self.data.kwargs.get('role')
-    if self.data.student_info or role == 'student':
-      student_info_form = StudentInfoForm(self.data.POST or None,
-          instance=self.data.student_info)
-    else:
-      student_info_form = EmptyForm()
-
-    if not role:
-      page_name = 'Edit your Profile'
-    elif role == 'student':
-      page_name = 'Register as a Student'
-    elif role == 'mentor':
-      page_name = 'Register as a Mentor'
-    elif role == 'org_admin':
-      page_name = 'Register as Org Admin'
-
-    tos_content = self._getTOSContent()
-
-    form = EmptyForm if self.data.user else UserForm
-    user_form = form(self.data.POST or None, instance=self.data.user)
-    if self.data.profile:
-      self.data.profile._fix_name()
-      profile_form = ProfileForm(self.data.POST or None,
-                                 instance=self.data.profile)
-    else:
-      profile_form = CreateProfileForm(tos_content, self.data.POST or None)
-    error = user_form.errors or profile_form.errors or student_info_form.errors
-
-    form = self.notificationForm()
-    notification_form = form(self.data.POST or None,
-                             instance=self.data.profile)
-
-    forms = [user_form, profile_form, notification_form, student_info_form]
-
-    context = {
-        'page_name': page_name,
-        'form_top_msg': LoggedInMsg(self.data, apply_role=True),
-        'forms': forms,
-        'error': error,
-    }
-
-    return context
-
-  def validateUser(self, dirty):
-    if self.data.user:
-      user_form = EmptyForm()
-    else:
-      user_form = UserForm(self.data.POST)
-
-    if not user_form.is_valid():
-      return user_form
-
-    if self.data.user:
-      return user_form
-
-    key_name = user_form.cleaned_data['link_id']
-    account = users.get_current_user()
-    user_form.cleaned_data['account'] = account
-    user_form.cleaned_data['user_id'] = account.user_id()
-    self.data.user = user_form.create(commit=False, key_name=key_name)
-    dirty.append(self.data.user)
-    return user_form
-
-  def validateProfile(self, dirty):
-    if self.data.profile:
-      profile_form = ProfileForm(self.data.POST,
-                                 instance=self.data.profile)
-    else:
-      tos_content = self._getTOSContent()
-      profile_form = CreateProfileForm(tos_content, self.data.POST)
-
-    if not profile_form.is_valid():
-      return profile_form, None
-
-    key_name = '%s/%s' % (self.data.program.key().name(),
-                          self.data.user.link_id)
-    profile_form.cleaned_data['link_id'] = self.data.user.link_id
-    profile_form.cleaned_data['user'] = self.data.user
-    profile_form.cleaned_data['scope'] = self.data.program
-
-    if self.data.profile:
-      profile = profile_form.save(commit=False)
-    else:
-      profile = profile_form.create(commit=False, key_name=key_name,
-                                    parent=self.data.user)
-
-    dirty.append(profile)
-
-    return profile_form, profile
-
-  def notificationForm(self):
-    if self.data.student_info or self.data.kwargs.get('role') == 'student':
-      return StudentNotificationForm
-
-    if self.data.is_org_admin or self.data.kwargs.get('role') == 'org_admin':
-      return AdminNotificationForm
-
-    return MentorNotificationForm
-
-  def validateNotifications(self, dirty, profile):
-    if not profile:
-      return EmptyForm(self.data.POST)
-
-    form = self.notificationForm()
-
-    notification_form = form(self.data.POST, instance=profile)
-
-    if not notification_form.is_valid():
-      return notification_form
-
-    notification_form.save(commit=False)
-    if profile not in dirty:
-      dirty.append(profile)
-
-    return notification_form
-
-  def validateStudent(self, dirty, profile):
-    if not (self.data.student_info or
-        self.data.kwargs.get('role') == 'student'):
-      return EmptyForm(self.data.POST)
-
-    student_form = StudentInfoForm(
-        self.data.POST, instance=self.data.student_info)
-
-    if not(profile and student_form.is_valid()):
-      return student_form
-
-    key_name = profile.key().name()
-
-    if self.data.student_info:
-      student_info = student_form.save(commit=False)
-    else:
-      student_info = student_form.create(
-          commit=False, key_name=key_name, parent=profile)
-      profile.is_student = True
-      profile.student_info = student_info
-
-    dirty.append(student_info)
-
-    return student_form
-
-  def validate(self):
-    dirty = []
-    user_form = self.validateUser(dirty)
-    if not user_form.is_valid():
-      return False
-    profile_form, profile = self.validateProfile(dirty)
-
-    notification_form = self.validateNotifications(dirty, profile)
-
-    student_form = self.validateStudent(dirty, profile)
-
-    if (user_form.is_valid() and profile_form.is_valid() and
-        notification_form.is_valid() and student_form.is_valid()):
-      db.run_in_transaction(db.put, dirty)
-      return True
-    else:
-      return False
 
   def jsonContext(self):
     return UNIVERSITIES
@@ -464,3 +205,38 @@ class ProfilePage(RequestHandler):
       link = 'gsoc_org_home'
 
     self.redirect.to(link)
+
+  def _getModulePrefix(self):
+    return 'gsoc'
+
+  def _getEditProfileURLName(self):
+    return 'edit_gsoc_profile'
+
+  def _getCreateProfileURLName(self):
+    return 'create_gsoc_profile'
+
+  def _getEditProfileURLPattern(self):
+    return url_patterns.PROGRAM
+
+  def _getCreateProfileURLPattern(self):
+    return url_patterns.CREATE_PROFILE
+
+  def _getEditProfileForm(self):
+    return GSoCProfileForm(self.data.POST or None, instance=self.data.profile)
+
+  def _getCreateProfileForm(self):
+    tos_content = self._getTOSContent()
+    return CreateGSoCProfileForm(tos_content, self.data.POST or None)
+
+  def _getNotificationForm(self):
+    if self.data.student_info or self.data.kwargs.get('role') == 'student':
+      return StudentNotificationForm
+
+    if self.data.is_org_admin or self.data.kwargs.get('role') == 'org_admin':
+      return AdminNotificationForm
+
+    return MentorNotificationForm
+
+  def _getStudentInfoForm(self):
+    return GSoCStudentInfoForm(self.data.POST or None, 
+        instance=self.data.student_info)
