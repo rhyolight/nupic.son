99d8413b6b48b862ca280eb4bf631299bd427a6c
Madhusudan.C.S <get_email Madhusudan.C.S>
2011-10-03 02:47:31 -0700

    Define methods that renders each type of form field in the GCIBoundField class.
    Use the CSS and structure specific to GCI as delivered by EchoDitto.

--- app/soc/modules/gci/views/forms.py
+++ app/soc/modules/gci/views/forms.py
@@ -26,12 +26,15 @@ from google.appengine.ext import db
 
 from django.core.urlresolvers import reverse
 from django.utils.safestring import mark_safe
+from django.template import defaultfilters
+from django.utils.formats import dateformat
 
 from soc.views import forms
 from soc.views import org_app
 
 
 TEMPLATE_PATH = 'v2/modules/gci/_form.html'
+
 
 class GCIModelForm(forms.ModelForm):
   """Django ModelForm class which uses our implementation of BoundField.
@@ -64,6 +67,9 @@ class GCIBoundField(forms.BoundField):
   """GCI specific BoundField representation.
   """
 
+  def __init__(self, form, field, name):
+    super(GCIBoundField, self).__init__(form, field, name)
+
   def render(self):
     widget = self.field.widget
 
@@ -72,16 +78,28 @@ class GCIBoundField(forms.BoundField):
 
     if isinstance(widget, forms.ReferenceWidget):
       return self.renderReferenceWidget()
+    elif isinstance(widget, forms.TOSWidget):
+      return self.renderTOSWidget()
+    elif isinstance(widget, forms.RadioSelect):
+      return self.renderRadioSelect()
+    elif isinstance(widget, forms.CheckboxSelectMultiple):
+      return self.renderCheckSelectMultiple()
     elif isinstance(widget, forms.TextInput):
       return self.renderTextInput()
-    elif isinstance(widget, forms.Textarea):
-      return self.renderTextArea()
     elif isinstance(widget, forms.DateInput):
-      return self.renderTextInput()
-    elif isinstance(widget, forms.DateTimeInput):
       return self.renderTextInput()
     elif isinstance(widget, forms.Select):
       return self.renderSelect()
+    elif isinstance(widget, forms.CheckboxInput):
+      return self.renderCheckboxInput()
+    elif isinstance(widget, forms.Textarea):
+      return self.renderTextArea()
+    elif isinstance(widget, forms.DateTimeInput):
+      return self.renderTextInput()
+    elif isinstance(widget, forms.HiddenInput):
+      return self.renderHiddenInput()
+    elif isinstance(widget, forms.FileInput):
+      return self.renderFileInput()
 
     return self.NOT_SUPPORTED_MSG_FMT % (
         widget.__class__.__name__)
@@ -89,7 +107,6 @@ class GCIBoundField(forms.BoundField):
   def renderTextInput(self):
     attrs = {
         'id': self.name,
-        'class': 'text',
         }
 
     return mark_safe('%s%s%s%s' % (
@@ -98,6 +115,21 @@ class GCIBoundField(forms.BoundField):
         self._render_error(),
         self._render_note(),
     ))
+
+  def renderCheckboxInput(self):
+    attrs = {
+        'id': self.name,
+        'style': 'opacity: 100;',
+        }
+
+    return mark_safe(
+        '<label>%s%s%s%s</label>%s' % (
+        self.as_widget(attrs=attrs),
+        self.field.label,
+        self._render_is_required(),
+        self._render_error(),
+        self._render_note(),
+        ))
 
   def renderTextArea(self):
     attrs = {
@@ -108,6 +140,40 @@ class GCIBoundField(forms.BoundField):
     return mark_safe('%s%s%s%s' % (
         self._render_label(),
         self.as_widget(attrs=attrs),
+        self._render_error(),  
+        self._render_note(),
+    ))
+
+  def renderReferenceWidget(self):
+    attrs = {
+        'id': self.name,
+        'name': self.name,
+        'type': "hidden",
+        'class': 'text',
+        }
+
+    hidden = self.as_widget(attrs=attrs)
+    original = self.form.initial.get(self.name)
+
+    key = self.form.initial.get(self.name)
+
+    if key:
+      entity = db.get(key)
+      if entity:
+        self.form.initial[self.name] = entity.key().name()
+
+    attrs = {
+        'id': self.name + "-pretty",
+        'name': self.name + "-pretty",
+        'class': 'text',
+        }
+    pretty = self.as_widget(attrs=attrs)
+    self.form.initial[self.name] = original
+
+    return mark_safe('%s%s%s%s%s' % (
+        self._render_label(),
+        pretty,
+        hidden,
         self._render_error(),
         self._render_note(),
     ))
@@ -121,6 +187,84 @@ class GCIBoundField(forms.BoundField):
     return mark_safe('%s%s%s%s' % (
         self.as_widget(attrs=attrs),
         self._render_is_required(),
+        self._render_error(),
+        self._render_note(),
+    ))
+
+  def renderTOSWidget(self):
+    checkbox_attrs = {
+        'id': self.name,
+        'style': 'opacity: 100;',
+        }
+
+    return mark_safe(
+        '<label>%s%s%s%s</label>%s' % (
+        self.as_widget(attrs=checkbox_attrs),
+        self.field.label,
+        self._render_is_required(),
+        self._render_error(),
+        self._render_note(),
+        ))
+
+  def renderHiddenInput(self):
+    attrs = {
+        'id': self.name,
+        'name': self.name,
+        'type': 'hidden',
+        'value': self.field.initial or '',
+        }
+    return self.as_widget(attrs=attrs)
+
+  def renderFileInput(self):
+    attrs = {
+        'id': self.name,
+        }
+
+    current_file_fmt = """
+        <br/>
+        <p>
+        File: <a href="%(link)s">%(name)s</a><br/>
+        Size: %(size)s <br/>
+        Uploaded on: %(uploaded)s UTC)
+        <p>
+    """
+
+    current_file = current_file_fmt % {
+        'name': self.field._file.filename,
+        'link': self.field._link,
+        'size': defaultfilters.filesizeformat(self.field._file.size),
+        'uploaded': dateformat.format(
+              self.field._file.creation, 'M jS Y, h:i:sA'),
+    } if self.field._file else ""
+
+    return mark_safe('%s%s%s%s%s' % (
+        self._render_label(),
+        self.as_widget(attrs=attrs),
+        self._render_error(),
+        self._render_note(),
+        current_file,
+    ))
+
+  def renderRadioSelect(self):
+    attrs = {
+        'id': self.name,
+        }
+
+    return mark_safe('%s%s%s%s' % (
+        self._render_label(),
+        self.as_widget(attrs=attrs),
+        self._render_error(),
+        self._render_note(),
+    ))
+
+  def renderCheckSelectMultiple(self):
+    attrs = {
+        'id': self.name,
+        }
+
+    return mark_safe('%s%s%s%s' % (
+        self._render_label(),
+        self.as_widget(attrs=attrs),
         self._render_error(),
         self._render_note(),
     ))
@@ -155,16 +299,24 @@ class GCIBoundField(forms.BoundField):
     return '<em>*</em>' if self.field.required else ''
 
   def _render_error(self):
-    return ''
+    if not self.errors:
+      return ''
+
+    return '<span class="form-row-error-msg">%s</span>' % (
+        self.errors[0])
 
   def _render_note(self, note=None):
     return '<span class="note">%s</span>' % (
         note if note else self.help_text)
 
   def div_class(self):
-    div_class = 'form-row'
+    prefix = getattr(self.form.Meta, 'css_prefix', None)
+    name = prefix + '-' + self.name if prefix else self.name
+
+    widget_div_class = self.field.widget.attrs.get('div_class', None)
+    if widget_div_class:
+      name = '%s %s' % (widget_div_class, name)
 
     if self.errors:
-      div_class += ' error'
-
-    return div_class
+      name += ' error'
+    return name
