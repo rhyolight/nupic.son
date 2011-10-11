ec2a4466e21ad864b0de8adcb42a2d9fc6eda55e
Daniel Hans <get_email Daniel Hans>
2011-10-11 19:09:28 +0200

    renderTextInput does not display two error messages, but one instead.

--- app/soc/modules/gci/views/forms.py
+++ app/soc/modules/gci/views/forms.py
@@ -195,10 +195,9 @@ class GCIBoundField(forms.BoundField):
         'id': self.name,
         }
 
-    return mark_safe('%s%s%s%s' % (
+    return mark_safe('%s%s%s' % (
         self._render_label(),
         self.as_widget(attrs=attrs),
-        self._render_error(),
         self._render_note(),
     ))
 
