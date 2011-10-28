67125b69689a23ef4bfd0e607323ace99a4ffcfc
Lennard de Rijk <get_email Lennard de Rijk>
2011-10-27 17:51:21 -0700

    DELETE_EXPIRATION for GCIWorkSubmissions set to 10 minutes.
    As suggested by Carol.

--- app/soc/modules/gci/logic/task.py
+++ app/soc/modules/gci/logic/task.py
@@ -97,8 +97,7 @@ DEF_UNCLAIMED_MSG = ugettext(
     'The claim on this task has been removed, someone else can claim it now.')
 
 
-# TODO(ljvderijk): Recalibrate
-DELETE_EXPIRATION = datetime.timedelta(days=10)
+DELETE_EXPIRATION = datetime.timedelta(minutes=10)
 
 # TODO(ljvderijk): Add basic subscribers when task is created
 
