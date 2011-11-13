bd3d432e8963a3840cc5668faa4da6ee52a434b9
Madhusudan.C.S <get_email Madhusudan.C.S>
2011-11-13 03:49:51 -0800

    Make the access violation message for task creation/editing less clumsy.

--- app/soc/modules/gci/views/helper/access_checker.py
+++ app/soc/modules/gci/views/helper/access_checker.py
@@ -50,12 +50,10 @@ DEF_ALL_WORK_STOPPED = ugettext(
     'submit work or make any changes to existing tasks.')
 
 DEF_NO_TASK_CREATE_PRIV = ugettext(
-    'You do not have sufficient privileges to create a new task for '
-    'the organization %s.' )
+    'You do not have sufficient privileges to create a new task for %s.' )
 
 DEF_NO_TASK_EDIT_PRIV = ugettext(
-    'You do not have sufficient privileges to edit a new task for '
-    'the organization %s.' )
+    'You do not have sufficient privileges to edit a new task for %s.' )
 
 DEF_NO_PREV_ORG_MEMBER = ugettext(
     'To apply as an organization for GCI you must have been a member of an '
