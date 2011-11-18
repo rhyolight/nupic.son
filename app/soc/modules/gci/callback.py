8cb90a9d0ca76b1a0604768c967c26b29a892f21
Daniel Hans <get_email Daniel Hans>
2011-11-18 18:40:50 +0100

    ListUserInvitesPage added to callback for GCI.

--- app/soc/modules/gci/callback.py
+++ app/soc/modules/gci/callback.py
@@ -76,6 +76,7 @@ class Callback(object):
     self.views.append(invite.InvitePage())
     self.views.append(invite.ManageInvite())
     self.views.append(invite.RespondInvite())
+    self.views.append(invite.ListUserInvitesPage())
     self.views.append(org_app.GCIOrgAppEditPage())
     self.views.append(org_profile.OrgProfilePage())
     self.views.append(org_app.GCIOrgAppPreviewPage())
