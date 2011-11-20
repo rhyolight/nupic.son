41951fbae9df74f5591b9cfc555279a1af31d234
Praveen Kumar <get_email Praveen Kumar>
2011-11-20 19:32:04 +0530

    Add OpenTasksList to org homepage view.

--- app/soc/modules/gci/views/org_home.py
+++ app/soc/modules/gci/views/org_home.py
@@ -14,10 +14,12 @@
 
 """Module containing the Org Homepage view.
 """
-
+from soc.logic.exceptions import AccessViolation
+from soc.views.helper import lists
 from soc.views.helper import url_patterns
 from soc.views.template import Template
 
+from soc.modules.gci.models.task import GCITask
 from soc.modules.gci.views.base import RequestHandler
 from soc.modules.gci.views.helper.url_patterns import url
 
@@ -53,6 +55,58 @@ class ContactUs(Template):
     return "v2/modules/gci/org_home/_contact_us.html"
 
 
+class OpenTasksList(Template):
+  """List to display all the open tasks for the current organization.
+  """
+  def __init__(self, request, data):
+    self.request = request
+    self.data = data
+    list_config = lists.ListConfiguration()
+    
+    list_config.addSimpleColumn('title', 'Title')
+    #list_config.addColumn('difficulty', 'Difficulty', lambda entity,
+    #                      *args: entity.taskDifficulty())
+    list_config.addColumn('task_type', 'Type', lambda entity,
+                            *args: entity.taskType())
+    list_config.addColumn('arbit_tag', 'Tags', lambda entity,
+                          *args: entity.taskArbitTag())
+    list_config.addColumn('time_to_complete', 'Time to complete',
+                          lambda entity, *args: entity.taskTimeToComplete())
+    
+    #list_config.setRowAction(
+    #    lambda e, *args: data.redirect.id(e.key.id()).urlOf('gci_view_task'))
+
+    #raise Exception
+    self.list_config = list_config
+  
+  def context(self):
+    description = 'List of all Open tasks.'
+    list = lists.ListConfigurationResponse(
+        self.data, self.list_config, 0, description)
+    return {
+        'lists': [list],
+    }
+    
+  
+  def getListData(self):
+    idx = lists.getListIndex(self.request)
+    if idx == 0:
+      q = GCITask.all()
+      #q.filter('program', self.data.program)
+      #q.filter('org', self.data.organization)
+      #q.filter('status IN', ['Open', 'Reopened'])
+      starter = lists.keyStarter
+
+      response_builder = lists.RawQueryContentResponseBuilder(
+          self.request, self.list_config, q, starter)
+      return response_builder.build()
+    else:
+      return None
+  
+  def templatePath(self):
+    return 'v2/modules/gci/org_home/_open_tasks.html'
+
+
 class OrgHomepage(RequestHandler):
   """Encapsulates all the methods required to render the org homepage.
   """
@@ -68,11 +122,20 @@ class OrgHomepage(RequestHandler):
   def checkAccess(self):
     pass
   
+  def jsonContext(self):
+    list_content = OpenTasksList(self.request, self.data).getListData()
+
+    if not list_content:
+      raise AccessViolation(
+          'You do not have access to this data')
+    return list_content.content()
+  
   def context(self):
     context = {
         'page_name': '%s - Home page' % (self.data.organization.name),
         'about_us': AboutUs(self.data),
         'contact_us': ContactUs(self.data),
+        'open_tasks_list': OpenTasksList(self.request, self.data),
     }
     
     return context
