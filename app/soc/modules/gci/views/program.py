3bc40ca14db87baefdc1828dfcb8b3602576b7d8
Madhusudan.C.S <get_email Madhusudan.C.S>
2011-11-19 17:32:13 -0800

    As a first step of refactoring move the tags processing code from the view to the form cleaner.
    This occured to me when there was an exception whenever the form errored
    out because of invalid "score value" to difficulty tag and tried to raise
    the ValidationError exception. This exception is caught and form errors
    are set only if this exception occurs within the form validation chain.
    Outside that chain, there is simply no code to catch that exception and
    it bubbles all the way upto the Application level and thus causes an HTTP
    500.
    
    It is fixed now. But this tag code still needs one more round of refactoring
    to move the logic specific code to tag logic.

--- app/soc/modules/gci/views/program.py
+++ app/soc/modules/gci/views/program.py
@@ -78,6 +78,124 @@ class ProgramForm(GCIModelForm):
     exclude = ['link_id', 'scope', 'scope_path', 'timeline', 'home',
                'slots_allocation', 'task_difficulties', 'task_types']
 
+  def clean_difficulty(self):
+    """Retrieve and validate task difficulty tags and its value from the form.
+    """
+    program = self.request_data.program
+    to_put = []
+
+    task_difficulties = self.request_data.POST.getlist('task_difficulty_name')
+    task_difficulties_values = self.request_data.POST.getlist(
+        'task_difficulty_value')
+
+    try:
+      difficulties_dict = dict(
+          (name, {'value': int(value), 'order': i}) for i, (name, value)
+               in enumerate(zip(task_difficulties, task_difficulties_values)))
+    except ValueError:
+        raise forms.ValidationError(
+          ugettext('The value for the tag must be an integer.'))
+
+    difficulty_tags = tags_logic.getTagsForProgram(TaskDifficultyTag, program)
+
+    for tag in difficulty_tags:
+      tag_name = tag.tag
+
+      if tag_name not in difficulties_dict:
+        continue
+
+      new_value = difficulties_dict.get(tag_name).get('value')
+      new_order = difficulties_dict.get(tag_name).get('order')
+
+      difficulties_dict.pop(tag_name)
+
+      if new_value == tag.value and new_order == tag.order:
+        continue
+
+      tag.value = new_value
+      tag.order = new_order
+      to_put.append(tag)
+
+    scope_path = program.key().id_or_name()
+
+    for name, props in difficulties_dict.items():
+      to_put.append(TaskDifficultyTag(
+          key_name=TaskDifficultyTag._key_name(scope_path, name),
+          scope=program, tag=name, value=props.get('value'),
+          order=props.get('order')))
+
+    self.cleaned_data['task_difficulty_entities'] = to_put
+
+  def clean_task_types(self):
+    """Retrieve and validate task type tags and its value from the form.
+    """
+    program = self.request_data.program
+    to_put = []
+
+    task_types = self.request_data.POST.getlist('task_type_name')
+
+    types_dict = dict((name, i) for i, name in enumerate(task_types))
+
+    type_tags = tags_logic.getTagsForProgram(TaskTypeTag, program)
+
+    for tag in type_tags:
+      tag_name = tag.tag
+
+      if tag_name not in types_dict:
+        continue
+
+      new_order = types_dict.pop(tag_name)
+
+      if new_order == tag.order:
+        continue
+
+      tag.order = new_order
+      to_put.append(tag)
+
+    scope_path = program.key().id_or_name()
+
+    for name, order in types_dict.items():
+      to_put.append(TaskTypeTag(
+          key_name=TaskTypeTag._key_name(scope_path, name),
+          scope=program, tag=name, order=order))
+
+    self.cleaned_data['task_type_entities'] = to_put
+
+  def clean(self):
+    """Cleans the data input by the user as a response to the form.
+
+    Here we populate the tags that are dynamically added using Javascript
+    in the form and do some validation on the tags. We cannot just assume
+    that the clean_* methods are called on difficulty and task_types because
+    they are not defined fields in the Django's form. In fact they are
+    excluded from the actual model form. So we will have to explicitly call
+    their cleaners.
+    """
+    super(ProgramForm, self).clean()
+
+    self.clean_difficulty()
+    self.clean_task_types()
+
+    return self.cleaned_data
+
+  def save(self):
+    """Save the form along with the tags.
+    """
+    super(ProgramForm, self).save()
+
+    # Note the dictionary keys for tags, i.e. task_difficulty_entities and
+    # task_type_entities is not the same as corresponding property names in
+    # the GCITask model so these values won't be stored when we call
+    # form.save() and hence we extend it here.
+    #
+    # Also purposefully make the key names different because we do not want
+    # the ModelForm code to save these entities since the corresponding
+    # properties are not regular Appengine property types. So we are happy
+    # handling them separately!
+    db.put(
+        self.cleaned_data['task_difficulty_entities'] +
+        self.cleaned_data['task_type_entities'])
+
 
 class ProgramPage(RequestHandler):
   """View for the program profile.
@@ -109,8 +227,7 @@ class ProgramPage(RequestHandler):
     return 'v2/modules/gci/program/base.html'
 
   def context(self):
-    scope_path = self.data.program.key().id_or_name()
-    program_form = ProgramForm(scope_path, self.data.POST or None,
+    program_form = ProgramForm(self.data, self.data.POST or None,
                                instance=self.data.program)
     return {
         'page_name': 'Edit program settings',
@@ -119,79 +236,13 @@ class ProgramPage(RequestHandler):
     }
 
   def validate(self):
-    program = self.data.program
-    to_put = []
-
-    scope_path = program.key().id_or_name()
-    program_form = ProgramForm(scope_path, self.data.POST, instance=program)
+    program_form = ProgramForm(self.data, self.data.POST,
+                               instance=self.data.program)
 
     if not program_form.is_valid():
       return False
 
-    task_difficulties = self.data.POST.getlist('task_difficulty_name')
-    task_difficulties_values = self.data.POST.getlist(
-        'task_difficulty_value')
-
-    try:
-      difficulties_dict = dict(
-          (name, {'value': int(value), 'order': i}) for i, (name, value)
-               in enumerate(zip(task_difficulties, task_difficulties_values)))
-    except ValueError:
-        raise django_forms.ValidationError(
-          ugettext('The value %s for the tag must be an integer.' % (
-              value)))
-
-    difficulty_tags = tags_logic.getTagsForProgram(TaskDifficultyTag, program)
-
-    for tag in difficulty_tags:
-      tag_name = tag.tag
-
-      if tag_name not in difficulties_dict:
-        continue
-
-      new_value = difficulties_dict.get(tag_name).get('value')
-      new_order = difficulties_dict.get(tag_name).get('order')
-
-      difficulties_dict.pop(tag_name)
-
-      if new_value == tag.value and new_order == tag.order:
-        continue
-
-      tag.value = new_value
-      tag.order = new_order
-      to_put.append(tag)
-
-    for name, props in difficulties_dict.items():
-      to_put.append(TaskDifficultyTag(
-          scope=program, tag=name, value=props.get('value'),
-          order=props.get('order')))
-
-
-    task_types = self.data.POST.getlist('task_type_name')
-
-    types_dict = dict((name, i) for i, name in enumerate(task_types))
-
-    type_tags = tags_logic.getTagsForProgram(TaskTypeTag, program)
-
-    for tag in type_tags:
-      tag_name = tag.tag
-
-      if tag_name not in types_dict:
-        continue
-
-      new_order = types_dict.pop(tag_name)
-
-      if new_order == tag.order:
-        continue
-
-      tag.order = new_order
-      to_put.append(tag)
-
-    for name, order in types_dict.items():
-      to_put.append(TaskTypeTag(scope=program, tag=name, order=order))
-
     program_form.save()
-    db.put(to_put)
 
     return True
 
