a7394e2e711738c9c48922103ad199df91091555
landor <get_email landor>
2011-12-13 14:08:50 -0500

    accessibility page mockup

--- mockups/code-in/master/index.php
+++ mockups/code-in/master/index.php
@@ -11,6 +11,7 @@
             <ul>
                 <li><a href="../home/">Homepage</a></li>
                 <li><a href="../accepted-organizations/">Accepted Organizations</a></li>
+                <li><a href="../accessibility/">Accessibility</a></li>
                 <li><a href="../dashboard-admin/">Admin Dashboard</a></li>
                 <li><a href="../dashboard-mentor/">Mentor Dashboard</a></li>
                 <li><a href="../dashboard-student/">Student Dashboard</a></li>
