8375c4fdd55fa70fd34ad8e49e33009ad55f6cc9
Madhusudan.C.S <get_email Madhusudan.C.S>
2011-12-23 05:28:09 +0530

    Implement a view and the required list to display organization specific information to admins/hosts.
    This is different from the accepted organizations view in that this page is
    accessible only by program admins/hosts. This page may contain additional
    confidential information like email IDs of the org admins which are not
    public.

--- app/soc/modules/gci/views/accepted_orgs.py
+++ app/soc/modules/gci/views/accepted_orgs.py
@@ -26,6 +26,7 @@ from soc.views.template import Template
 from soc.views.helper import url as url_helper
 from soc.views.helper import url_patterns
 
+from soc.modules.gci.logic import profile as profile_logic
 from soc.modules.gci.models.organization import GCIOrganization
 from soc.modules.gci.views.base import RequestHandler
 #from soc.modules.gci.views.base_templates import ProgramSelect
@@ -116,4 +117,97 @@ class AcceptedOrgsPage(RequestHandler):
         'page_name': "Accepted organizations for %s" % self.data.program.name,
         'accepted_orgs_list': AcceptedOrgsList(self.request, self.data),
         #'program_select': ProgramSelect(self.data, 'gci_accepted_orgs'),
+    }
+
+
+class AcceptedOrgsAdminList(Template):
+  """Template for list of accepted organizations for admins.
+  """
+
+  def __init__(self, request, data):
+    self.request = request
+    self.data = data
+    r = data.redirect
+
+    list_config = lists.ListConfiguration()
+    list_config.addColumn('name', 'Name',
+        lambda e, *args: e.name.strip())
+    list_config.addSimpleColumn('link_id', 'Link ID', hidden=True)
+    list_config.setRowAction(
+        lambda e, *args: r.organization(e).urlOf(url_names.GCI_ORG_HOME))
+    list_config.setDefaultPagination(False)
+    list_config.setDefaultSort('name')
+    list_config.addColumn(
+      'org_admins', 'Org Admins',
+      lambda e, org_admins, *args: ", ".join(
+          ["%s <%s>" % (o.name(), o.email) for o in org_admins[e.key()]]),
+      hidden=True)
+
+    self._list_config = list_config
+
+  def context(self):
+    description = 'List of organizations accepted into %s' % (
+            self.data.program.name)
+
+    list = lists.ListConfigurationResponse(
+        self.data, self._list_config, 0, description)
+
+    return {
+        'lists': [list],
+    }
+
+  def getListData(self):
+    idx = lists.getListIndex(self.request)
+    if idx == 0:
+      q = GCIOrganization.all()
+      q.filter('scope', self.data.program)
+      q.filter('status IN', ['new', 'active', 'inactive'])
+
+      starter = lists.keyStarter
+
+      def prefetcher(entities):
+        prefetched_dict = {}
+        for ent in entities:
+          prefetched_dict[ent.key()] = profile_logic.orgAdminsForOrg(ent)
+
+        return [prefetched_dict], {}
+
+      response_builder = lists.RawQueryContentResponseBuilder(
+          self.request, self._list_config, q, starter, prefetcher=prefetcher)
+      return response_builder.build()
+    else:
+      return None
+
+  def templatePath(self):
+    return "v2/modules/gci/accepted_orgs/_project_list.html"
+
+
+class AcceptedOrgsAdminPage(RequestHandler):
+  """View for the accepted organizations page for admin with additional info.
+  """
+
+  def templatePath(self):
+    return 'v2/modules/gci/accepted_orgs/base.html'
+
+  def djangoURLPatterns(self):
+    return [
+        url(r'admin/accepted_orgs/%s$' % url_patterns.PROGRAM, self,
+            name='gci_admin_accepted_orgs'),
+    ]
+
+  def checkAccess(self):
+    self.check.isHost()
+
+  def jsonContext(self):
+    list_content = AcceptedOrgsAdminList(self.request, self.data).getListData()
+
+    if not list_content:
+      raise AccessViolation(
+          'You do not have access to this data')
+    return list_content.content()
+
+  def context(self):
+    return {
+        'page_name': "Accepted organizations for %s" % self.data.program.name,
+        'accepted_orgs_list': AcceptedOrgsAdminList(self.request, self.data),
     }
