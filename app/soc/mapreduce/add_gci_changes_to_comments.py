285a347afe184fd552323b5c1a60c4fc20205185
Madhusudan.C.S <get_email Madhusudan.C.S>
2011-12-27 20:37:45 +0530

    Add a detailed module level docstring explaining why this mapreduce is needed.

--- app/soc/mapreduce/add_gci_changes_to_comments.py
+++ app/soc/mapreduce/add_gci_changes_to_comments.py
@@ -17,6 +17,27 @@
 
 """MapReduce to populate the changes property to the action comments for
 GCI 2011 comments.
+
+Initially when GCI 2011 started we did not have the concept of rendering
+Changelog for the GCITasks. So it was not really necessary to differentiate
+between the actual comments posted by the users and the comments that were
+recorded as part of some action performed by users. However for rendering
+the changelog this becomes necessary.
+
+The initial thought was to separate the actions out to a separate GCIAction
+model, but then merge-sorting the GCIComment and GCIAction lists while
+rendering the changelog would be an additional overhead. For this reason
+and to be consistent with what we did during GCI 2010, we populate the
+changes property in the GCIComment model itself. So that way, the comment
+entities with a value for the changes property are actions and others are
+regular comments.
+
+So all this mapreduce is trying to do is inferring the changes for the
+comments from the comment title. We may have problems if some mentor or
+student has used the same title as one of these action-comments to their
+regular comments. But we are assuming that effect is not disastrous. If
+some one notices such a thing has happened, we can easily revert it by
+unsetting the changes property.
 """
 
 
