b9fd22ed54977a54afd9e9f52b0e4556e91621fe
Praveen Kumar <get_email Praveen Kumar>
2012-01-09 19:47:15 +0530

    Fix seed_db.

--- app/soc/models/seed_db.py
+++ app/soc/models/seed_db.py
@@ -407,6 +407,7 @@ def seed(request, *args, **kwargs):
       'school_country': 'United States',
       'school_name': 'Test School',
       'school_home_page': 'http://www.example.com',
+      'program': gsoc2009,
   }
   student_info = GSoCStudentInfo(**student_info_properties)
   student_info.put()
