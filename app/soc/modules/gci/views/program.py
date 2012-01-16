a1039d5f591dc42751d88e044193b51d340a142a
Daniel Hans <get_email Daniel Hans>
2012-01-16 15:56:18 -0800

    Difficulty removed from GCIProgram edit. Is is hardcoded now anyway.

--- app/soc/modules/gci/views/program.py
+++ app/soc/modules/gci/views/program.py
@@ -30,7 +30,6 @@ from soc.views import forms
 from soc.views.helper import url_patterns
 
 from soc.modules.gci.models.program import GCIProgram
-from soc.modules.gci.models.task import TaskDifficultyTag
 from soc.modules.gci.models.timeline import GCITimeline
 from soc.modules.gci.views.base import RequestHandler
 from soc.modules.gci.views.forms import GCIModelForm
@@ -57,102 +56,23 @@ class ProgramForm(GCIModelForm):
     super(ProgramForm, self).__init__(*args, **kwargs)
 
     if self.instance:
-      difficulty_tags = tags_logic.getTagsForProgram(
-          TaskDifficultyTag, self.instance, order=['order'])
-      self.task_difficulties_json = json.dumps(
-          [[t.tag, t.value] for t in difficulty_tags])
-
       self.task_types_json = json.dumps(self.instance.task_types)
 
   class Meta:
     css_prefix = 'program_form'
     model = GCIProgram
     exclude = ['link_id', 'scope', 'scope_path', 'timeline', 'home',
-               'slots_allocation', 'task_difficulties', 'task_types']
-
-  def clean_difficulty(self):
-    """Retrieve and validate task difficulty tags and its value from the form.
-    """
-    program = self.request_data.program
-    to_put = []
-
-    task_difficulties = self.request_data.POST.getlist('task_difficulty_name')
-    task_difficulties_values = self.request_data.POST.getlist(
-        'task_difficulty_value')
-
-    try:
-      difficulties_dict = dict(
-          (name, {'value': int(value), 'order': i}) for i, (name, value)
-               in enumerate(zip(task_difficulties, task_difficulties_values)))
-    except ValueError:
-        raise forms.ValidationError(
-          ugettext('The value for the tag must be an integer.'))
-
-    difficulty_tags = tags_logic.getTagsForProgram(TaskDifficultyTag, program)
-
-    for tag in difficulty_tags:
-      tag_name = tag.tag
-
-      if tag_name not in difficulties_dict:
-        continue
-
-      new_value = difficulties_dict.get(tag_name).get('value')
-      new_order = difficulties_dict.get(tag_name).get('order')
-
-      difficulties_dict.pop(tag_name)
-
-      if new_value == tag.value and new_order == tag.order:
-        continue
-
-      tag.value = new_value
-      tag.order = new_order
-      to_put.append(tag)
-
-    scope_path = program.key().id_or_name()
-
-    for name, props in difficulties_dict.items():
-      to_put.append(TaskDifficultyTag(
-          key_name=TaskDifficultyTag._key_name(scope_path, name),
-          scope=program, tag=name, value=props.get('value'),
-          order=props.get('order')))
-
-    self.cleaned_data['task_difficulty_entities'] = to_put
+               'slots_allocation', 'task_types']
 
   def clean(self):
     """Cleans the data input by the user as a response to the form.
-
-    Here we populate the tags that are dynamically added using Javascript
-    in the form and do some validation on the tags. We cannot just assume
-    that the clean_* methods are called on difficulty and task_types because
-    they are not defined fields in the Django's form. In fact they are
-    excluded from the actual model form. So we will have to explicitly call
-    their cleaners.
     """
     super(ProgramForm, self).clean()
-
-    self.clean_difficulty()
 
     self.cleaned_data['task_types'] = self.request_data.POST.getlist(
         'task_type_name')
 
     return self.cleaned_data
-
-  def save(self):
-    """Save the form along with the tags.
-    """
-    super(ProgramForm, self).save()
-
-    # Note the dictionary keys for tags, i.e. task_difficulty_entities and
-    # task_type_entities is not the same as corresponding property names in
-    # the GCITask model so these values won't be stored when we call
-    # form.save() and hence we extend it here.
-    #
-    # Also purposefully make the key names different because we do not want
-    # the ModelForm code to save these entities since the corresponding
-    # properties are not regular Appengine property types. So we are happy
-    # handling them separately!
-    db.put(
-        self.cleaned_data['task_difficulty_entities'])
 
 
 class ProgramPage(RequestHandler):
