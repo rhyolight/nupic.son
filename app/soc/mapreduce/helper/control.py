661fb5e2576899f2fbcb161a0ac73f0e06144d07
Madhusudan.C.S <get_email Madhusudan.C.S>
2012-02-26 20:47:24 -0800

    Implement a mapreduce helper to programmatically start mapreduce jobs.
    This is a very simple wrapper over the mapreduce library's mapreduce.control. The
    only extra work that this mapper does is to read the definition from the mapreduce.yaml
    and use those arguments whereever relevant instead of hard coding them in our code base.
    This has the advantage that the mapreduce jobs can now either be started from our
    program or still forcefully run from http://<DOMAN>/mapreduce/status page.

---
+++ app/soc/mapreduce/helper/control.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python2.5
+#
+# Copyright 2011 the Melange authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+"""This module contains the helpers to start a mapper job given the name in
+mapreduce.yaml for the job.
+"""
+
+
+from mapreduce import control
+from mapreduce import status
+
+
+def start_map(name, params={}, eta=None, countdown=None):
+  for config in status.MapReduceYaml.to_dict(status.get_mapreduce_yaml()):
+    if config.get('name') == name:
+      break
+
+  # Add the mapreduce specific parameters to the params dictionary
+  config['mapper_params'].update(params)
+
+  control.start_map(config['name'], config['mapper_handler'],
+                    config['mapper_input_reader'], config['mapper_params'],
+                    eta=eta,
+                    countdown=countdown)
