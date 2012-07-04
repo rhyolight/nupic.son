5ea9fcfc94bd339d790b54d224c21b82f61f0d36
Mario Ferraro <get_email Mario Ferraro>
2012-07-04 08:52:29 +0100

    Hide the map if lat and lon are not set.
    Also remove unnecessary global variables and functions.

--- app/soc/content/js/templates/modules/gsoc/profile_readonly.js
+++ app/soc/content/js/templates/modules/gsoc/profile_readonly.js
@@ -18,37 +18,21 @@
 
 melange.templates.inherit(
   function (_self, context) {
-    var css_prefix = "profile_show";
-  
     // Create global variables
-    var map;
-    var marker;
-  
-    // The following strings can be customized to reflect ids in the page.
-    // You can also add or remove fields used for GMap Geocoding in 
-    // the JSON address object
-  
-    var current_lat = 0;
-    var current_lng = 0;
-  
-    // Two different levels for zoom: Starting one and an inner that 
-    // is used when showing the map if lat and lon page fields are set
-    var world_zoom = 1;
-    var address_zoom = 13;
-  
-    var map_div = "profile_map";
+    var css_prefix = "profile_show";
   
     // Id of the element which the map will be appended after.
     var append_to = "#gsoc_" + css_prefix + "-contact-info-private";
-  
+
+    var map_div = "profile_map";
+
     var field_lat = "#latitude";
     var field_lng = "#longitude";
   
-    // Read lat and lng fields and store them
-    function readLatLngFields() {
-      current_lat = jQuery(field_lat).text();
-      current_lng = jQuery(field_lng).text();
-    }
+    var lat = 0;
+    var lng = 0;
+
+    var map_zoom = 13;
   
     // Public function to load the map
     function map_load() {
@@ -58,35 +42,27 @@ melange.templates.inherit(
         panControl: true,
         zoomControl: true
       };
-      var starting_point;
-      var zoom_selected = world_zoom;
-      var show_marker = false;
   
       // Create the map and pass initialization
-      map = new google.maps.Map(jQuery("#" + map_div)[0], init_map_options);
+      var map = new google.maps.Map(jQuery("#" + map_div)[0], init_map_options);
   
-      // If lat and lng fields are not empty then
-      // do not show the marker
-      if (jQuery(field_lat).text() !== "" && jQuery(field_lng).text() !== "") {
-        readLatLngFields();
-        zoom_selected = address_zoom;
-        show_marker = true;
-      }
-  
-      // Set map center, marker coords if present
-      starting_point = new google.maps.LatLng(current_lat, current_lng);
-      map.setZoom(zoom_selected);
-      map.setCenter(starting_point);
-      marker = new google.maps.Marker({position: starting_point, draggable: false});
-      if (show_marker) {
-        marker.setMap(map);
-      }
+      // Set map center and marker coords
+      var marker_position = new google.maps.LatLng(lat, lng);
+      map.setZoom(map_zoom);
+      map.setCenter(marker_position);
+      marker = new google.maps.Marker({position: marker_position, draggable: false});
+      marker.setMap(map);
     }
   
     jQuery(
       function () {
-        jQuery(append_to).append("<div id='" + map_div + "' style=\"width: 100%\"></div>");
-        melange.loadGoogleApi("maps", "3", {other_params: "sensor=false"}, map_load);
+        lat = jQuery(field_lat).text();
+        lng = jQuery(field_lng).text();
+        // If lat and lng fields are not set, then don't show the map at all
+        if (lat !== "" && lng !== "") {
+          jQuery(append_to).append("<div id='" + map_div + "' style=\"width: 100%\"></div>");
+          melange.loadGoogleApi("maps", "3", {other_params: "sensor=false"}, map_load);
+        }
       }
     );
   }
