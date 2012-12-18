313694238713f056407c153af8cb66b31040a245
Madhusudan.C.S <get_email Madhusudan.C.S>
2012-12-18 19:38:42 +0530

    Appengine does not let us run batch queries within a transaction, so run the query outside the transaction.

--- app/soc/logic/delete_account.py
+++ app/soc/logic/delete_account.py
@@ -27,8 +27,8 @@ from soc.logic import user as user_logic
 from soc.models import user
 
 from soc.modules.gci.logic import profile as profile_logic
-from soc.modules.gci.models import comment
-from soc.modules.gci.models import task
+from soc.modules.gci.models import comment as comment_model
+from soc.modules.gci.models import task as task_model
 
 
 ADMIN_REQUEST_EMAIL_SUBJEST = """
@@ -88,16 +88,31 @@ def confirm_delete(profile):
   user_delete = not (profile_logic.hasOtherGCIProfiles(profile) or
                      profile_logic.hasOtherGCIProfiles(profile))
 
-  task_sub_q = task.GCITask.all().filter('subscribers', profile)
+  task_sub_q = task_model.GCITask.all().filter('subscribers', profile)
+  task_sub_remove_list = []
+  for task in task_sub_q.run():
+    task_sub_remove_list.append(task)
 
-  tasks_created_by_q = task.GCITask.all().filter('created_by', profile)
+  tasks_created_by_q = task_model.GCITask.all().filter('created_by', profile)
+  task_created_list = []
+  for task in tasks_created_by_q.run():
+    task_created_list.append(task)
 
-  tasks_modified_by_q = task.GCITask.all().filter('modified_by', profile)
+  tasks_modified_by_q = task_model.GCITask.all().filter('modified_by', profile)
+  task_modified_list = []
+  for task in tasks_modified_by_q.run():
+    task_modified_list.append(task)
 
-  tasks_student_q = task.GCITask.all().filter('student', profile)
+  tasks_student_q = task_model.GCITask.all().filter('student', profile)
+  task_student_list = []
+  for task in tasks_student_q.run():
+    task_student_list.append(task)
 
-  comments_created_by_q = comment.GCIComment.all().filter(
+  comments_created_by_q = comment_model.GCIComment.all().filter(
       'created_by', profile.user)
+  comments_created_by_list = []
+  for comment in comments_created_by_q.run():
+    comments_created_by_list.append(comment)
 
   dummy_user = user_logic.getOrCreateDummyMelangeDeletedUser()
   dummy_profile = profile_logic.getOrCreateDummyMelangeDeletedProfile(
@@ -112,24 +127,28 @@ def confirm_delete(profile):
     # The batch size for query.run() is 20, in most of the cases we have
     # seen so far the user had a few tasks with subscriptions, created_by,
     # modified_by etc., so this should still be single datastore hits per
-    # loop.
-    for task in task_sub_q.run():
+    # loop. Also, by running the query outside the transaction we may run
+    # into situations of user subscribing to the task or creating or modifying
+    # tasks or performing another activity after this batch fetch. However,
+    # the chances of that happening is very low and can be traded-off for
+    # the bigger problem of half run transactions.
+    for task in task_sub_remove_list:
       task.subscribers.remove(profile_key)
       entities_to_save.add(task)
 
-    for task in tasks_created_by_q.run():
+    for task in task_created_list:
       task.created_by = dummy_profile
       entities_to_save.add(task)
 
-    for task in tasks_modified_by_q.run():
+    for task in task_modified_list:
       task.modified_by = dummy_profile
       entities_to_save.add(task)
 
-    for task in tasks_student_q.run():
+    for task in task_student_list:
       task.student = dummy_profile
       entities_to_save.add(task)
 
-    for comment in comments_created_by_q.run():
+    for comment in comments_created_by_list:
       comment.created_by = dummy_user
       entities_to_save.add(comment)
 
