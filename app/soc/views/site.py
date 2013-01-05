SRabbelier/Melange

commit a28054f4304232e8dd130615d2131c6c5a96cdb4

Port site/edit page to the new system

app/soc/views/site.py
@@ -1,0 +1,105 @@
+#!/usr/bin/env python2.5
+#
+# Copyright 2011 the Melange authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Module for the site global pages.
+"""
+
+__authors__ = [
+  '"Sverre Rabbelier" <sverre@rabbelier.nl>',
+  ]
+
+
+from django import forms
+from django.conf.urls.defaults import url
+from django.utils.translation import ugettext
+
+from soc.logic import cleaning
+from soc.models.work import Work
+from soc.models.site import Site
+from soc.modules import callback
+from soc.views.base import SiteRequestHandler
+from soc.views.forms import ModelForm
+from soc.views.helper import widgets
+
+
+class SiteForm(ModelForm):
+  """Django form for the site settings.
+  """
+
+  class Meta:
+    model = Site
+    exclude = ['link_id', 'scope', 'scope_path', 'home', 'tos', 'xsrf_secret_key', 'active_program']
+
+  currently_active_program = forms.ChoiceField(
+      required=False, choices=callback.getCore().getProgramMap())
+
+  home_link_id = widgets.ReferenceField(
+      reference_url='document', required=False,
+      filter_fields={'prefix': 'site'},
+      label=ugettext('Home page Document link ID'),
+      help_text=Work.link_id.help_text)
+
+  tos_link_id = widgets.ReferenceField(
+      reference_url='document', required=False,
+      filter_fields={'prefix': 'site'},
+      label=ugettext('Terms of Service Document link ID'),
+      help_text=Work.link_id.help_text)
+
+  clean_noreply_email = cleaning.clean_empty_field('noreply_email')
+
+
+class SitePage(SiteRequestHandler):
+  """View for the participant profile.
+  """
+
+  def djangoURLPatterns(self):
+    return [
+        url(r'^site/edit$', self, name='edit_site_settings'),
+    ]
+
+  def checkAccess(self):
+    # TODO: check is developer
+    pass
+
+  def templatePath(self):
+    # TODO: make this specific to the current active program
+    return 'v2/soc/site/base.html'
+
+  def context(self):
+    if self.data.request.method == 'POST':
+      site_form = SiteForm(self.data.POST, instance=self.data.site)
+    else:
+      site_form = SiteForm(instance=self.data.site)
+    return {
+        'page_name': 'Edit site settings',
+        'site_form': site_form.render(),
+    }
+
+  def validate(self):
+    site_form = SiteForm(self.data.POST, instance=self.data.site)
+
+    if not site_form.is_valid():
+      return False
+
+    site_form.save()
+
+  def post(self):
+    """Handler for HTTP POST request.
+    """
+    if self.validate():
+      self.redirect(reverse('edit_site_settings'))
+    else:
+      self.get()

app/soc/modules/soc_core/callback.py
@@ -68,8 +68,10 @@ class Callback(object):
     """Instantiates all view objects.
     """
     from soc.views import legacy
+    from soc.views import site
 
     self.views.append(legacy.Legacy())
+    self.views.append(site.SitePage())
 
   def registerWithSitemap(self):
     """Called by the server when sitemap entries should be registered.

app/soc/templates/v2/soc/site/base.html
@@ -1,0 +1,46 @@
+{% extends "v2/modules/gsoc/base.html" %}
+{% comment %}
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+{% endcomment %}
+
+{% block stylesheets %}
+  {{ block.super }}
+  <link rel="stylesheet" type="text/css" media="screen" href="/soc/content/css/v2/gsoc/forms.css" />
+  <link rel="stylesheet" type="text/css" media="screen" href="/soc/content/css/v2/gsoc/uniform.default.css" />
+{% endblock stylesheets %}
+
+{% block page_content %}
+
+<!-- begin form -->
+{% comment %} TODO: change class/id after creating custom css {% endcomment %}
+<form action="#" method="post" id="form" class="form-register">
+  <h2 id="form-register-title">Edit site settings</h2> <p id="form-register-req" class="req">* fields required</p>
+  {{ site_form }}
+  <div id="form-register-fieldset-button-row" class="row button-row">
+    <input id="form-register-submit" type="submit" value="Submit" class="submit" />
+  </div>
+</form>
+<!-- end form -->
+
+{% endblock page_content %}
+
+{% block dependencies %}
+  [
+    dep.melange.menu,
+    dep.uniform,
+    null,
+    function () {
+      jQuery("select, input:radio, input:file, input:checkbox").uniform();
+    }
+  ]
+{% endblock dependencies %}

app/soc/views/legacy.py
@@ -35,7 +35,6 @@ class Legacy(object):
     patterns = [
         # TODO: replace with redirect to active program homepage
         (r'^$', 'soc.views.models.site.main_public'),
-        (r'^site/edit$', 'soc.views.models.site.main_edit'),
     ]
 
     from soc.modules.gsoc.views.models import timeline

rhyolight/nupic.son

commit 847c4e4ebca75a605f66c99766bc886af2466536

Fix soc.views.site.EditSitePage.validate to return True or False.
Previously it returned False or None (which would be interpreted
by callers as False). This defect appears to have been introduced
in revision a28054f4304232e8dd130615d2131c6c5a96cdb4.

app/soc/views/site.py
@@ -108,10 +108,11 @@ class EditSitePage(base.SiteRequestHandler):
     site_form = SiteForm(GSoCBoundField, self.data.POST,
                          instance=self.data.site)
 
-    if not site_form.is_valid():
+    if site_form.is_valid():
+      site_form.save()
+      return True
+    else:
       return False
-
-    site_form.save()
 
   def post(self):
     """Handler for HTTP POST request."""