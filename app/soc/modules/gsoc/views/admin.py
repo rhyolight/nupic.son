20abd51f342afa89785c0b2ff1f6db6ccf1b8422
Daniel Hans <get_email Daniel Hans>
2013-01-17 14:06:50 -0800

    'Edit program' renamed to 'Edit program settings' on admin dashboards.
    This is because it makes it consistant with actual page which is opened.

--- app/soc/modules/gsoc/views/admin.py
+++ app/soc/modules/gsoc/views/admin.py
@@ -281,7 +281,7 @@ class ProgramSettingsDashboard(Dashboard):
             'description': ugettext(
                 'Edit your program settings such as information, slots, '
                 'documents, etc.'),
-            'title': 'Edit program',
+            'title': 'Edit program settings',
             'link': r.urlOf('edit_gsoc_program')
         },
         {
