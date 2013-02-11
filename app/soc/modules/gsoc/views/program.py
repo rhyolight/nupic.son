be15dc095edba3c5b1fafb849577419be2f567a4
Daniel Hans <get_email Daniel Hans>
2013-02-11 14:02:17 +0100

    edit_gsoc_program URL pattern is moved to another pattern.
    Before this change it was assigned to 'gsoc/program/sponsor_id/program_id' pattern. After this change it will refer to 'gsoc/program/edit/sponsor_id/program_id' pattern. The reason for this revision is that the former pattern is not clear enough and does not suggest that actual editing is involved.

--- app/soc/modules/gsoc/views/program.py
+++ app/soc/modules/gsoc/views/program.py
@@ -197,9 +197,9 @@ class ProgramPage(base.GSoCRequestHandler):
 
   def djangoURLPatterns(self):
     return [
-        url_patterns.url(r'program/%s$' % soc_url_patterns.PROGRAM, self,
+        url_patterns.url(r'program/%s$' % soc_url_patterns.PROGRAM, self),
+        url_patterns.url(r'program/edit/%s$' % soc_url_patterns.PROGRAM, self,
             name='edit_gsoc_program'),
-        url_patterns.url(r'program/edit/%s$' % soc_url_patterns.PROGRAM, self),
     ]
 
   def jsonContext(self, data, check, mutator):
