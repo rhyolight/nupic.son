b3b7dc33cb21a81c80f0b1746ac5ab337a5119b7
Daniel Hans <get_email Daniel Hans>
2013-02-28 01:57:02 +0100

    forms module imports our own copy of djangoforms.
    This is because the module in question is not supported by Google App Engine on 2.7 runtime.

--- app/soc/views/forms.py
+++ app/soc/views/forms.py
@@ -24,7 +24,6 @@ import itertools
 import re
 
 from google.appengine.ext import db
-from google.appengine.ext.db import djangoforms
 
 from django import forms
 from django.core.exceptions import ImproperlyConfigured
@@ -34,6 +33,8 @@ from django.utils.encoding import force_unicode
 from django.utils.html import conditional_escape
 from django.utils.safestring import mark_safe
 from django.utils.translation import ugettext
+
+from djangoforms import djangoforms
 
 from soc.views.helper.surveys import SurveySchema
 
