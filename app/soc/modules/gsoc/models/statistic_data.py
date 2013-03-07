353df9407785d8558a03059741150de15f279cae
Nathaniel Manista <get_email Nathaniel Manista>
2013-03-07 09:14:02 -0800

    Drop all shebang lines in soc.modules.gsoc.

--- app/soc/modules/gsoc/models/statistic_data.py
+++ app/soc/modules/gsoc/models/statistic_data.py
@@ -1,13 +1,11 @@
-#!/usr/bin/env python2.5
-#
 # Copyright 2011 the Melange authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #   http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,6 +18,7 @@
 import soc.models.statistic_data
 
 
+# TODO(nathaniel): Empty subclass.
 class GSoCStatisticData(soc.models.statistic_data.StatisticData):
   """Model class for GSoCStatisticData.
   """
