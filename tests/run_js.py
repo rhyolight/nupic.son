f49bae8dcc36c7b5e3d467c291dc47bf990b1c94
Mario Ferraro <get_email Mario Ferraro>
2013-03-18 16:17:04 +0100

    Attempt to make testem find phantomjs.

--- tests/run_js.py
+++ tests/run_js.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python2.7
 import subprocess
+import os
 
+_environ = dict(os.environ)
+os.environ["PATH"] += os.pathsep + './node_modules/phantomjs/lib'
 subprocess.call("./bin/node ./node_modules/testem/testem.js ci", shell=True)
+os.environ.clear()
+os.environ.update(_environ)
