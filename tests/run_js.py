506be8ee211afe6f9f7518d6d5b006eae80cfb92
Mario Ferraro <get_email Mario Ferraro>
2013-03-18 16:25:35 +0100

    Fix mistake.

--- tests/run_js.py
+++ tests/run_js.py
@@ -3,7 +3,7 @@ import subprocess
 import os
 
 _environ = dict(os.environ)
-os.environ["PATH"] += os.environ["PATH"] + ':./node_modules/phantomjs/lib'
+os.environ["PATH"] += ':./node_modules/phantomjs/lib'
 subprocess.call("./bin/node ./node_modules/testem/testem.js ci", shell=True)
 os.environ.clear()
 os.environ.update(_environ)
