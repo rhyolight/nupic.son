0136ad1f1d136745d51bf222f898dd7c6fbba3bc
Daniel Hans <get_email Daniel Hans>
2013-03-19 16:02:01 +0100

    Project creating helper functions moved to module's scope.
    They may be reused by other classes in this module now. At some point they may be moved to their own utility module, if their functionality is needed on other test modules.

--- tests/app/soc/modules/gsoc/views/test_project_details.py
+++ tests/app/soc/modules/gsoc/views/test_project_details.py
@@ -12,14 +12,72 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Tests for project_detail views.
-"""
-
+"""Tests for project_detail views."""
 
 from tests.profile_utils import GSoCProfileHelper
 from tests.test_utils import GSoCDjangoTestCase
 
 from soc.modules.gsoc.models.project import GSoCProject
+from soc.modules.gsoc.views import project_details
+
+
+def _createProjectForStudent(program, org, dev_test, student=None):
+  """Returns a newly created GSoCProject for the specified student.
+  If a new student instance is not provided, a new profile is created.
+
+  Args:
+    program: GSoCProgram instance for which the project is to be created
+    org: GSoCOrganization instance for which the project is to be created
+    dev_test: whether it is dev test environment
+    student: the specified GSoCProfile student instance to mentor the project
+
+  Returns:
+    the newly created GSoCProject instance
+  """
+  if not student:
+    student_helper = GSoCProfileHelper(program, dev_test)
+    student_helper.createOtherUser('student@example.com')
+    student = student_helper.createStudent()
+
+  mentor_helper = GSoCProfileHelper(program, dev_test)
+  mentor_helper.createOtherUser('mentor@example.com')
+  mentor_helper.createMentorWithProject(org, student)
+
+  project = GSoCProject.all().get()
+  project.is_featured = False
+  project.status = 'accepted'
+  project.put()
+
+  return project
+
+
+def _createProjectForMentor(program, org, dev_test, mentor=None):
+  """Returns a newly created GSoCProject for the specified mentor.
+  If a new mentor instance is not provided, a new profile is created.
+
+  Args:
+    program: GSoCProgram instance for which the project is to be created
+    org: GSoCOrganization instance for which the project is to be created
+    dev_test: whether it is dev test environment
+    mentor: the specified GSoCProfile mentor instance to mentor the project
+
+  Returns:
+    the newly created GSoCProject instance
+  """
+  if not mentor:
+    mentor_helper = GSoCProfileHelper(program, dev_test)
+    mentor_helper.createOtherUser('mentor@example.com')
+    mentor = mentor_helper.createMentor(org)
+
+  student_helper = GSoCProfileHelper(program, dev_test)
+  student_helper.createOtherUser('student@example.com')
+  student_helper.createStudentWithProject(org, mentor)
+  project = GSoCProject.all().get()
+  project.is_featured = False
+  project.status = 'accepted'
+  project.put()
+  return project
+
 
 class ProjectDetailsTest(GSoCDjangoTestCase):
   """Tests project details page.
@@ -99,62 +157,13 @@ class ProjectDetailsUpdateTest(GSoCDjangoTestCase):
     super(ProjectDetailsUpdateTest, self).setUp()
     self.init()
 
-  def _createProjectForMentor(self, mentor=None):
-    """Returns a newly created GSoCProject for the specified mentor.
-    If a new mentor instance is not provided, a new profile is created.
-
-    Args:
-      mentor: the specified GSoCProfile mentor instance to mentor the project
-
-    Returns:
-      the newly created GSoCProject instance
-    """
-    if not mentor:
-      mentor_helper = GSoCProfileHelper(self.gsoc, self.dev_test)
-      mentor_helper.createOtherUser('mentor@example.com')
-      mentor = mentor_helper.createMentor(self.org)
-
-    student_helper = GSoCProfileHelper(self.gsoc, self.dev_test)
-    student_helper.createOtherUser('student@example.com')
-    student_helper.createStudentWithProject(self.org, mentor)
-    project = GSoCProject.all().get()
-    project.is_featured = False
-    project.status = 'accepted'
-    project.put()
-    return project
-
-  def _createProjectForStudent(self, student=None):
-    """Returns a newly created GSoCProject for the specified student.
-    If a new student instance is not provided, a new profile is created.
-
-    Args:
-      student: the specified GSoCProfile student instance to mentor the project
-
-    Returns:
-      the newly created GSoCProject instance
-    """
-    if not student:
-      student_helper = GSoCProfileHelper(self.gsoc, self.dev_test)
-      student_helper.createOtherUser('student@example.com')
-      student = student_helper.createStudent()
-
-    mentor_helper = GSoCProfileHelper(self.gsoc, self.dev_test)
-    mentor_helper.createOtherUser('mentor@example.com')
-    mentor_helper.createMentorWithProject(self.org, student)
-
-    project = GSoCProject.all().get()
-    project.is_featured = False
-    project.status = 'accepted'
-    project.put()
-    return project
-
   def _getProjectUpdateUrl(self, project):
     return '/gsoc/project/update/%s/%s' % (
         project.parent_key().name(), project.key().id())
 
   def testLoneUserAccessForbidden(self):
     self.timeline.studentsAnnounced()
-    project = self._createProjectForMentor()
+    project = _createProjectForMentor(self.gsoc, self.org, self.dev_test)
 
     url = self._getProjectUpdateUrl(project)
     response = self.get(url)
@@ -165,7 +174,8 @@ class ProjectDetailsUpdateTest(GSoCDjangoTestCase):
     self.timeline.studentsAnnounced()
 
     mentor = self.data.createMentor(self.org)
-    project = self._createProjectForMentor(mentor=mentor)
+    project = _createProjectForMentor(
+        self.gsoc, self.org, self.dev_test, mentor=mentor)
 
     url = self._getProjectUpdateUrl(project)
     response = self.get(url)
@@ -176,7 +186,7 @@ class ProjectDetailsUpdateTest(GSoCDjangoTestCase):
     self.timeline.studentsAnnounced()
 
     self.data.createOrgAdmin(self.org)
-    project = self._createProjectForMentor()
+    project = _createProjectForMentor(self.gsoc, self.org, self.dev_test)
 
     url = self._getProjectUpdateUrl(project)
     response = self.get(url)
@@ -187,7 +197,7 @@ class ProjectDetailsUpdateTest(GSoCDjangoTestCase):
 
     another_org = self.createOrg()
     self.data.createOrgAdmin(another_org)
-    project = self._createProjectForMentor()
+    project = _createProjectForMentor(self.gsoc, self.org, self.dev_test)
 
     url = self._getProjectUpdateUrl(project)
     response = self.get(url)
@@ -198,7 +208,7 @@ class ProjectDetailsUpdateTest(GSoCDjangoTestCase):
     self.timeline.studentsAnnounced()
 
     self.data.createHost()
-    project = self._createProjectForMentor()
+    project = _createProjectForMentor(self.gsoc, self.org, self.dev_test)
 
     url = self._getProjectUpdateUrl(project)
     response = self.get(url)
@@ -208,7 +218,8 @@ class ProjectDetailsUpdateTest(GSoCDjangoTestCase):
     self.timeline.studentsAnnounced()
 
     student = self.data.createStudent()
-    project = self._createProjectForStudent(student=student)
+    project = _createProjectForStudent(
+        self.gsoc, self.org, self.dev_test, student=student)
 
     url = self._getProjectUpdateUrl(project)
     response = self.get(url)
@@ -218,7 +229,7 @@ class ProjectDetailsUpdateTest(GSoCDjangoTestCase):
     self.timeline.studentsAnnounced()
 
     student = self.data.createStudent()
-    project = self._createProjectForStudent()
+    project = _createProjectForStudent(self.gsoc, self.org, self.dev_test)
 
     url = self._getProjectUpdateUrl(project)
     response = self.get(url)
