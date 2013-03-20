a1b47cadb90e019745517d055cd7d8ef08101de3
Mario Ferraro <get_email Mario Ferraro>
2013-03-20 16:43:09 +0100

    Reverse order of tests.
    Seems like nose exiting prevents js tests to run.
    Also use correct python style for sets.

--- tests/run.py
+++ tests/run.py
@@ -322,12 +322,12 @@ def main():
     tests.update(sys.argv[i+1].split(','))
     del sys.argv[i:i+2]
   else:
-    tests = {'js', 'pyunit'}
+    tests = set(['js', 'pyunit'])
 
-  if 'pyunit' in tests:
-    run_pyunit_tests()
   if 'js' in tests:
     run_js_tests()
+  if 'pyunit' in tests:
+    run_pyunit_tests()
 
 if __name__ == '__main__':
   main()
