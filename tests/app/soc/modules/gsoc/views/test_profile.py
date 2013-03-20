34ef1ba41412ca3a4406fcbb4a8eb3f014665eaa
Madhusudan.C.S <get_email Madhusudan.C.S>
2013-03-20 12:46:44 +0100

    Add assertions to make sure student info entity gets created while creating student profile.

--- tests/app/soc/modules/gsoc/views/test_profile.py
+++ tests/app/soc/modules/gsoc/views/test_profile.py
@@ -168,11 +168,18 @@ class ProfileViewTest(GSoCDjangoTestCase):
     })
 
     response = self.post(role_url, postdata)
-
     self.assertResponseRedirect(response, url + '?validated')
 
     # hacky
     profile = GSoCProfile.all().get()
+
+    self.assertEquals(postdata.get('given_name'), profile.given_name)
+    self.assertEquals(postdata.get('surname'), profile.surname)
+
+    # Make sure student info entity is created with right values.
+    self.assertEquals(postdata.get('school_name'),
+                      profile.student_info.school_name)
+
     profile.delete()
 
     postdata.update({
