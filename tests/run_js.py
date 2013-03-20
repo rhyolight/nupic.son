ce6b2cc0b2fe748a4b8ef3345958687b57aea8e6
Mario Ferraro <get_email Mario Ferraro>
2013-03-20 12:25:09 +0100

    Integrate JS tests in run.py.
    Thanks to Leo for helping me out :)

--- tests/run_js.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python2.7
-import subprocess
-import os
-
-_environ = os.environ.copy()
-_environ["PATH"] += ':./node_modules/phantomjs/lib/phantom/bin'
-
-subprocess.call("./bin/node ./node_modules/testem/testem.js ci", env=_environ, shell=True)
