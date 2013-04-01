82ccc7394ee9469db9a342b25a35bdb29513e2f2
Daniel Hans <get_email Daniel Hans>
2013-04-01 21:32:43 +0200

    A unversal class which may be used to obtain a template with list of all organizations added.
    This is an initial version which will be changed. Currently it is more or less a copy of a template from accepted_orgs view. The reason for having this template in a separate view is that a list of organizations is needed in a lot of places and a lot of code is duplicated.

---
+++ app/soc/modules/gsoc/templates/org_list.py
@@ -0,0 +1,79 @@
+# Copyright 2013 the Melange authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Module for templates with organizations list."""
+
+from django.utils import html as http_utils
+
+from soc.views import template
+from soc.views.helper import lists
+
+from soc.modules.gsoc.models import organization as org_model
+from soc.modules.gsoc.models import profile as profile_model
+
+
+class AcceptedOrgsList(template.Template):
+  """Template for list of accepted organizations."""
+
+  def __init__(self, data):
+    self.data = data
+
+    list_config = lists.ListConfiguration()
+    list_config.addPlainTextColumn('name', 'Name',
+        (lambda e, *args: e.short_name.strip()), width=75)
+    list_config.addSimpleColumn('link_id', 'Organization ID', hidden=True)
+
+    list_config = self.extraColumn(list_config)
+    self._list_config = list_config
+
+  def extraColumn(self, list_config):
+    raise NotImplementedError
+
+  def context(self):
+    description = 'List of organizations accepted into %s' % (
+        self.data.program.name)
+
+    list = lists.ListConfigurationResponse(
+        self.data, self._list_config, 0, description)
+
+    return {
+        'lists': [list],
+    }
+
+  def getListData(self):
+    idx = lists.getListIndex(self.data.request)
+    if idx != 0:
+      return None
+
+    q = org_model.GSoCOrganization.all().filter('scope', self.data.program)
+
+    starter = lists.keyStarter
+    prefetcher = self._getPrefetcher()
+
+    response_builder = lists.RawQueryContentResponseBuilder(
+        self.data.request, self._list_config, q, starter, prefetcher=prefetcher)
+
+    return response_builder.build()
+
+  def templatePath(self):
+    return "v2/modules/gsoc/admin/_accepted_orgs_list.html"
+
+  def _getPrefetcher(self):
+    """Returns a prefetcher for the list.
+
+    It may be overridden by subclasses. By default no prefetcher is returned.
+
+    Returns: a function to prefetch entities for the list or None
+    """
+    return None
