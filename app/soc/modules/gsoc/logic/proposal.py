364f82b59350a3eda2b30102713cdd8dbe8ae550
Daniel Hans <get_email Daniel Hans>
2013-04-26 15:43:56 +0200

    Some stylish fixes for proposal logic.

--- app/soc/modules/gsoc/logic/proposal.py
+++ app/soc/modules/gsoc/logic/proposal.py
@@ -12,54 +12,52 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""GSOC logic for proposals.
-"""
-
+"""GSoC logic for proposals."""
 
 from google.appengine.ext import db
 
-from soc.modules.gsoc.models.proposal import GSoCProposal
+from soc.modules.gsoc.models import proposal as proposal_model
 
 
 def getProposalsToBeAcceptedForOrg(org_entity, step_size=25):
-  """Returns all StudentProposals which will be accepted into the program
+  """Returns all proposals which will be accepted into the program
   for the given organization.
 
-  params:
+  Args:
     org_entity: the Organization for which the proposals should be checked
     step_size: optional parameter to specify the amount of Student Proposals
-              that should be retrieved per roundtrip to the datastore
+        that should be retrieved per roundtrip to the datastore
 
-  returns:
-    List with all StudentProposal which will be accepted into the program
+  Returns:
+    List with all GSoCProposal which will be accepted into the program
   """
 
   # check if there are already slots taken by this org
-  q = GSoCProposal.all()
-  q.filter('org', org_entity)
-  q.filter('status', 'accepted')
+  query = proposal_model.GSoCProposal.all()
+  query.filter('org', org_entity)
+  query.filter('status', 'accepted')
 
-  slots_left_to_assign = max(0, org_entity.slots - q.count())
+  slots_left_to_assign = max(0, org_entity.slots - query.count())
   if slots_left_to_assign == 0:
     # no slots left so return nothing
     return []
 
-  q = GSoCProposal.all()
-  q.filter('org', org_entity)
-  q.filter('status', 'pending')
-  q.filter('accept_as_project', True)
-  q.filter('has_mentor', True)
-  q.order('-score')
+  query = proposal_model.GSoCProposal.all()
+  query.filter('org', org_entity)
+  query.filter('status', 'pending')
+  query.filter('accept_as_project', True)
+  query.filter('has_mentor', True)
+  query.order('-score')
 
   # We are not putting this into the filter because order and != do not mix
   # in GAE.
-  proposals = q.fetch(slots_left_to_assign)
+  proposals = query.fetch(slots_left_to_assign)
 
   offset = slots_left_to_assign
   # retrieve as many additional proposals as needed in case the top
   # N do not have a mentor assigned
   while len(proposals) < slots_left_to_assign:
-    new_proposals = q.fetch(step_size, offset=offset)
+    new_proposals = query.fetch(step_size, offset=offset)
 
     if not new_proposals:
       # we ran out of proposals`
@@ -73,20 +71,20 @@ def getProposalsToBeAcceptedForOrg(org_entity, step_size=25):
 
 
 def getProposalsQuery(keys_only=False, ancestor=None, **properties):
-  """Returns the Appengine GSoCProposal query object for the given set
-  of properties.
+  """Returns the Appengine proposal_model.GSoCProposal query object
+  for the given set of properties.
 
   Args:
     ancestor: The student for whom the proposals must be fetched.
     properties: keyword arguments containing the properties for which the
         query must be constructed.
   """
-  q = db.Query(GSoCProposal, keys_only=keys_only)
+  query = db.Query(proposal_model.GSoCProposal, keys_only=keys_only)
 
   if ancestor:
-    q.ancestor(ancestor)
+    query.ancestor(ancestor)
 
   for k, v in properties.items():
-    q.filter(k, v)
+    query.filter(k, v)
 
-  return q
+  return query
