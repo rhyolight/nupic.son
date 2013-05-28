040075d204a64b6969c5c4f9325d90e4adbcb31a
Drew Rodman <get_email Drew Rodman>
2013-05-27 23:22:07 -0400

    Add new methods to connection logic.
    These changes were poached (via copy and paste, sadly) from a
    different (no longer existing) branch in the interest of keeping
    a cleaner commit history. This refactor was necessary in order to
    begin moving the Connection entity logic out of the view module and
    into a more accessible and reasonable location.

--- app/soc/modules/gsoc/logic/connection.py
+++ app/soc/modules/gsoc/logic/connection.py
@@ -15,7 +15,9 @@
 """Query and functions for GSoCConnection.
 """
 
+from soc.logic import exceptions
 from soc.modules.gsoc.models.connection import GSoCConnection
+from soc.modules.gsoc.models.connection_message import GSoCConnectionMessage
 
 
 def queryForAncestor(ancestor, keys_only=False):
@@ -32,3 +34,62 @@ def queryForAncestorAndOrganization(ancestor, organization, keys_only=False):
   query.filter('organization', organization)
   return query
 
+def connectionExists(user, organization):
+  """Returns True if a GSoCConnection object exists for the given User and
+  Organization, else False.
+  """
+  query = queryforAncestorAndOrganization(user, organization, True)
+  return query.count(limit=1) > 0
+
+def createConnection(profile, org, user_state, org_state, role):
+  """Create a new GSoCConnection instance based on the contents of the form
+  and the roles provided.
+
+  Args:
+      profile: GSoCProfile with which to establish the connection.
+      org: Organization with which to establish the connection.
+      user_state: The user's response state for the connection.
+      org_state: The org's response state for the connection.
+      role: Role to offer the user (see soc.models.Connection for opts).
+
+  Returns:
+      Newly created GSoCConnection instance.
+
+  Raises:
+      AccessViolation if a connection exists between the user and organization.
+  """
+  if connectionExists(profile.parent(), org):
+    raise exceptions.AccessViolation(DEF_CONNECTION_EXISTS)
+
+  connection = GSoCConnection(parent=profile.parent())
+  connection.organization = org
+  connection.user_state = user_state
+  connection.org_state = org_state
+  connection.role = role
+  connection.put()
+
+  return connection
+
+def createConnectionMessage(connection, author, content, auto_generated=False):
+  """Create a new GSoCConnectionMessage to represent a message left
+  on a GSoCConnection entity.
+
+  Args:
+      connection: GSoCConnection on which the message was left.
+      author: Profile of the user leaving the message.
+      content: String content of the message.
+      auto_generated: True if the message was system-generated, False if the
+        message contains a user-provided message,
+
+  Returns:
+      Newly created GSoCConnectionMessage entity.
+  """
+  message = GSoCConnectionMessage(parent=connection)
+  message.content = content
+  if auto_generated:
+    message.is_auto_generated = auto_generated
+  else:
+    message.author = author
+  message.put()
+
+  return message
