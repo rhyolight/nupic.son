62981071dd88d4d396f73eed408c3c5fdc5e1dc2
Daniel Hans <get_email Daniel Hans>
2013-06-04 16:49:39 +0200

    Two unit tests added accept_withdraw_projects view.
    One of them simply tries to accept one proposal. The other one accepts two proposals in a single request.

--- tests/app/soc/modules/gsoc/views/test_accept_withdraw_projects.py
+++ tests/app/soc/modules/gsoc/views/test_accept_withdraw_projects.py
@@ -14,6 +14,14 @@
 
 """Unit tests for manage projects related views."""
 
+import json
+
+from soc.modules.gsoc.models import profile as profile_model
+from soc.modules.gsoc.models import project as project_model
+from soc.modules.gsoc.models import proposal as proposal_model
+from soc.modules.gsoc.views import accept_withdraw_projects
+
+from tests import profile_utils
 from tests import test_utils
 
 
@@ -68,3 +76,70 @@ class AcceptProposalsTest(test_utils.GSoCDjangoTestCase):
     response = self.get(self.url)
     self.assertResponseOK(response)
     self._assertTemplatesUsed(response)
+
+  def testAcceptProposal(self):
+    """Tests that a proposal is correctly accepted."""
+    self.data.createHost()
+
+    mentor = profile_utils.GSoCProfileHelper(
+        self.gsoc, False).createMentor(self.org)
+    student = profile_utils.GSoCProfileHelper(
+        self.gsoc, False).createStudentWithProposal(self.org, mentor)
+    proposal = proposal_model.GSoCProposal.all().ancestor(student).get()
+
+    list_data = [{
+        accept_withdraw_projects._PROPOSAL_KEY: str(proposal.key())
+        }]
+    post_data = {
+        'button_id': 'accept',
+        'data': json.dumps(list_data),
+        'idx': 0,
+        }
+    self.post(self.url, post_data)
+
+    # check if proposal is accepted correctly
+    proposal = proposal_model.GSoCProposal.get(proposal.key())
+    self.assertEqual(proposal.status, proposal_model.STATUS_ACCEPTED)
+
+    # check if a project is created
+    project = project_model.GSoCProject.all().ancestor(student).get()
+    self.assertEqual(project.status, 'accepted')
+    self.assertEqual(project.proposal.key(), proposal.key())
+
+    # check if number of projects is updated
+    student_info = profile_model.GSoCStudentInfo.all().ancestor(student).get()
+    self.assertEqual(student_info.number_of_projects, 1)
+
+  def testAcceptTwoProposals(self):
+    """Tests that two proposals can be accepted in the same request."""
+
+    self.data.createHost()
+
+    mentor = profile_utils.GSoCProfileHelper(
+        self.gsoc, False).createMentor(self.org)
+    student1 = profile_utils.GSoCProfileHelper(
+        self.gsoc, False).createStudentWithProposal(self.org, mentor)
+    student2 = profile_utils.GSoCProfileHelper(
+        self.gsoc, False).createStudentWithProposal(self.org, mentor)
+
+    proposal1 = proposal_model.GSoCProposal.all().ancestor(student1).get()
+    proposal2 = proposal_model.GSoCProposal.all().ancestor(student2).get()
+
+    list_data = [
+        {accept_withdraw_projects._PROPOSAL_KEY: str(proposal1.key())},
+        {accept_withdraw_projects._PROPOSAL_KEY: str(proposal2.key())}
+        ]
+
+    post_data = {
+        'button_id': 'accept',
+        'data': json.dumps(list_data),
+        'idx': 0,
+        }
+    self.post(self.url, post_data)
+
+    # check if both proposals are accepted correctly
+    proposal1 = proposal_model.GSoCProposal.get(proposal1.key())
+    self.assertEqual(proposal1.status, proposal_model.STATUS_ACCEPTED)
+
+    proposal2 = proposal_model.GSoCProposal.get(proposal2.key())
+    self.assertEqual(proposal2.status, proposal_model.STATUS_ACCEPTED)
