526792189d9db6e319add3c384b44484ffaf335b
Daniel Hans <get_email Daniel Hans>
2013-08-10 21:57:23 +0200

    Initial implementation of context() and post() for StartConnectionAsUser.
    There is a TODO left in post() function, as recent changes to Connection model and logic must be merged into this branch first.

--- app/codein/views/connection.py
+++ app/codein/views/connection.py
@@ -14,9 +14,101 @@
 
 """Module with Code In specific connection views."""
 
+from django import forms as django_forms
+from django.utils import translation
+
+from melange.models import connection as connection_model
 from melange.request import access
 
+from codein.views.helper import urls
+
 from soc.modules.gci.views import base
+from soc.modules.gci.views import forms as gci_forms
+from soc.modules.gci.views.helper import url_patterns as ci_url_patterns
+from soc.views.helper import url_patterns
+
+
+START_CONNECTION_AS_USER_PAGE_NAME = translation.ugettext(
+    'Start connection with organization')
+
+START_CONNECTION_MESSAGE_LABEL = translation.ugettext(
+    'Message')
+
+START_CONNECTION_AS_USER_FORM_ROLE_LABEL = translation.ugettext(
+    'Requested Role')
+
+START_CONNECTION_AS_USER_FORM_MESSAGE_HELP_TEXT = translation.ugettext(
+    'Optional message to the organization')
+
+START_CONNECTION_AS_USER_FORM_ROLE_HELP_TEXT = translation.ugettext(
+    'Role requested from the organization')
+
+ROLE_CHOICES = [
+    (connection_model.MENTOR_ROLE, 'Mentor'),
+    (connection_model.ORG_ADMIN_ROLE, 'Organization Admin'),
+    ]
+
+
+class ConnectionForm(gci_forms.GCIModelForm):
+  """Django form to show specific fields for an organization.
+
+  Upon creation the form can be customized using instance methods so as
+  to accommodate actual use cases.
+  """
+
+  message = gci_forms.CharField(widget=gci_forms.Textarea(), required=False)
+
+  def __init__(self, **kwargs):
+    """Initializes a new instance of connection form."""
+    super(ConnectionForm, self).__init__(**kwargs)
+
+    self.fields['message'].label = START_CONNECTION_MESSAGE_LABEL
+
+    # set widget for role field
+    self.fields['user_role'].widget = django_forms.fields.Select(
+        choices=ROLE_CHOICES)
+
+  def setHelpTextForMessage(self, help_text):
+    """Sets help text for 'message' field.
+
+    Args:
+      help_text: a string containing help text to set.
+    """
+    self.fields['message'].help_text = help_text
+
+  def setLabelForRole(self, label):
+    """Sets label for 'role' field.
+
+    Args:
+      label: a string containing the label to set.
+    """
+    self.fields['user_role'].label = label
+
+  def setHelpTextForRole(self, help_text):
+    """Sets help text for 'role' field.
+
+    Args:
+      help_text: a string containing help text to set.
+    """
+    self.fields['user_role'].help_text = help_text
+
+  class Meta:
+    model = connection_model.Connection
+    fields = ['user_role']
+
+
+def _formToStartConnectionAsUser(**kwargs):
+  """Returns a Django form to start connection as a user.
+
+  Args:
+    data: request_data.RequestData object describing the current request.
+  """
+  form = ConnectionForm(**kwargs)
+  form.setLabelForRole(START_CONNECTION_AS_USER_FORM_ROLE_LABEL)
+  form.setHelpTextForRole(START_CONNECTION_AS_USER_FORM_ROLE_HELP_TEXT)
+  form.setHelpTextForMessage(START_CONNECTION_AS_USER_FORM_MESSAGE_HELP_TEXT)
+
+  return form
 
 
 class StartConnectionAsUser(base.GCIRequestHandler):
@@ -24,10 +116,33 @@ class StartConnectionAsUser(base.GCIRequestHandler):
 
   access_checker = access.NON_STUDENT_ACCESS_CHECKER
 
+  def djangoURLPatterns(self):
+    """See base.GCIRequestHandler.djangoURLPatterns for specification."""
+    return [
+        ci_url_patterns.url(
+            r'connection/start/user/%s$' % url_patterns.USER_ORG,
+            self, name=urls.UrlNames.CONNECTION_START_AS_USER)
+    ]
+
   def templatePath(self):
     """See base.GCIRequestHandler.templatePath for specification."""
     return 'modules/gci/form_base.html'
 
   def context(self, data, check, mutator):
     """See base.GCIRequestHandler.context for specification."""
-    pass
+    return {
+        'page_name': START_CONNECTION_AS_USER_PAGE_NAME,
+        'organization': data.organization.link_id,
+        'forms': [_formToStartConnectionAsUser()]
+        }
+
+  def post(self, data, check, mutator):
+    """See base.GCIRequestHandler.post for specification."""
+
+    form = _formToStartConnectionAsUser(data=data.POST)
+    if form.is_valid():
+      # TODO(daniel): create actual connection
+      pass
+    else:
+      # TODO(nathaniel): problematic self-use.
+      return self.get(data, check, mutator)
