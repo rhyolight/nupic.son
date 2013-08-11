a82153c74d6a14236f86eb54dbdef3e7d9eed738
Daniel Hans <get_email Daniel Hans>
2013-08-11 02:25:32 +0200

    connection module added to Melange's views package.
    As of right now, one function was moved there from GSoC specific connection module. This function will be used by both CI and GSoC modules, so it is put in common scope.
    
    Also, some minor style changes are applied to the function body.

---
+++ app/melange/views/connection.py
@@ -0,0 +1,67 @@
+# Copyright 2013 the Melange authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Module with Code In specific connection views."""
+
+from google.appengine.ext import db
+
+from melange.logic import connection as connection_logic
+from melange.models import connection as connection_model
+from melange.request import exception
+
+from soc.tasks import mailer
+
+
+@db.transactional
+def createConnectionTxn(
+    data, profile, organization, message, context, recipients,
+    org_role=connection_model.NO_ROLE, user_role=connection_model.NO_ROLE):
+  """Creates a new Connection entity, attach any messages provided by the
+  initiator and send a notification email to the recipient(s).
+
+  Args:
+    data: RequestData object for the current request.
+    profile: Profile with which to connect.
+    organization: Organization with which to connect.
+    message: User-provided message for the connection.
+    context: The notification context method.
+    recipients: List of one or more recipients for the notification email.
+    org_state: Org state for the connection.
+    user_state: User state for the connection.
+
+  Returns:
+    The newly created Connection entity.
+  """
+  if connection_logic.connectionExists(profile.parent(), organization):
+    raise exception.Forbidden(
+        message=connection_logic.CONNECTION_EXISTS_ERROR %
+        (profile.name, organization.name))
+  # Do not create a connection if a user already has a role within the org.
+  if organization.key() in profile.mentor_for:
+    raise exception.Forbidden(message=USER_HAS_ROLE % profile.name())
+  # Generate the new connection.
+  new_connection = connection_logic.createConnection(
+      profile=profile, org=organization,
+      org_role=org_role, user_role=user_role)
+  # Attach any user-provided messages to the connection.
+  if message:
+    connection_logic.createConnectionMessage(
+        connection=new_connection, author=profile, content=message)
+  # Dispatch an email to the user.
+  notification = context(data=data, connection=new_connection,
+      recipients=recipients, message=message)
+  sub_txn = mailer.getSpawnMailTaskTxn(notification, parent=new_connection)
+  sub_txn()
+
+  return new_connection
