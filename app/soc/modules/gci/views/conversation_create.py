5fcaf0acd56df263a6011cf44a332124d18bd93e
Drew Gottlieb <get_email Drew Gottlieb>
2013-08-13 15:25:42 -0400

    Conversation form now handles POSTing, cleaning posted data, and creating the conversation.
    Added a test to ensure error messages are created when appropriate, and that conversations are created correctly as a result of submitting the form.

--- app/soc/modules/gci/views/conversation_create.py
+++ app/soc/modules/gci/views/conversation_create.py
@@ -14,19 +14,33 @@
 
 """Module containing the view for a new conversation form."""
 
+import json
+
+from datetime import datetime
+
 from django import forms as django_forms
+from django.utils import html
 from django.utils import translation
+from django.forms import util as forms_util
+
+from google.appengine.ext import db
+from google.appengine.ext import ndb
+
+from soc.logic import cleaning
 
 from soc.views import template
 from soc.views.helper import url_patterns
 
 from soc.models import conversation as conversation_model
+from soc.models import user as user_model
 
 from soc.modules.gci.views import base as base_views
 from soc.modules.gci.views import forms as gci_forms
 from soc.modules.gci.views.helper import url_patterns as gciurl_patterns
 from soc.modules.gci.views.helper import url_names as gciurl_names
 
+from soc.modules.gci.logic import conversation as gciconversation_logic
+from soc.modules.gci.logic import message as gcimessage_logic
 from soc.modules.gci.logic import organization as gciorganization_logic
 from soc.modules.gci.logic import profile as gciprofile_logic
 
@@ -45,6 +59,16 @@ ROLE_PROGRAM_MENTORS = 'ProgramMentors'
 ROLE_PROGRAM_STUDENTS = 'ProgramStudents'
 ROLE_ORGANIZATION_ADMINISTRATORS = 'OrganizationAdministrators'
 ROLE_ORGANIZATION_MENTORS = 'OrganizationMentors'
+
+DEF_BLANK_MESSAGE = translation.ugettext('Your message cannot be blank.')
+DEF_BLANK_SUBJECT = translation.ugettext('Your subject cannot be blank.')
+DEF_NO_USERNAMES_SPECIFIED = translation.ugettext(
+    'One or more usernames must be specified.')
+DEF_NO_CONVERSATION_SPECIFIED = translation.ugettext(
+    'An organization must be specified.')
+DEF_NO_ROLES_SPECIFIED = translation.ugettext(
+    'At least one role must be specified.')
+DEF_INVALID_RECIPIENTS_TYPE = translation.ugettext('Invalid recipients type.')
 
 
 def createProgramRoleChoices(data):
@@ -142,7 +166,7 @@ class ConversationCreateForm(gci_forms.GCIModelForm):
 
     if self.organizations and self.organization_role_choices:
       self.fields['organization'] = django_forms.ChoiceField(
-          choices=map(
+          required=False, choices=map(
               lambda org: (org.key(), org.name), self.organizations))
       self.fields['organization_roles'] = django_forms.MultipleChoiceField(
           label=translation.ugettext('Roles'), required=False,
@@ -184,6 +208,167 @@ class ConversationCreateForm(gci_forms.GCIModelForm):
     self.bound_fields = {}
     for name, field in self.fields.items():
       self.bound_fields[name] = gci_forms.GCIBoundField(self, field, name)
+
+  def create(self, commit=True, key_name=None, parent=None):
+    """Save this form's cleaned data into a conversation model instance.
+
+    See soc.views.forms.ModelForm.create for original specification.
+
+    Args:
+      commit: Optional bool, default True; If True, the conversation model  
+              is also saved to the datastore. A message model will only be
+              created if the conversation is comitted.
+      key_name: The key name of the new model instance; None by default.
+      parent: Key (ndb) for the entity that is the new entity's parent; None by
+              default.
+
+    Returns:
+      The GCIConversation model instance created by this call.
+    """
+
+    cleaned_data = self.cleaned_data
+    creator = cleaned_data['creator']
+    organization = cleaned_data['organization']
+    user_keys = set()
+
+    if creator is not None:
+      user_keys.add(creator)
+
+    recipients_type = cleaned_data['recipients_type']
+
+    if recipients_type != conversation_model.ORGANIZATION:
+      organization = None
+
+    conversation = gciconversation_model.GCIConversation(
+        program=cleaned_data['program'], id=key_name, parent=parent,
+        subject=cleaned_data['subject'], creator=creator,
+        recipients_type=recipients_type, organization=organization,
+        include_admins=cleaned_data.get('include_admins', False),
+        include_mentors=cleaned_data.get('include_mentors', False),
+        include_students=cleaned_data.get('include_students', False),
+        auto_update_users=cleaned_data['auto_update_users'])
+
+    if not commit:
+      return conversation
+
+    conversation.put()
+
+    if recipients_type == conversation_model.USER:
+      user_keys.update(cleaned_data['users'])
+      for user_key in user_keys:
+        gciconversation_logic.addUserToConversation(conversation.key, user_key)
+    else:
+      gciconversation_logic.refreshConversationParticipants(conversation.key)
+
+    gciconversation_logic.createMessage(
+        conversation=conversation.key, user=creator,
+        content=cleaned_data['message_content'])
+
+    return conversation
+
+  def clean_organization(self):
+    key_str = self.data.get('organization')
+    if key_str:
+      return ndb.Key(urlsafe=key_str)
+    else:
+      return None
+
+  def clean_users(self):
+    usernames = json.loads(self.data.get('users'))
+    if not usernames or not isinstance(usernames, list):
+      return []
+
+    user_keys = []
+    invalid_usernames = []
+    for username in usernames:
+      if username:
+        key = db.Key.from_path('User', username)
+        if db.get(key):
+          user_keys.append(ndb.Key.from_old_key(key))
+        else:
+          invalid_usernames.append(username)
+
+    if invalid_usernames:
+      if len(invalid_usernames) == 1:
+        error = (translation.ugettext('%s is not a valid username.') %
+            invalid_usernames[0])
+      elif len(invalid_usernames) == 2:
+        error = (translation.ugettext('%s and %s are not valid usernames.') % 
+            (invalid_usernames[0], invalid_usernames[1]))
+      else:
+        last = invalid_usernames.pop()
+        error = (translation.ugettext('%s, and %s are not valid usernames.') % 
+            (', '.join(invalid_usernames), last))
+      raise django_forms.ValidationError(error)
+
+    return user_keys
+
+  def clean_message_content(self):
+    content = cleaning.sanitize_html_string(
+        self.data.get('message_content').strip())
+
+    if len(html.strip_tags(content).strip()) == 0:
+      raise django_forms.ValidationError(DEF_BLANK_MESSAGE)
+
+    return content
+
+  def clean_subject(self):
+    subject = html.strip_tags(self.data.get('subject')).strip()
+
+    if not subject:
+      raise django_forms.ValidationError(DEF_BLANK_SUBJECT)
+
+    return subject
+
+  def clean(self):
+    super(ConversationCreateForm, self).clean()
+
+    cleaned_data = self.cleaned_data
+    recipients_type = cleaned_data.get('recipients_type')
+
+    if recipients_type == conversation_model.USER:
+      if not cleaned_data.get('users'):
+        errors = self._errors.setdefault('users', forms_util.ErrorList())
+        errors.append(DEF_NO_USERNAMES_SPECIFIED)
+    elif recipients_type == conversation_model.PROGRAM:
+      roles = cleaned_data.get('program_roles')
+      if roles:
+        if ROLE_PROGRAM_ADMINISTRATORS in roles:
+          self.cleaned_data['include_admins'] = True
+        if ROLE_PROGRAM_MENTORS in roles:
+          self.cleaned_data['include_mentors'] = True
+        if ROLE_PROGRAM_STUDENTS in roles:
+          self.cleaned_data['include_students'] = True
+      else:
+        errors = self._errors.setdefault('program_roles',
+            forms_util.ErrorList())
+        errors.append(DEF_NO_ROLES_SPECIFIED)
+    elif recipients_type == conversation_model.ORGANIZATION:
+      if not cleaned_data.get('organization'):
+        errors = self._errors.setdefault('organization', forms_util.ErrorList())
+        errors.append(DEF_NO_CONVERSATION_SPECIFIED)
+      roles = cleaned_data.get('organization_roles')
+      if roles:
+        if ROLE_ORGANIZATION_ADMINISTRATORS in roles:
+          self.cleaned_data['include_admins'] = True
+        if ROLE_ORGANIZATION_MENTORS in roles:
+          self.cleaned_data['include_mentors'] = True
+      else:
+        errors = self._errors.setdefault('organization_roles',
+            forms_util.ErrorList())
+        errors.append(DEF_NO_ROLES_SPECIFIED)
+    else:
+      errors = self._errors.setdefault('recipients_type',
+          forms_util.ErrorList())
+      errors.append(DEF_INVALID_RECIPIENTS_TYPE)
+
+    cleaned_data['program'] = ndb.Key.from_old_key(
+        self.request_data.program.key())
+
+    cleaned_data['creator'] = ndb.Key.from_old_key(
+        self.request_data.user.key())
+
+    return cleaned_data
 
 
 class ConversationFormErrorTemplate(template.Template):
@@ -259,3 +444,35 @@ class ConversationCreatePage(base_views.GCIRequestHandler):
         'page_name': translation.ugettext('Create a new conversation'),
         'conversation_form_template': ConversationFormTemplate(data),
     }
+
+  def createConversationFromForm(self, data):
+    """Creates a new conversation and message based on data inserted in the
+    form.
+
+    It also creates ConversationUsers for all the conversation participants.
+
+    Args:
+      data: A RequestData for the current request.
+
+    Returns:
+      The newly created GCIConversation entity.
+    """
+    form = ConversationCreateForm(data)
+
+    form.is_valid()
+
+    if not form.is_valid():
+      return None
+
+    return form.create()
+
+  def post(self, data, check, mutator):
+    """See soc.modules.gci.views.base.GCIRequestHandler.post for full
+    specification.
+    """
+    conversation = self.createConversationFromForm(data)
+    if conversation:
+      return data.redirect.id(id=conversation.key.integer_id()).to(
+          name=gciurl_names.GCI_CONVERSATION)
+    else:
+      return self.get(data, check, mutator)
--- tests/app/soc/modules/gci/views/test_conversation_create.py
+++ tests/app/soc/modules/gci/views/test_conversation_create.py
@@ -16,8 +16,18 @@
 
 import unittest
 
+from google.appengine.ext import ndb
+
 from tests import profile_utils
 from tests import program_utils
+from tests.utils import conversation_utils
+
+from soc.models import conversation as conversation_model
+
+from soc.modules.gci.logic import conversation as gciconversation_logic
+from soc.modules.gci.logic import message as gcimessage_logic
+
+from soc.modules.gci.models import message as gcimessage_model
 
 from soc.modules.gci.views import conversation_create as gciconversation_create_view
 
@@ -30,7 +40,7 @@ class MockRequestData:
     user: A User entity for the request.
     profile: A GCIProfile entity for the request.
   """
-  def __init__(self, program_helper, profile_helper):
+  def __init__(self, program_helper, profile_helper, post=None):
     """Populates the request data attributes.
 
     Args:
@@ -40,14 +50,15 @@ class MockRequestData:
     self.program = program_helper.program
     self.profile = profile_helper.profile
     self.user = self.profile.user
-    self.POST = None
+    self.POST = post
 
 
 class GCICreateConversationFormTest(unittest.TestCase):
   """Tests the views for creating GCI conversations."""
 
   def setUp(self):
-    self.program_helper = program_utils.GCIProgramHelper()
+    self.conv_utils = conversation_utils.GCIConversationHelper()
+    self.program_helper = self.conv_utils.program_helper
     self.program_helper.createProgram()
     self.org_a = self.program_helper.createNewOrg({'name': 'org_a'})
     self.org_b = self.program_helper.createNewOrg({'name': 'org_b'})
@@ -55,14 +66,126 @@ class GCICreateConversationFormTest(unittest.TestCase):
     self.org_d = self.program_helper.createNewOrg({'name': 'org_d'})
     self.createProfileAndRequest()
 
-  def createProfileAndRequest(self):
+  def assertEmpty(self, item):
+    """Asserts that an object is not empty.
+
+    Args:
+      item: The object.
+    """
+    l = len(item)
+    if l > 0:
+      raise AssertionError('Object is not empty, with a length of %d.' % l)
+
+  def assertConversation(
+      self, conversation, subject=None, creator=None, recipients_type=None,
+      include_admins=None, include_mentors=None, include_students=None,
+      organization=None, message_content=None, users=None,
+      auto_update_users=None):
+    """Asserts that a conversation has been created with certain properties.
+
+    Only conversation must be passed. The other attributes are optional, and
+    will not be tested if None.
+
+    Args:
+      conversation: Key (ndb) of GCIConversation.
+      subject: If not None, test that the conversation has this subject string.
+      creator: If not None, test that the conversation and first message (if
+               applicable) was created by this User key (ndb).
+      recipients_type: If not None, test that the conversation has this
+                       recipients_type.
+      include_admins: If not None, test that the conversations will include
+                      admins or not.
+      include_mentors: If not None, test that the conversations will include
+                       mentors or not.
+      include_students: If not None, test that the conversations will include
+                        students or not.
+      organization: If not None, test that the conversation organization is this
+                    organization key (ndb).
+      message_content: If not None, test that the first message in the
+                       conversation has this content.
+      users: If not None, test that this list of User keys (ndb) are added to
+             the conversation.
+      auto_update_users: If not None, test that the conversation has the correct
+                         auto_update_users value.
+    """
+    self.assertIsNotNone(conversation, msg='The conversation key is None.')
+
+    conversation_ent = conversation.get()
+    message_query = (gcimessage_logic.queryForConversation(conversation)
+        .order(gcimessage_model.GCIMessage.sent_on)
+        .fetch(1))
+    message_ent = message_query[0] if message_query else None
+
+    if subject is not None:
+      self.assertEqual(
+          subject, conversation_ent.subject,
+          msg='Conversation subject is incorrect.')
+
+    if creator is not None:
+      self.assertEqual(
+          creator, conversation_ent.creator,
+          msg='Conversation creator is incorrect.')
+      if message_ent:
+        self.assertEqual(
+            creator, message_ent.author,
+            msg='Conversation message author is incorrect.')
+
+    if recipients_type is not None:
+      self.assertEqual(
+          recipients_type, conversation_ent.recipients_type,
+          msg='Conversation recipients_type is incorrect.')
+
+    if include_admins is not None:
+      self.assertEqual(
+          include_admins, conversation_ent.include_admins,
+          msg='Conversation include_admins is incorrect.')
+
+    if include_mentors is not None:
+      self.assertEqual(
+          include_mentors, conversation_ent.include_mentors,
+          msg='Conversation include_mentors is incorrect.')
+
+    if include_students is not None:
+      self.assertEqual(
+          include_students, conversation_ent.include_students,
+          msg='Conversation include_students is incorrect.')
+
+    if organization is not None:
+      self.assertEqual(
+          organization, conversation_ent.organization,
+          msg='Conversation organization is incorrect.')
+
+    if message_content is not None:
+      self.assertIsNotNone(
+          message_ent,
+          msg='Cannot test message content because initial message is None.')
+      self.assertEqual(
+          message_content, message_ent.content,
+          msg='Conversation message content is incorrect.')
+
+    if users is not None:
+      added_users = map(
+        lambda e: e.user,
+        gciconversation_logic.queryConversationUserForConversation(
+            conversation))
+      self.assertEqual(
+          set(users), set(added_users), msg='Conversation users are incorrect.')
+
+    if auto_update_users is not None:
+      self.assertEqual(
+          auto_update_users, conversation_ent.auto_update_users,
+          msg='Conversation auto_update_users is incorrect.')
+
+  def createProfileAndRequest(self, post=None):
     """Creates a new GCIProfileHelper, along with a new MockRequestData for the
     helper.
     """
     self.profile_helper = profile_utils.GCIProfileHelper(
         self.program_helper.program, False)
     self.profile_helper.createProfile()
-    self.mock_data = MockRequestData(self.program_helper, self.profile_helper)
+    self.user_key = ndb.Key.from_old_key(self.profile_helper.profile.user.key())
+    self.mock_data = MockRequestData(
+        self.program_helper, self.profile_helper, post)
 
   def testCreateProgramRoleChoices(self):
     """Tests that createProgramRoleChoices() returns the appropriate program
@@ -329,3 +452,224 @@ class GCICreateConversationFormTest(unittest.TestCase):
     self.assertIn('org_b', organization_html)
     self.assertIn('org_c', organization_html)
     self.assertNotIn('org_d', organization_html)
+
+  def testSubmission(self):
+    """Tests that the submitting the form correctly creates a conversation or
+    displays the right errors.
+    """
+
+    org_a_key = ndb.Key.from_old_key(self.org_a.key())
+    org_b_key = ndb.Key.from_old_key(self.org_b.key())
+    org_c_key = ndb.Key.from_old_key(self.org_c.key())
+    org_d_key = ndb.Key.from_old_key(self.org_d.key())
+
+    # Create three dummy admin users, two as admins for two orgs, then a third
+    # that is an admin for both first two orgs.
+    dummy_org_admin_a = self.conv_utils.createUser(
+        return_key=True, roles=[conversation_utils.ADMIN],
+        admin_organizations=[org_a_key])
+    dummy_org_admin_b = self.conv_utils.createUser(
+        return_key=True, roles=[conversation_utils.ADMIN],
+        admin_organizations=[org_b_key])
+    dummy_org_admin_ab = self.conv_utils.createUser(
+        return_key=True, roles=[conversation_utils.ADMIN],
+        admin_organizations=[org_a_key, org_b_key])
+
+    # Create three dummy mentor users, two as mentors for two orgs, then a third
+    # that is a mentor for both first two orgs.
+    dummy_org_mentor_a = self.conv_utils.createUser(
+        return_key=True, roles=[conversation_utils.MENTOR],
+        mentor_organizations=[org_a_key])
+    dummy_org_mentor_b = self.conv_utils.createUser(
+        return_key=True, roles=[conversation_utils.MENTOR],
+        mentor_organizations=[org_b_key])
+    dummy_org_mentor_ab = self.conv_utils.createUser(
+        return_key=True, roles=[conversation_utils.MENTOR],
+        mentor_organizations=[org_a_key, org_b_key])
+
+    # Create two dummy students
+    dummy_student_a = self.conv_utils.createUser(
+        return_key=True, roles=[conversation_utils.STUDENT])
+    dummy_student_b = self.conv_utils.createUser(
+        return_key=True, roles=[conversation_utils.STUDENT])
+
+    # Create a student who's also a mentor of the third org
+    dummy_student_org_mentor_c = self.conv_utils.createUser(
+        return_key=True, mentor_organizations=[org_c_key],
+        roles=[conversation_utils.STUDENT, conversation_utils.MENTOR])
+
+    # Test for correct errors in a mostly blank form for recipients_type 'User'
+    self.createProfileAndRequest({
+        'recipients_type': conversation_model.USER,
+      })
+    self.profile_helper.createDeveloper()
+    form = gciconversation_create_view.ConversationCreateForm(self.mock_data)
+    self.assertFalse(form.is_valid())
+    self.assertEmpty(form.bound_fields['recipients_type'].errors)
+    self.assertIn('required', ''.join(form.bound_fields['users'].errors))
+    self.assertIn('required', ''.join(form.bound_fields['subject'].errors))
+    self.assertIn(
+        'required', ''.join(form.bound_fields['message_content'].errors))
+    self.assertEmpty(form.bound_fields['organization'].errors)
+    self.assertEmpty(form.bound_fields['organization_roles'].errors)
+    self.assertEmpty(form.bound_fields['program_roles'].errors)
+
+    # Test for empty users array
+    self.createProfileAndRequest({
+        'recipients_type': conversation_model.USER,
+        'users': '[]',
+      })
+    self.profile_helper.createDeveloper()
+    form = gciconversation_create_view.ConversationCreateForm(self.mock_data)
+    self.assertIn('specified', ''.join(form.bound_fields['users'].errors))
+
+    # Test for invalid user error
+    name_a = ndb.Key.to_old_key(dummy_student_a).name()
+    name_b = ndb.Key.to_old_key(dummy_org_mentor_b).name()
+    self.createProfileAndRequest({
+        'recipients_type': conversation_model.PROGRAM,
+        'users': '["%s", "foo", "%s", "bar"]' % (name_a, name_b),
+      })
+    self.profile_helper.createDeveloper()
+    form = gciconversation_create_view.ConversationCreateForm(self.mock_data)
+    self.assertFalse(form.is_valid())
+    self.assertIn('foo', ''.join(form.bound_fields['users'].errors))
+    self.assertIn('bar', ''.join(form.bound_fields['users'].errors))
+
+    # Test for no errors if valid users given
+    name_a = ndb.Key.to_old_key(dummy_student_a).name()
+    name_b = ndb.Key.to_old_key(dummy_org_mentor_b).name()
+    self.createProfileAndRequest({
+        'recipients_type': conversation_model.PROGRAM,
+        'users': '["%s", "%s"]' % (name_a, name_b),
+      })
+    self.profile_helper.createDeveloper()
+    form = gciconversation_create_view.ConversationCreateForm(self.mock_data)
+    self.assertFalse(form.is_valid())
+    self.assertEmpty(form.bound_fields['users'].errors)
+
+    # Test for correct errors in a mostly blank form for recipients_type
+    # 'Program'
+    self.createProfileAndRequest({
+        'recipients_type': conversation_model.PROGRAM,
+        'users': '[]',
+      })
+    self.profile_helper.createDeveloper()
+    form = gciconversation_create_view.ConversationCreateForm(self.mock_data)
+    self.assertFalse(form.is_valid())
+    self.assertEmpty(form.bound_fields['recipients_type'].errors)
+    self.assertEmpty(form.bound_fields['users'].errors)
+    self.assertIn('required', ''.join(form.bound_fields['subject'].errors))
+    self.assertIn(
+        'required', ''.join(form.bound_fields['message_content'].errors))
+    self.assertEmpty(form.bound_fields['organization'].errors)
+    self.assertEmpty(form.bound_fields['organization_roles'].errors)
+    self.assertIn(
+        'specified', ''.join(form.bound_fields['program_roles'].errors))
+
+    # Test for correct errors in a mostly blank form for recipients_type
+    # 'Organization'
+    self.createProfileAndRequest({
+        'recipients_type': conversation_model.ORGANIZATION,
+        'users': '[]',
+      })
+    self.profile_helper.createDeveloper()
+    form = gciconversation_create_view.ConversationCreateForm(self.mock_data)
+    self.assertFalse(form.is_valid())
+    self.assertEmpty(form.bound_fields['recipients_type'].errors)
+    self.assertEmpty(form.bound_fields['users'].errors)
+    self.assertIn('required', ''.join(form.bound_fields['subject'].errors))
+    self.assertIn(
+        'required', ''.join(form.bound_fields['message_content'].errors))
+    self.assertIn(
+        'specified', ''.join(form.bound_fields['organization'].errors))
+    self.assertIn(
+        'specified', ''.join(form.bound_fields['organization_roles'].errors))
+    self.assertEmpty(form.bound_fields['program_roles'].errors)
+
+    # Test for invalid errors
+    self.createProfileAndRequest({
+        'recipients_type': conversation_model.USER,
+        'users': '[]',
+        'message_content': '  \t\r\n<h1></h1>  ',
+        'subject': '  <b>   </b> ',
+      })
+    self.profile_helper.createDeveloper()
+    form = gciconversation_create_view.ConversationCreateForm(self.mock_data)
+    self.assertFalse(form.is_valid())
+    self.assertIn('specified', ''.join(form.bound_fields['users'].errors))
+    self.assertIn('blank', ''.join(form.bound_fields['subject'].errors))
+    self.assertIn('blank', ''.join(form.bound_fields['message_content'].errors))
+
+    # Test that a conversation is correctly created for specific users.
+    # Whitespace is added to the start and end of subject and content to
+    # make sure it is stripped.
+    name_a = ndb.Key.to_old_key(dummy_student_a).name()
+    name_b = ndb.Key.to_old_key(dummy_org_mentor_b).name()
+    subject = 'Hello world!'
+    content = '<h1>Foo</h1><p>Bar</p>'
+    self.createProfileAndRequest({
+        'recipients_type': conversation_model.USER,
+        'users': '["%s", "%s"]' % (name_a, name_b),
+        'subject': '  \r\n\t  %s  \n ' % subject,
+        'message_content': '  \r\n\t  %s  \n ' % content,
+      })
+    self.profile_helper.createDeveloper()
+    form = gciconversation_create_view.ConversationCreateForm(self.mock_data)
+    self.assertTrue(form.is_valid())
+    conversation = form.create()
+    self.assertConversation(
+        conversation.key, recipients_type=conversation_model.USER,
+        creator=self.user_key, subject=subject, message_content=content,
+        auto_update_users=False,
+        users=[self.user_key, dummy_student_a, dummy_org_mentor_b])
+
+    # Test that a conversation is correctly created for users with specific
+    # roles within the program.
+    self.createProfileAndRequest({
+        'recipients_type': conversation_model.PROGRAM,
+        'users': '[]',
+        'program_roles': [
+              gciconversation_create_view.ROLE_PROGRAM_MENTORS,
+              gciconversation_create_view.ROLE_PROGRAM_STUDENTS,
+            ],
+        'subject': subject,
+        'message_content': content,
+      })
+    self.profile_helper.createDeveloper()
+    form = gciconversation_create_view.ConversationCreateForm(self.mock_data)
+    self.assertTrue(form.is_valid())
+    conversation = form.create()
+    self.assertConversation(
+        conversation.key, recipients_type=conversation_model.PROGRAM,
+        creator=self.user_key, subject=subject, message_content=content,
+        include_students=True, include_mentors=True, include_admins=False,
+        auto_update_users=False, users=[
+            self.user_key, dummy_org_mentor_a, dummy_org_mentor_b,
+            dummy_org_mentor_ab, dummy_student_a, dummy_student_b,
+            dummy_student_org_mentor_c])
+
+    # Test that a conversation is correctly created for users with specific
+    # roles within an organization. Also tests that it saves auto_update_users.
+    self.createProfileAndRequest({
+        'recipients_type': conversation_model.ORGANIZATION,
+        'users': '[]',
+        'organization': str(self.org_b.key()),
+        'organization_roles': [
+              gciconversation_create_view.ROLE_ORGANIZATION_MENTORS,
+            ],
+        'subject': subject,
+        'message_content': content,
+        'auto_update_users': 'on',
+      })
+    self.profile_helper.createDeveloper()
+    form = gciconversation_create_view.ConversationCreateForm(self.mock_data)
+    self.assertTrue(form.is_valid())
+    conversation = form.create()
+    self.assertConversation(
+        conversation.key, recipients_type=conversation_model.ORGANIZATION,
+        creator=self.user_key, subject=subject, message_content=content,
+        include_students=False, include_mentors=True, include_admins=False,
+        organization=ndb.Key.from_old_key(self.org_b.key()),
+        auto_update_users=True,
+        users=[self.user_key, dummy_org_mentor_b, dummy_org_mentor_ab])
