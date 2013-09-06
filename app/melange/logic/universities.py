3866aa22384384fde338c5f2374bcf05bc65add8
Daniel Hans <get_email Daniel Hans>
2013-09-06 14:04:25 +0200

    List of universities for program is now cached in a local Python structure per VM.
    This will drastically reduce time that is necessary to access list of universities for the second time from more than 60 seconds to few milliseconds.

--- app/melange/logic/universities.py
+++ app/melange/logic/universities.py
@@ -22,6 +22,66 @@ from melange.models import universities as universities_model
 #: Maximal number of universities that can be stored in one cluster.
 MAX_UNIVERSITIES_PER_CLUSTER = 5000
 
+# TODO(daniel): this class should be thread-safe
+class _CachedUniversities(object):
+  """Helper class that manages already cached lists of universities for
+  particular programs.
+  """
+
+  def __init__(self):
+    """Initializes a new instance of the class."""
+    self._cached_map = {}
+
+  def get(self, program_key):
+    """Returns universities, which have been predefined for the specified
+    program.
+
+    Args:
+      program_key: program key.
+
+    Returns:
+      list of university_model.Unversity entities.
+    """
+    if program_key not in self._cached_map:
+      self._cached_map[program_key] = _fetchUniversitiesFromDB(program_key)
+    return self._cached_map[program_key]
+
+_CACHED_UNIVERSITIES = _CachedUniversities()
+
+
+def _fetchUniversitiesFromDB(program_key):
+  """Fetches universities, which have been predefined for the specified
+  program, directly from datastore.
+
+  Args:
+    program_key: program key.
+
+  Returns:
+    list of university_model.Unversity entities.
+  """
+  university_clusters = ndb.Query(
+      kind=universities_model.UniversityCluster._get_kind(),
+      ancestor=ndb.Key.from_old_key(program_key)).fetch(1000)
+
+  universities = []
+  for university_cluster in university_clusters:
+    universities.extend(university_cluster.universities)
+
+  return universities
+
+
+def getUniversitiesForProgram(program_key):
+  """Returns universities that were defined for the specified program.
+
+  Args:
+    program_key: program key.
+
+  Returns:
+    list of university_model.Unversity entities.
+  """
+  return _CACHED_UNIVERSITIES.get(program_key)
+
+
 def uploadUniversities(input_data, program):
   """Uploads a list of predefined universities for the specified program.
 
@@ -52,23 +112,3 @@ def uploadUniversities(input_data, program):
 
   program.predefined_schools_counter += len(university_cluster.universities)
   program.put()
-
-
-def getUniversitiesForProgram(program_key):
-  """Returns universities that were defined for the specified program.
-
-  Args:
-    program_key: program key.
-
-  Returns:
-    list of university_model.Unversity entities.
-  """
-  university_clusters = ndb.Query(
-      kind=universities_model.UniversityCluster._get_kind(),
-      ancestor=ndb.Key.from_old_key(program_key)).fetch(1000)
-
-  universities = []
-  for university_cluster in university_clusters:
-    universities.extend(university_cluster.universities)
-
-  return universities
