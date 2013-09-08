bc19473ab70ead71e049c4f9dfee2e87f0f18360
Daniel Hans <get_email Daniel Hans>
2013-09-08 02:35:59 +0200

    Most of existing school logic removed. Also, a new function added.
    getSchoolsFromReader returns a list of School objects (new public class) based on the specified reader. It will be used to read university data from uploaded blob.

--- app/melange/logic/school.py
+++ app/melange/logic/school.py
@@ -14,110 +14,47 @@
 
 """Logic for schools."""
 
-import threading
+import csv
 
-from google.appengine.ext import ndb
-
-from melange.models import school as school_model
+from django.utils import html as html_utils
 
 
-#: Maximal number of schools that can be stored in one cluster.
-MAX_SCHOOLS_PER_CLUSTER = 5000
+class School(object):
+  """Class that represents a single school."""
 
-class _CachedSchools(object):
-  """Helper class that manages already cached lists of schools for
-  particular programs.
-  """
-
-  def __init__(self):
-    """Initializes a new instance of the class."""
-    # makes sure that ndb.Future is created once per program
-    self._init_lock = threading.Lock()
-
-    # maps program keys to ndb.Future objects with schools
-    self._cached_map = {}
-
-  def get(self, program_key):
-    """Returns schools, which have been predefined for the specified program.
+  def __init__(self, uid, name, country):
+    """Initializes a new instance of School class.
 
     Args:
-      program_key: program key.
-
-    Returns:
-      list of school_model.School entities.
+      uid: identifier of the school.
+      name: full name of the school.
+      country: country in which the school is located.
     """
-    if program_key not in self._cached_map:
-      with self._init_lock:
-        if program_key not in self._cached_map:
-          self._cached_map[program_key] = _schoolsFromDBFuture(
-              program_key)
+    self.uid = uid
+    self.name = name
+    self.country = country
 
-    return self._cached_map[program_key].get_result()
 
-_CACHED_SCHOOLS = _CachedSchools()
-
-@ndb.tasklet
-def _schoolsFromDBFuture(program_key):
-  """Tasklet that fetches asynchronously all schools, which have been
-  predefined for the specified program, directly from datastore.
+def getSchoolsFromReader(reader):
+  """Returns schools that were predefined for the specified input reader.
 
   Args:
-    program_key: program key.
+    reader: reader object that describes input for the underlying CSV reader.
 
   Returns:
-    ndb.Future object whose result is a list of 
-    school_model.School entities.
+    list of School objects.
   """
-  school_clusters = yield ndb.Query(
-      kind=school_model.SchoolCluster._get_kind(),
-      ancestor=ndb.Key.from_old_key(program_key)).fetch_async(1000)
+  if not reader:
+    return []
+  else:
 
-  schools = []
-  for school_cluster in school_clusters:
-    schools.extend(school_cluster.schools)
-
-  raise ndb.Return(schools)
-
-
-def getSchoolsForProgram(program_key):
-  """Returns schools that were defined for the specified program.
-
-  Args:
-    program_key: program key.
-
-  Returns:
-    list of school_model.School entities.
-  """
-  return _CACHED_SCHOOLS.get(program_key)
-
-
-def uploadSchools(input_data, program):
-  """Uploads a list of predefined schools for the specified program.
-
-  Args:
-    input_data: list of tuples. Each element of that tuple represents a single
-      school and has exactly three elements. The first one is unique
-      identifier of the school, the second one is its name and the third
-      one is the country in which the institution is located.
-    program: program entity.
-
-  Returns:
-    list of newly created school_model.SchoolCluster entities that contain
-    all school coming from the input data.
-  """
-  if len(input_data) > MAX_SCHOOLS_PER_CLUSTER:
-    raise ValueError(
-        '%s items in input. Maximal supported value is %s' % (
-            len(input_data), MAX_SCHOOLS_PER_CLUSTER))
-
-  school_cluster = school_model.SchoolCluster(
-      parent=ndb.Key.from_old_key(program.key()))
-
-  for uid, name, country in input_data:
-    school_cluster.schools.append(
-        school_model.School(uid=uid, name=name, country=country))
-
-  school_cluster.put()
-
-  program.predefined_schools_counter += len(school_cluster.schools)
-  program.put()
+    schools = []
+    csv_reader = csv.reader(reader, delimiter='\t')
+    for row in csv_reader:
+      # skip possible empty lines
+      if row:
+        schools.append(School(
+            html_utils.escape(row[0]),
+            html_utils.escape(row[1]),
+            html_utils.escape(row[2])))
+    return schools
--- tests/app/melange/logic/test_school.py
+++ tests/app/melange/logic/test_school.py
@@ -14,110 +14,33 @@
 
 """Tests for schools logic."""
 
-import mock
+import StringIO
 import unittest
 
-from google.appengine.ext import ndb
-
 from melange.logic import school as school_logic
-from melange.models import school as school_model
-
-from soc.models import program as program_model
-from soc.modules.seeder.logic.seeder import logic as seeder_logic
 
 
-TEST_INPUT_DATA = (
-    ('uid1', 'name1', 'country1'),
-    ('uid2', 'name2', 'country2'),
-    ('uid3', 'name3', 'country3'))
+TEST_INPUT_SIZE = 10
+TEST_INPUT_READER_DATA = ''.join(
+    'uid%s\tname%s\tcountry%s\n' % (i, i, i) for i in range(TEST_INPUT_SIZE))
 
-class UploadSchoolsTest(unittest.TestCase):
-  """Unit tests for uploadSchools function."""
+class GetSchoolsFromReaderTest(unittest.TestCase):
+  """Unit tests for getSchoolsFromReader function."""
 
-  def setUp(self):
-    """See unittest.TestCase.setUp for specification."""
-    program_properties = {'predefined_schools_counter': 0}
-    self.program = seeder_logic.seed(program_model.Program, program_properties)
-
-  def testSchoolsAreUploaded(self):
-    """Tests that schools are uploaded to entity."""
-    school_logic.uploadSchools(TEST_INPUT_DATA, self.program)
-
-    school_clusters = ndb.Query(
-        kind=school_model.SchoolCluster._get_kind(),
-        ancestor=ndb.Key.from_old_key(self.program.key())).fetch(1000)
-
-    # check that SchoolCluster is returned
-    self.assertEqual(len(school_clusters), 1)
-
-    # check that correct number of schools is stored
-    school_cluster = school_clusters[0]
-    self.assertEqual(
-        len(school_cluster.schools), len(TEST_INPUT_DATA))
-
-    # check that all data is correct
-    for i in range(len(TEST_INPUT_DATA)):
-      self.assertEqual(school_cluster.schools[i].uid, TEST_INPUT_DATA[i][0])
-      self.assertEqual(school_cluster.schools[i].name, TEST_INPUT_DATA[i][1])
-      self.assertEqual(
-          school_cluster.schools[i].country, TEST_INPUT_DATA[i][2])
-
-  @mock.patch('melange.logic.school.MAX_SCHOOLS_PER_CLUSTER', new=2)
-  def testMoreThanMaxItems(self):
-    """Tests that error is raised when more items than allowed is passed."""
-    with self.assertRaises(ValueError):
-      school_logic.uploadSchools(TEST_INPUT_DATA, self.program)
-    
-  def testCounterIsUpdated(self):
-    """Tests that counter in program model is updated correctly."""
-    # update a few schools
-    school_logic.uploadSchools(TEST_INPUT_DATA, self.program)
-
-    # check that counter is updated
-    program = program_model.Program.get(self.program.key())
-    self.assertEqual(program.predefined_schools_counter, len(TEST_INPUT_DATA))
-
-    # update a few more schools
-    next_data = [('uid%s' % i, 'name', 'country') for i in range(5)]
-    school_logic.uploadSchools(next_data, self.program)
-
-    # check that counter is updated
-    program = program_model.Program.get(self.program.key())
-    self.assertEqual(
-        program.predefined_schools_counter, len(TEST_INPUT_DATA) + 5)
-
-
-class GetSchoolsForProgramTest(unittest.TestCase):
-  """Unit tests for getSchoolsForProgram function."""
-
-  def setUp(self):
-    """See unittest.TestCase.setUp for specification."""
-    self.program = seeder_logic.seed(program_model.Program)
-
-    # seed new schools
-    schools = []
-    for input_data in TEST_INPUT_DATA:
-      schools.append(school_model.School(
-          uid=input_data[0], name=input_data[1], country=input_data[2]))
-    school_model.SchoolCluster(
-        parent=ndb.Key.from_old_key(self.program.key()), schools=schools).put()
-
-  def testForProgram(self):
-    """Tests that all schools are returned for the program."""
-    schools = school_logic.getSchoolsForProgram(self.program.key())
-
-    # check that all schools are returned
-    self.assertEqual(len(schools), len(TEST_INPUT_DATA))
-
-    for i in range(len(TEST_INPUT_DATA)):
-      self.assertEqual(schools[i].uid, TEST_INPUT_DATA[i][0])
-      self.assertEqual(schools[i].name, TEST_INPUT_DATA[i][1])
-      self.assertEqual(schools[i].country, TEST_INPUT_DATA[i][2])
-
-  def testForOtherProgram(self):
-    """Tests that no schools are returned for another program."""
-    other_program = seeder_logic.seed(program_model.Program)
-    schools = school_logic.getSchoolsForProgram(other_program.key())
-
-    # check that no schools are returned
+  def testGetSchoolsFromEmptyReader(self):
+    """Tests that no schools are returned for empty input reader."""
+    reader = StringIO.StringIO()
+    schools = school_logic.getSchoolsFromReader(reader)
     self.assertListEqual(schools, [])
+
+  def testGetSchoolsFromNonEmptyReader(self):
+    """Tests that schools are correctly returned for valid input."""
+    reader = StringIO.StringIO(TEST_INPUT_READER_DATA)
+    schools = school_logic.getSchoolsFromReader(reader)
+
+    self.assertEqual(len(schools), TEST_INPUT_SIZE)
+    for i in range(TEST_INPUT_SIZE):
+      self.assertEqual(schools[i].uid, 'uid%s' % i)
+      self.assertEqual(schools[i].name, 'name%s' % i)
+      self.assertEqual(schools[i].country, 'country%s' % i)
+
