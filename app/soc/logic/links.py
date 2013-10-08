042768fc486c907da1ca6addf3dd4ecfd16fd659
Daniel Hans <get_email Daniel Hans>
2013-10-08 16:24:33 +0200

    Sponsor and program related kwargs are obtained from program key.
    No RPC call is required, as the hardcoded pattern used for program's key name is as follows:
    <sponsor_id>/<program_id>
    
    Both names can be easily computed by splitting the whole key name.
    
    Retriving a program entity becomes problematic if the function is executed within a transaction, as it probably triggers a new DB call on a different entity group.

--- app/soc/logic/links.py
+++ app/soc/logic/links.py
@@ -162,18 +162,23 @@ class Linker(object):
     with the specified profile and numeric identifier of some other entity.
 
     Args:
-      profile: profile entity.
-      entity_id: numeric ID of entity.
-      url_name: the name with which a URL was registered with Django.
+      profile: Profile entity.
+      entity_id: Numeric ID of entity.
+      url_name: The name with which a URL was registered with Django.
 
     Returns:
       The URL of the page matching the given names for the given profile
       and organization.
     """
-    program = profile.program
+    # TODO(daniel): it should be moved to a utility function; maybe even
+    # in Profile model
+    program_key = profile_model.Profile.program.get_value_for_datastore(
+        profile)
+    sponsor_id, program_id = program_key.name().split('/')
+
     kwargs = {
-        'sponsor': program_logic.getSponsorKey(program).name(),
-        'program': program.program_id,
+        'sponsor': sponsor_id,
+        'program': program_id,
         'user': profile.parent_key().name(),
         'id': entity_id,
         }
