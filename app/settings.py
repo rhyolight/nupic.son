0e8b45cae753e9dc9fbfcaf0d8f14bc00179082a
Daniel Hans <get_email Daniel Hans>
2013-10-10 20:53:57 +0200

    Commented out eggs.Loader removed from Django settings.
    This loader was not in use since the inception of that file. There is absoultely no point to keep it around.

--- app/settings.py
+++ app/settings.py
@@ -73,7 +73,6 @@ ADMIN_MEDIA_PREFIX = '/media/'
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.Loader',
-    #'django.template.loaders.eggs.Loader',
 )
 
 MIDDLEWARE_CLASSES = (
