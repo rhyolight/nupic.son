8dd0636bdf11d86869c5adbdcd1097a747d00f1e
Daniel Hans <get_email Daniel Hans>
2013-11-05 20:37:21 +0100

    new_window is an optional argument of RedirectCustomRow constructor.
    In majority of cases, the redirected page should be loaded in the same window. Why should clients be bothered with passing it every time?

--- app/melange/utils/lists.py
+++ app/melange/utils/lists.py
@@ -481,15 +481,15 @@ class RedirectCustomRow(Row):
   The link that will be used to redirection is custom to each row.
   """
 
-  def __init__(self, new_window):
+  def __init__(self, new_window=None):
     """Initializes a RedirectCustomRow object.
 
     Args:
-      new_window: A bool indicating whether the redirected page should be
-        loaded in a new window, when a row is clicked.
+      new_window: If specified to True, the redirected page should be
+        loaded in a new window.
     """
     super(RedirectCustomRow, self).__init__('redirect_custom')
-    self.new_window = new_window
+    self.new_window = new_window or False
 
   def _getParameters(self):
     """See Row._getParameters for specification"""
