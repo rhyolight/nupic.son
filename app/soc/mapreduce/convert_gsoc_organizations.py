43abe03262bdf52e4eeb5461297707498d56414f
Daniel Hans <get_email Daniel Hans>
2013-11-12 21:37:45 +0100

    Initial version of a mapreduce script to create new NDB organizations based on existing entities.
    It will create both Organization entity and a corresponding SurveyResponse.

--- app/soc/mapreduce/convert_gsoc_organizations.py
+++ app/soc/mapreduce/convert_gsoc_organizations.py
@@ -12,23 +12,87 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Mapreduce updating GSoCOrganization.
-"""
+"""Mapreduce converting GSoCOrganization to the new SOCOrganization model."""
 
+from google.appengine.ext import ndb
 
 from mapreduce import operation
 
+from melange.models import contact as contact_model
+from melange.models import survey as survey_model
 
-def process_org_tags(org):
-  """Copy organization tags stored in OrgTag to the organization entity.
+# This MapReduce requires this model to have been imported.
+# pylint: disable=unused-import
+from soc.models.org_app_survey import OrgAppSurvey
+# pylint: enable=unused-import
+
+from soc.models.org_app_record import OrgAppRecord
+from soc.modules.gsoc.models.organization import GSoCOrganization
+
+# This MapReduce requires this model to have been imported.
+# pylint: disable=unused-import
+from soc.modules.gsoc.models.program import GSoCProgram
+# pylint: enable=unused-import
+
+from summerofcode.models import organization as org_model
+
+
+def convertOrg(org_key):
+  """Converts the specified organization by creating a new organization entity
+  that inherits from NDB model.
+
+  Args:
+    org_key: Organization key.
   """
+  organization = GSoCOrganization.get(org_key)
 
-  tags = []
+  contact_properties = {
+      'blog': organization.blog,
+      'facebook': organization.facebook,
+      'feed_url': organization.feed_url,
+      'google_plus': organization.google_plus,
+      'irc_channel': organization.irc_channel,
+      'mailing_list': organization.pub_mailing_list,
+      'twitter': organization.twitter,
+      'web_page': organization.home_page,
+      }
+  contact = contact_model.Contact(**contact_properties)
 
-  for org_tag in org.org_tag:
-    tags.append(org_tag.tag)
+  entity_id = '%s/%s' % (
+      organization.program.key().name(), organization.link_id)
+  org_properties = {
+      'contact': contact,
+      'description': organization.description,
+      'ideas_page': organization.ideas,
+      'logo_url': organization.logo_url,
+      'name': organization.name,
+      'org_id': organization.link_id,
+      'program': ndb.Key.from_old_key(organization.program.key())
+      }
+  new_organization = org_model.SOCOrganization(id=entity_id, **org_properties)
 
-  org.tags = list(set(tags))
+  # find organization application record corresponding to this organization
+  app_record = OrgAppRecord.all().filter(
+      'org_id', organization.link_id).filter(
+          'program', organization.program).get()
+  if app_record:
+    app_properties = {}
+    app_properties['created_on'] = getattr(app_record, 'created')
+    app_properties['modified_on'] = getattr(app_record, 'modified')
 
-  yield operation.db.Put(org)
-  yield operation.counters.Increment('org_tags_updated')
+    for prop in app_record.dynamic_properties():
+      app_properties[prop] = getattr(app_record, prop)
+
+    survey = ndb.Key.from_old_key(app_record.survey.key())
+
+    survey_response = survey_model.SurveyResponse(
+        parent=new_organization.key, survey=survey, **app_properties)
+
+  @ndb.transactional
+  def convertOrgTxn():
+    """Creates new entities within a transaction."""
+    new_organization.put()
+    survey_response.put()
+
+  convertOrgTxn()
+  operation.counters.Increment('Organizations converted')
