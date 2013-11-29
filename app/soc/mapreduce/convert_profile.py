d4414d72816d7c8b51ec9c7670f0e5e4bc7dcb79
Daniel Hans <get_email Daniel Hans>
2013-11-29 23:20:02 +0100

    MapReduce script to convert profiles.
    Now, it just appends keys for new organizations to the existing lists. So, if there is a list:
    
    mentor_for = [db.Key('GSoCOrganization', 'sponsor_id/program_id/org_id')],
    
    it will be converted to:
    
    mentor_for = [db.Key('GSoCOrganization', 'sponsor_id/program_id/org_id'), db.Key('SOCOrganization', 'sponsor_id/program_id/org_id')].
    
    It is safe, as long as we just check if a particular db.Key is located in that list. It is not safe, if we try to retrive an entity associated with 'SOCOrganization' from it. AppEngine will complain that there is no model implementation for 'SOCOrganization'. It is correct, as there is no DB implementation for 'SOCOrganization' - it is based on NDB.
    Anyway, I did not find any place at which we would try to fetch an entity based on those lists. However, it was a quick search.

---
+++ app/soc/mapreduce/convert_profile.py
@@ -0,0 +1,61 @@
+# Copyright 2013 the Melange authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Mapreduce converting profile model."""
+
+from google.appengine.ext import db
+
+from mapreduce import operation
+
+# This MapReduce requires this model to have been imported.
+# pylint: disable=unused-import
+from soc.modules.gci.models.profile import GCIProfile
+from soc.modules.gsoc.models.profile import GSoCProfile
+# pylint: enable=unused-import
+
+
+@db.transactional
+def convertGSoCProfileRolesTxn(profile_key):
+  """Converts lists of organizations for which the specified profile has
+  role in a transaction.
+
+  Args:
+    profile_key: Profile key.
+  """
+  profile = db.get(profile_key)
+  new_mentor_for = []
+  for org_key in profile.mentor_for:
+    new_mentor_for.append(org_key)
+    new_mentor_for.append(
+        db.Key.from_path('SOCOrganization', org_key.name()))
+  profile.mentor_for = new_mentor_for
+
+  new_org_admin_for = []
+  for org_key in profile.org_admin_for:
+    new_org_admin_for.append(org_key)
+    new_org_admin_for.append(
+        db.Key.from_path('SOCOrganization', org_key.name()))
+  profile.org_admin_for = new_org_admin_for
+
+  profile.put()
+
+
+def convertGSoCProfileRoles(profile_key):
+  """Converts lists of organizations for which the specified profile has
+  role in a transaction.
+
+  Args:
+    profile_key: Profile key.
+  """
+  convertGSoCProfileRolesTxn(profile_key)
+  operation.counters.Increment('Profiles converted')
