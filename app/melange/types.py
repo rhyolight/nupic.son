35b3490788cb1bfa5990d4ba1192bd5d60802391
Daniel Hans <get_email Daniel Hans>
2013-12-19 21:36:47 +0100

    ndb_profile_model field added to Models class.
    It will eventually replace profile_model, when all entities are updated.

--- app/melange/types.py
+++ app/melange/types.py
@@ -15,6 +15,7 @@
 """Definitions of Melange types."""
 
 from melange.models import organization as ndb_organization
+from melange.models import profile as ndb_profile
 from melange.models import user
 
 from soc.models import organization
@@ -35,8 +36,9 @@ class Models(object):
   """
 
   def __init__(
-      self, ndb_org_model=None, org_model=None, profile_model=None,
-      program_model=None, program_messages_model=None, timeline_model=None):
+      self, ndb_org_model=None, ndb_profile_model=None, org_model=None,
+      profile_model=None, program_model=None, program_messages_model=None,
+      timeline_model=None):
     """Initializes new instance of Models class.
 
     Args:
@@ -48,6 +50,7 @@ class Models(object):
     """
     # TODO(daniel): remove when not necessary
     self.ndb_org_model = ndb_org_model
+    self.ndb_profile_model = ndb_profile_model
     self.org_model = org_model
     self.profile_model = profile_model
     self.program_model = program_model
@@ -57,6 +60,7 @@ class Models(object):
 
 MELANGE_MODELS = Models(
     ndb_org_model=ndb_organization.Organization,
+    ndb_profile_model=ndb_profile.Profile,
     org_model=organization.Organization,
     profile_model=profile.Profile,
     program_model=program.Program,
