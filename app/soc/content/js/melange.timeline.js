f740591584836cd2b9ed9aad1aac4f536880d154
Mario Ferraro <get_email Mario Ferraro>
2014-01-14 16:05:04 +0000

    Externalize shadeColor function.

--- app/soc/content/js/melange.timeline.js
+++ app/soc/content/js/melange.timeline.js
@@ -353,26 +353,24 @@
       }
 
       return slices;
-    },
-
-    // Source http://stackoverflow.com/a/13542669/1194327
-    shadeColor: function (color, percent) {
-      var num = parseInt(color.slice(1), 16),
-        amt = Math.round(2.55 * percent),
-        R = (num >> 16) + amt,
-        B = (num >> 8 & 0x00FF) + amt,
-        G = (num & 0x0000FF) + amt;
-
-      return "#" + (
-        0x1000000 +
-        (R < 255 ? R < 1 ? 0 : R : 255) * 0x10000 +
-        (B < 255 ? B < 1 ? 0 : B : 255) * 0x100 +
-        (G < 255 ? G < 1 ? 0 : G : 255)
-      ).toString(16).slice(1);
     }
-
   };
 
+  Timeline.prototype.shadeColor = function(color, percent) {
+    // Source http://stackoverflow.com/a/13542669/1194327
+    var num = parseInt(color.slice(1), 16);
+    var amt = Math.round(2.55 * percent);
+    var R = (num >> 16) + amt;
+    var B = (num >> 8 & 0x00FF) + amt;
+    var G = (num & 0x0000FF) + amt;
+
+    return "#" + (
+      0x1000000 +
+      (R < 255 ? R < 1 ? 0 : R : 255) * 0x10000 +
+      (B < 255 ? B < 1 ? 0 : B : 255) * 0x100 +
+      (G < 255 ? G < 1 ? 0 : G : 255)
+    ).toString(16).slice(1);
+  };
 
   Timeline.prototype.enrichRaphaelObjectWithCustomAttributes = function (R) {
     R.customAttributes.segment = function (x, y, r, a1, a2) {
