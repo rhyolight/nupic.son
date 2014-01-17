91b1dca26866335973c061ae303463dad760d1f1
Drew Rodman <get_email Drew Rodman>
2014-01-17 14:58:42 +0000

    NDB migration for student_evaluation view in GSoC.
    Replaces old references to work with new NDB models or apply
    conversions until the models in question are updated (which will
    consequently cause the tests to fail).

--- app/soc/modules/gsoc/views/student_evaluation.py
+++ app/soc/modules/gsoc/views/student_evaluation.py
@@ -38,6 +38,7 @@ from soc.modules.gsoc.views import base
 from soc.modules.gsoc.views import forms as gsoc_forms
 from soc.modules.gsoc.views.helper import url_patterns
 
+from summerofcode.models import organization
 from summerofcode.request import links
 
 
@@ -185,8 +186,12 @@ class GSoCStudentEvaluationTakePage(base.GSoCRequestHandler):
         data.student_evaluation, data.url_ndb_profile, show_url=show_url)
 
     check.isProfileActive()
+    # TODO(dcrodman): When GSoCProject is converted to NDB, this Key
+    # conversion will need to be removed.
     org_key = project_model.GSoCProject.org.get_value_for_datastore(
         data.url_project)
+    org_key = ndb.Key.from_old_key(org_key)
+
     if data.orgAdminFor(org_key):
       raise exception.Redirect(show_url)
 
@@ -235,12 +240,14 @@ class GSoCStudentEvaluationTakePage(base.GSoCRequestHandler):
     if not form.is_valid():
       return None
 
+    user_key = data.ndb_user.key.to_old_key()
+
     if not data.student_evaluation_record:
       org_key = project_model.GSoCProject.org.get_value_for_datastore(
           data.url_project)
       form.cleaned_data['project'] = data.url_project
       form.cleaned_data['org'] = org_key
-      form.cleaned_data['user'] = data.user
+      form.cleaned_data['user'] = user_key
       form.cleaned_data['survey'] = data.student_evaluation
       entity = form.create(commit=True)
     else:
@@ -375,8 +382,9 @@ class GSoCStudentEvaluationShowPage(base.GSoCRequestHandler):
     assert isSet(data.student_evaluation)
 
     check.isProfileActive()
-    org_key = project_model.GSoCProject.org.get_value_for_datastore(
+    old_entity = project_model.GSoCProject.org.get_value_for_datastore(
         data.url_project)
+    org_key = ndb.Key.from_old_key(old_entity)
     if data.orgAdminFor(org_key):
       data.role = 'org_admin'
       if data.timeline.afterSurveyEnd(data.student_evaluation):
@@ -396,15 +404,15 @@ class GSoCStudentEvaluationShowPage(base.GSoCRequestHandler):
     assert isSet(data.student_evaluation_record)
 
     record = data.student_evaluation_record
-    student = data.url_profile
+    student = data.url_ndb_profile
 
     org_key = project_model.GSoCProject.org.get_value_for_datastore(
         data.url_project)
     org = ndb.Key.from_old_key(org_key).get()
 
     context = {
-        'page_name': 'Student evaluation - %s' % (student.name()),
-        'student': student.name(),
+        'page_name': 'Student evaluation - %s' % (student.public_name),
+        'student': student.public_name,
         'organization': org.name,
         'project': data.url_project.title,
         'css_prefix': GSoCStudentEvaluationReadOnlyTemplate.Meta.css_prefix,
