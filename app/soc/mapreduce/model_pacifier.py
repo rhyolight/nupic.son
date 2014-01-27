d82a2ad2bbf500581be9909e9ba43ee6591c0b9b
Daniel Hans <get_email Daniel Hans>
2014-01-28 00:58:17 +0100

    New version of model pacifier to remove all entities for NDB models.
    The old one does not work.

--- app/soc/mapreduce/model_pacifier.py
+++ app/soc/mapreduce/model_pacifier.py
@@ -13,10 +13,17 @@
 # limitations under the License.
 
 """MapReduce that removes all entities of the model which is specified
-as its parameter."""
+as its parameter.
 
+Be aware that these scripts delete all the data belonging to a model. It
+may be impossible to restore that data, so please use this module with
+extreme caution after you have consulted the development mailing list.
+"""
+
+from google.appengine.ext import ndb
 
 from mapreduce import operation
+
 
 def process(entity):
   """Processes the entity by deleting it.
@@ -25,4 +32,24 @@ def process(entity):
     entity: the specified entity
   """
   yield operation.db.Delete(entity)
+  yield operation.counters.Increment('entity_deleted')
+
+
+@ndb.transactional
+def _deleteEntityTxn(entity_key):
+  """Deletes the specified entity in a transaction.
+
+  Args:
+    entity: Entity's key.
+  """
+  ndb.Key.from_old_key(entity_key).delete()
+
+
+def processNDB(entity_key):
+  """Processes the entity by deleting it.
+
+  Args:
+    entity: the specified entity
+  """
+  _deleteEntityTxn(entity_key)
   yield operation.counters.Increment('entity_deleted')
